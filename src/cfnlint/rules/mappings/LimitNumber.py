4a39d3af928afde02bae7994cc4f14ce3ba414bd
Chuck Meyer <cmmeyer@amazon.com>
2018-05-18 00:10:08 -0400

    Added checks for limits on resources, mappings, parameters and outputs

---
+++ src/cfnlint/rules/mappings/LimitNumber.py
@@ -0,0 +1,41 @@
+"""
+  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+  software and associated documentation files (the "Software"), to deal in the Software
+  without restriction, including without limitation the rights to use, copy, modify,
+  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+from cfnlint import CloudFormationLintRule
+from cfnlint import RuleMatch
+from cfnlint.helpers import LIMITS
+
+
+class LimitNumber(CloudFormationLintRule):
+    """Check if maximum Mapping limit is exceeded"""
+    id = 'E7010'
+    shortdesc = 'Mapping limit not exceeded'
+    description = 'Check the number of Mappings in the template is less' \
+                  'than the upper limit'
+    tags = ['base', 'mappings', 'limits']
+
+    def match(self, cfn):
+        """Check CloudFormation Mappings"""
+
+        matches = list()
+
+        # Check number of mappings against the defined limit
+        mappings = cfn.template.get('Mappings', {})
+        if len(mappings) > LIMITS['mappings']['number']:
+            message = 'Maximum number of mappings ({0}) exceeded'
+            matches.append(RuleMatch(['Mappings'], message.format(LIMITS['mappings']['number'])))
+
+        return matches
