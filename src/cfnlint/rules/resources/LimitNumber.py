229ceb4bba0d1b714cd9563acde8d83d3d98ce2b
Frank van Boven <frank@cenotaph.nl>
2018-06-12 15:49:52 +0200

    Added the amount to the limit messages to clarify

--- src/cfnlint/rules/resources/LimitNumber.py
+++ src/cfnlint/rules/resources/LimitNumber.py
@@ -35,7 +35,7 @@ class LimitNumber(CloudFormationLintRule):
         # Check number of resources against the defined limit
         resources = cfn.get_resources()
         if len(resources) > LIMITS['resources']['number']:
-            message = 'Maximum number of resources ({0}) exceeded'
-            matches.append(RuleMatch(['Resources'], message.format(LIMITS['resources']['number'])))
+            message = 'The number of resources ({0}) exceeds the limit ({1})'
+            matches.append(RuleMatch(['Resources'], message.format(len(resources), LIMITS['resources']['number'])))
 
         return matches
