a35878a176049239c8afc65156114ae713b46c37
kddejong <kddejong@amazon.com>
2018-09-06 08:40:03 -0500

    Fix decode when a template syntax issue is found

--- src/cfnlint/decode/__init__.py
+++ src/cfnlint/decode/__init__.py
@@ -74,7 +74,7 @@ def decode(filename, ignore_bad_template):
         else:
             matches = [create_match_yaml_parser_error(err, filename)]
 
-    if not isinstance(template, dict):
+    if not isinstance(template, dict) and not matches:
         # Template isn't a dict which means nearly nothing will work
         matches = [cfnlint.Match(1, 1, 1, 1, filename, cfnlint.ParseError(), message='Template needs to be an object.')]
     return (template, matches)
