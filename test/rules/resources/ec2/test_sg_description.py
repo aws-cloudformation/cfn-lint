03d283e65033dcbf259a39704c27b3fcb07a8582
Chuck Meyer <cmmeyer@amazon.com>
2018-09-20 11:16:16 -0400

    Added tests for E2509 that include Fn::Sub mappings

--- test/rules/resources/ec2/test_sg_description.py
+++ test/rules/resources/ec2/test_sg_description.py
@@ -31,4 +31,4 @@ class TestPropertySgDescription(BaseRuleTestCase):
 
     def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('fixtures/templates/bad/properties_sg_description.yaml', 3)
+        self.helper_file_negative('fixtures/templates/bad/properties_sg_description.yaml', 4)
