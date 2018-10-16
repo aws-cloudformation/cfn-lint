1e0205bf867921dd35db13548d84843731a067eb
Frank van Boven <35613489+fatbasstard@users.noreply.github.com>
2018-10-16 16:13:21 +0200

    Improve invalid file loading (#373)
    * Improve invalid file loading
    
    Improve the loading of correct files, like binaries.
    
    Handle the `ReaderError` thrown when it tries to load the file. Wraps it to a `ParseError` to the end-user as a standard "unable to load and parse the given file)
    
    * Add extra catch scenario
    
    Catch specific UnicodeDecodeError errors.

--- src/cfnlint/decode/__init__.py
+++ src/cfnlint/decode/__init__.py
@@ -23,6 +23,7 @@ try:
 except ImportError:
     JSONDecodeError = ValueError
 from yaml.parser import ParserError, ScannerError
+from yaml import YAMLError
 import cfnlint.decode.cfn_yaml
 import cfnlint.decode.cfn_json
 
@@ -51,6 +52,9 @@ def decode(filename, ignore_bad_template):
 
         if matches:
             return(None, matches)
+    except UnicodeDecodeError as err:
+        LOGGER.error('Cannot read file contents: %s', filename)
+        matches.append(create_match_file_error(filename, 'Cannot read file contents: %s' % filename))
     except cfnlint.decode.cfn_yaml.CfnParseError as err:
         err.match.Filename = filename
         matches = [err.match]
@@ -77,6 +81,8 @@ def decode(filename, ignore_bad_template):
                     return(None, [create_match_file_error(filename, 'Tried to parse %s as JSON but got error: %s' % (filename, str(json_err)))])
         else:
             matches = [create_match_yaml_parser_error(err, filename)]
+    except YAMLError as err:
+        matches = [create_match_file_error(filename, err)]
 
     if not isinstance(template, dict) and not matches:
         # Template isn't a dict which means nearly nothing will work
