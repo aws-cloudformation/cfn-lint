31bd5667eb73f6801d1ab5fffb04c5867cdfdb80
kddejong <36457093+kddejong@users.noreply.github.com>
2018-10-24 10:15:33 -0500

    Support strings for IAM policies (#398)
    * Support strings for IAM policies
    
    * Allow statements to be a single dict when not a list

--- src/cfnlint/transform.py
+++ src/cfnlint/transform.py
@@ -22,7 +22,6 @@ from samtranslator.translator.translator import Translator
 from samtranslator.public.exceptions import InvalidDocumentException
 
 import cfnlint.helpers
-from cfnlint.decode.node import dict_node, list_node
 
 
 class Transform(object):
@@ -112,8 +111,8 @@ class Transform(object):
                     pass
 
             with WarningSuppressLogger(parser.logging):
-                self._template = sam_translator.translate(sam_template=self._template, parameter_values={})
-                self._correct_objects()
+                self._template = cfnlint.helpers.convert_dict(
+                    sam_translator.translate(sam_template=self._template, parameter_values={}))
         except InvalidDocumentException as e:
             for cause in e.causes:
                 matches.append(cfnlint.Match(
@@ -122,29 +121,6 @@ class Transform(object):
                     self._filename, cfnlint.TransformError(), cause.message))
 
         return matches
-
-    def _correct_objects_loop(self, template):
-        """
-            Looping for dynamic depths and types
-        """
-        if isinstance(template, dict):
-            if not isinstance(template, dict_node):
-                template = dict_node(template, (0, 0), (0, 0))
-            for k, v in template.items():
-                template[k] = self._correct_objects_loop(v)
-        elif isinstance(template, list):
-            if not isinstance(template, list_node):
-                template = list_node(template, (0, 0), (0, 0))
-            for i, v in enumerate(template):
-                template[i] = self._correct_objects_loop(v)
-
-        return template
-
-    def _correct_objects(self):
-        """
-            Corrects the template to have the correct types of objects, list, strings
-        """
-        self._template = self._correct_objects_loop(self._template)
 
     @staticmethod
     def is_s3_uri(uri):
