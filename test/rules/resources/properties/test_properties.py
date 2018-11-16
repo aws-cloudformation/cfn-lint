588ea76e0bc0914bbe05eae6597027f84725f046
kddejong <36457093+kddejong@users.noreply.github.com>
2018-11-15 23:34:22 -0600

    Support functions that could return objects (#465)

--- test/rules/resources/properties/test_properties.py
+++ test/rules/resources/properties/test_properties.py
@@ -37,7 +37,7 @@ class TestResourceProperties(BaseRuleTestCase):
 
     def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('fixtures/templates/bad/generic.yaml', 6)
+        self.helper_file_negative('fixtures/templates/bad/generic.yaml', 9)
 
     def test_file_negative_2(self):
         """Failure test"""
