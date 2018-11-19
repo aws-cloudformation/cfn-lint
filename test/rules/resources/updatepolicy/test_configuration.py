96d0c8d59aedecedb71cf348697886b5d3beb277
kddejong <36457093+kddejong@users.noreply.github.com>
2018-11-19 13:32:02 -0600

    Rewrite rule E3016 to handle conditions (#468)

--- test/rules/resources/updatepolicy/test_configuration.py
+++ test/rules/resources/updatepolicy/test_configuration.py
@@ -34,4 +34,4 @@ class TestConfiguration(BaseRuleTestCase):
 
     def test_file_negative_alias(self):
         """Test failure"""
-        self.helper_file_negative('fixtures/templates/bad/resources/updatepolicy/config.yaml', 10)
+        self.helper_file_negative('fixtures/templates/bad/resources/updatepolicy/config.yaml', 11)
