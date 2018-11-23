7345b04d17326d9e7fc7b32997d793dce1bbd1c4
Daniel Roschka <daniel.roschka@connected-health.eu>
2018-11-23 08:41:47 +0100

    Add support for Python 3.7 AWS Lambda runtime

--- src/cfnlint/rules/resources/lmbd/FunctionRuntime.py
+++ src/cfnlint/rules/resources/lmbd/FunctionRuntime.py
@@ -28,7 +28,7 @@ class FunctionRuntime(CloudFormationLintRule):
 
     runtimes = [
         'nodejs', 'nodejs4.3', 'nodejs6.10', 'nodejs8.10', 'java8', 'python2.7',
-        'python3.6', 'dotnetcore1.0', 'dotnetcore2.0', 'dotnetcore2.1',
+        'python3.6', 'python3.7', 'dotnetcore1.0', 'dotnetcore2.0', 'dotnetcore2.1',
         'nodejs4.3-edge', 'go1.x'
     ]
 
