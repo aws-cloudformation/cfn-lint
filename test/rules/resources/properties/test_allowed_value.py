a3dc1f3d2bebdc21be97fc3b69c6f395e3263e6c
Frank van Boven <frank@cenotaph.nl>
2019-01-02 15:06:56 +0100

    Create specific test fixtures for the allowed_values rule

--- test/rules/resources/properties/test_allowed_value.py
+++ test/rules/resources/properties/test_allowed_value.py
@@ -25,25 +25,13 @@ class TestAllowedValue(BaseRuleTestCase):
         super(TestAllowedValue, self).setUp()
         self.collection.register(AllowedValue())
         self.success_templates = [
-            'test/fixtures/templates/good/resources_lambda.yaml'
+            'test/fixtures/templates/good/resources/properties/allowed_values.yaml'
         ]
 
     def test_file_positive(self):
         """Test Positive"""
         self.helper_file_positive()
 
-    def test_file_negative_runtime(self):
+    def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/resources_lambda.yaml', 3)
-
-    def test_file_negative_recordset(self):
-        """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/route53.yaml', 1)
-
-    def test_file_negative_ebsvolume(self):
-        """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/properties_ebs.yaml', 1)
-
-    def test_file_negative_vpctenancy(self):
-        """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/properties_ec2_network.yaml', 2)
+        self.helper_file_negative('test/fixtures/templates/bad/resources/properties/allowed_values.yaml', 4)
