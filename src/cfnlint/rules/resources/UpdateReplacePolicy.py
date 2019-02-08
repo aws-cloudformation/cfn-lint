ee77a62c964f9a765dff90050ef338faae74558f
Chuck Meyer <cmmeyer@amazon.com>
2019-02-07 22:15:26 -0500

    E3036 New rule for UpdateReplacePolicy values  (#635)
    * E3036 New rule for UpdateReplacePolicy values
    
    * Fixed tags on rule
    
    * Added alt text to the logo

--- src/cfnlint/rules/resources/UpdateReplacePolicy.py
+++ src/cfnlint/rules/resources/UpdateReplacePolicy.py
@@ -19,16 +19,16 @@ from cfnlint import CloudFormationLintRule
 from cfnlint import RuleMatch
 
 
-class DeletionPolicy(CloudFormationLintRule):
+class UpdateReplacePolicy(CloudFormationLintRule):
     """Check Base Resource Configuration"""
-    id = 'E3035'
-    shortdesc = 'Check DeletionPolicy values for Resources'
-    description = 'Check that the DeletionPolicy values are valid'
-    source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-deletionpolicy.html'
-    tags = ['resources', 'deletionpolicy']
+    id = 'E3036'
+    shortdesc = 'Check UpdateReplacePolicy values for Resources'
+    description = 'Check that the UpdateReplacePolicy values are valid'
+    source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-updatereplacepolicy.html'
+    tags = ['resources', 'updatereplacepolicy']
 
     def check_value(self, key, path, res_type):
-        """Check resource names for DeletionPolicy"""
+        """Check resource names for UpdateReplacePolicy"""
         matches = []
 
         valid_values = [
@@ -47,17 +47,17 @@ class DeletionPolicy(CloudFormationLintRule):
             'AWS::Redshift::Cluster'
         ]
         if not isinstance(key, (six.text_type, six.string_types)):
-            message = 'DeletionPolicy values should be of string at {0}'
+            message = 'UpdateReplacePolicy values should be of string at {0}'
             matches.append(RuleMatch(path, message.format('/'.join(map(str, path)))))
             return matches
         if key not in valid_values:
-            message = 'DeletionPolicy should be only one of {0} at {1}'
+            message = 'UpdateReplacePolicy should be only one of {0} at {1}'
             matches.append(RuleMatch(
                 path,
                 message.format(', '.join(map(str, valid_values)),
                                '/'.join(map(str, path)))))
         if key == 'Snapshot' and res_type not in valid_snapshot_types:
-            message = 'DeletionPolicy cannot be Snapshot for resources of type {0} at {1}'
+            message = 'UpdateReplacePolicy cannot be Snapshot for resources of type {0} at {1}'
             matches.append(RuleMatch(
                 path,
                 message.format(res_type,
@@ -73,15 +73,15 @@ class DeletionPolicy(CloudFormationLintRule):
         resources = cfn.get_resources()
 
         for resource_name, resource_values in resources.items():
-            deletion_policies = resource_values.get('DeletionPolicy')
-            if deletion_policies:
-                path = ['Resources', resource_name, 'DeletionPolicy']
+            updatereplace_policies = resource_values.get('UpdateReplacePolicy')
+            if updatereplace_policies:
+                path = ['Resources', resource_name, 'UpdateReplacePolicy']
                 res_type = resource_values.get('Type')
-                self.logger.debug('Validating DeletionPolicy for %s base configuration', resource_name)
-                if isinstance(deletion_policies, list):
-                    message = 'Only one DeletionPolicy allowed per resource at {0}'
+                self.logger.debug('Validating UpdateReplacePolicy for %s base configuration', resource_name)
+                if isinstance(updatereplace_policies, list):
+                    message = 'Only one UpdateReplacePolicy allowed per resource at {0}'
                     matches.append(RuleMatch(path, message.format('/'.join(map(str, path)))))
                 else:
-                    matches.extend(self.check_value(deletion_policies, path, res_type))
+                    matches.extend(self.check_value(updatereplace_policies, path, res_type))
 
         return matches
