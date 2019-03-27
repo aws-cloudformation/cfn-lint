2d9d15bae5bb6ef041f1a114441f9bf2d67ec524
Chuck Meyer <chuck.meyer@boldpenguin.com>
2019-03-27 08:11:30 -0400

    E2532 allow new string pattern for ActionID version

--- src/cfnlint/rules/resources/codepipeline/CodepipelineStageActions.py
+++ src/cfnlint/rules/resources/codepipeline/CodepipelineStageActions.py
@@ -14,6 +14,7 @@
   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import re
 import six
 from cfnlint import CloudFormationLintRule
 from cfnlint import RuleMatch
@@ -101,6 +102,7 @@ class CodepipelineStageActions(CloudFormationLintRule):
         'OutputArtifacts': 'OutputArtifactRange',
     }
 
+
     def check_artifact_counts(self, action, artifact_type, path):
         """Check that artifact counts are within valid ranges."""
         matches = []
@@ -159,15 +161,30 @@ class CodepipelineStageActions(CloudFormationLintRule):
         """Check that action type version is valid."""
         matches = []
 
+        REGEX_VERSION_STRING = re.compile(r'^[0-9A-Za-z_-]+$')
+        LENGTH_MIN = 1
+        LENGTH_MAX = 9
+
         version = action.get('ActionTypeId', {}).get('Version')
         if isinstance(version, dict):
             self.logger.debug('Unable to validate version when an object is used.  Skipping')
-        elif version != '1':
-            message = 'For all currently supported action types, the only valid version string is "1".'
-            matches.append(RuleMatch(
-                path + ['ActionTypeId', 'Version'],
-                message
-            ))
+        elif isinstance(version, (six.string_types)):
+            if not re.match(REGEX_VERSION_STRING, version):
+                message = 'For all action types, the version string must match the pattern [0-9A-Za-z_-]+.'
+                matches.append(RuleMatch(
+                    path + ['ActionTypeId', 'Version'],
+                    message
+                ))
+            elif len(version) > LENGTH_MAX:
+                message = 'The length of the version string ({0}) must be less than {1} characters'
+                matches.append(RuleMatch(
+                    path + ['ActionTypeId', 'Version'],
+                    message.format(version, LENGTH_MAX)))
+            elif len(version) < LENGTH_MIN:
+                message = 'The length of the version string ({0}) must be at least {1} characters'
+                matches.append(RuleMatch(
+                    path + ['ActionTypeId', 'Version'],
+                    message.format(version, LENGTH_MIN)))
         return matches
 
     def check_names_unique(self, action, path, action_names):
