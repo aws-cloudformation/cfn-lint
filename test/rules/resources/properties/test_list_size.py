8cfbfd3a40851c14633061dc88140541e56a0785
Kevin DeJong <kddejong@amazon.com>
2019-03-30 09:16:54 -0500

    Extend specs for list size and create new rule (#759)

--- test/rules/resources/properties/test_list_size.py
+++ test/rules/resources/properties/test_list_size.py
@@ -14,21 +14,25 @@
   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-from cfnlint.rules.resources.iam.Limits import Limits  # pylint: disable=E0401
+from cfnlint.rules.resources.properties.ListSize import ListSize  # pylint: disable=E0401
 from ... import BaseRuleTestCase
 
 
-class TestPropertyIamLimits(BaseRuleTestCase):
-    """Test IAM Limits"""
+class TestListSize(BaseRuleTestCase):
+    """Test List Size Property Configuration"""
     def setUp(self):
         """Setup"""
-        super(TestPropertyIamLimits, self).setUp()
-        self.collection.register(Limits())
+        super(TestListSize, self).setUp()
+        self.collection.register(ListSize())
 
     def test_file_positive(self):
         """Test Positive"""
         self.helper_file_positive()
 
-    def test_role_assume_role_policy_document(self):
+    def test_file_negative_iam_managed_policies(self):
         """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/resources_iam_role_assume_role_policy_document.yaml', 1)
+        self.helper_file_negative('test/fixtures/templates/bad/resources_iam_managedpolicyarns.yaml', 3)
+
+    def test_file_negative_iam_user_groups(self):
+        """Test failure"""
+        self.helper_file_negative('test/fixtures/templates/bad/resources_iam_user_groups.yaml', 1)
