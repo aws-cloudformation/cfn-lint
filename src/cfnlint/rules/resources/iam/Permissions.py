9f302edca79a40e0201d7484f28a4310341afb54
Kevin DeJong <kddejong@amazon.com>
2019-05-30 20:51:58 -0500

    Include more resources in W3037 (#939)

--- src/cfnlint/rules/resources/iam/Permissions.py
+++ src/cfnlint/rules/resources/iam/Permissions.py
@@ -14,8 +14,9 @@
   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+import json
 import six
-
+from cfnlint.helpers import convert_dict
 from cfnlint import CloudFormationLintRule
 from cfnlint import RuleMatch
 
@@ -31,12 +32,25 @@ class Permissions(CloudFormationLintRule):
     tags = ['properties', 'iam', 'permissions']
     experimental = True
 
-    IAM_PERMISSION_RESOURCE_TYPES = [
-        'AWS::IAM::ManagedPolicy',
-        'AWS::IAM::Policy',
-        'AWS::SNS::TopicPolicy',
-        'AWS::SQS::QueuePolicy'
-    ]
+    IAM_PERMISSION_RESOURCE_TYPES = {
+        'AWS::SNS::TopicPolicy': 'PolicyDocument',
+        'AWS::S3::BucketPolicy': 'PolicyDocument',
+        'AWS::KMS::Key': 'KeyPolicy',
+        'AWS::SQS::QueuePolicy': 'PolicyDocument',
+        'AWS::Elasticsearch::Domain': 'AccessPolicies',
+        'AWS::IAM::Group': 'Policies',
+        'AWS::IAM::ManagedPolicy': 'PolicyDocument',
+        'AWS::IAM::Policy': 'PolicyDocument',
+        'AWS::IAM::Role': 'Policies',
+        'AWS::IAM::User': 'Policies',
+    }
+
+    def __init__(self):
+        """Init"""
+        super(Permissions, self).__init__()
+        self.service_map = self.load_service_map()
+        for resource_type in self.IAM_PERMISSION_RESOURCE_TYPES:
+            self.resource_property_types.append(resource_type)
 
     def load_service_map(self):
         """
@@ -59,50 +73,87 @@ class Permissions(CloudFormationLintRule):
 
         return policy_service_map
 
-    def check_permissions(self, action, path, service_map):
+    def check_policy_document(self, value, path, start_mark, end_mark):
+        """Check policy document"""
+        matches = []
+
+        if isinstance(value, six.string_types):
+            try:
+                value = convert_dict(json.loads(value), start_mark, end_mark)
+            except Exception as ex:  # pylint: disable=W0703,W0612
+                message = 'IAM Policy Documents need to be JSON'
+                matches.append(RuleMatch(path[:], message))
+                return matches
+
+        if not isinstance(value, dict):
+            return matches
+
+        for p_vs, p_p in value.items_safe(path[:], (dict)):
+            statements = p_vs.get('Statement')
+            if statements:
+                if isinstance(statements, dict):
+                    statements = [statements]
+                if isinstance(statements, list):
+                    for index, statement in enumerate(statements):
+                        actions = []
+
+                        if isinstance(statement, dict):
+                            actions.extend(self.get_actions(statement))
+
+                        elif isinstance(statement, list):
+                            for effective_permission in statement:
+                                actions.extend(self.get_actions(effective_permission))
+
+                        for action in actions:
+                            matches.extend(self.check_permissions(action, p_p + ['Statement', index]))
+
+        return matches
+
+    def check_permissions(self, action, path):
         """
         Check if permission is valid
         """
         matches = []
-
         if action == '*':
             return matches
 
-        service, permission = action.split(':')
+        service, permission = action.split(':', 1)
         # Get lowercase so we can check case insenstive. Keep the original values for the message
         service_value = service.lower()
         permission_value = permission.lower()
 
-        if service_value in service_map:
-            if permission_value.endswith('*'):
+        if service_value in self.service_map:
+            if permission_value == '*':
+                pass
+            elif permission_value.endswith('*'):
                 wilcarded_permission = permission_value.split('*')[0]
-                if not any(wilcarded_permission in action for action in service_map[service_value]):
+                if not any(wilcarded_permission in action for action in self.service_map[service_value]):
                     message = 'Invalid permission "{}" for "{}" found in permissions'
                     matches.append(
                         RuleMatch(
                             path,
-                            message.format(permission, service, '/'.join(path))))
+                            message.format(permission, service)))
 
             elif permission_value.startswith('*'):
                 wilcarded_permission = permission_value.split('*')[1]
-                if not any(wilcarded_permission in action for action in service_map[service_value]):
+                if not any(wilcarded_permission in action for action in self.service_map[service_value]):
                     message = 'Invalid permission "{}" for "{}" found in permissions'
                     matches.append(
                         RuleMatch(
                             path,
-                            message.format(permission, service, '/'.join(path))))
-            elif permission_value not in service_map[service_value]:
+                            message.format(permission, service)))
+            elif permission_value not in self.service_map[service_value]:
                 message = 'Invalid permission "{}" for "{}" found in permissions'
                 matches.append(
                     RuleMatch(
                         path,
-                        message.format(permission, service, '/'.join(path))))
+                        message.format(permission, service)))
         else:
             message = 'Invalid service "{}" found in permissions'
             matches.append(
                 RuleMatch(
                     path,
-                    message.format(service, '/'.join(path))))
+                    message.format(service)))
 
         return matches
 
@@ -125,29 +176,28 @@ class Permissions(CloudFormationLintRule):
 
         return actions
 
-    def match(self, cfn):
-        """Check IAM Permissions"""
+    def match_resource_properties(self, properties, resourcetype, path, cfn):
+        """Check CloudFormation Properties"""
         matches = []
 
-        statements = []
-        service_map = self.load_service_map()
-        for resource_type in self.IAM_PERMISSION_RESOURCE_TYPES:
-            statements.extend(cfn.get_resource_properties([resource_type, 'PolicyDocument', 'Statement']))
-
-        for statement in statements:
-            path = statement['Path']
-            effective_permissions = statement['Value']
-
-            actions = []
-
-            if isinstance(effective_permissions, dict):
-                actions.extend(self.get_actions(effective_permissions))
-
-            elif isinstance(effective_permissions, list):
-                for effective_permission in effective_permissions:
-                    actions.extend(self.get_actions(effective_permission))
-
-            for action in actions:
-                matches.extend(self.check_permissions(action, path, service_map))
+        key = self.IAM_PERMISSION_RESOURCE_TYPES.get(resourcetype)
+        for key, value in properties.items():
+            if key == 'Policies' and isinstance(value, list):
+                for index, policy in enumerate(properties.get(key, [])):
+                    matches.extend(
+                        cfn.check_value(
+                            obj=policy, key='PolicyDocument',
+                            path=path[:] + ['Policies', index],
+                            check_value=self.check_policy_document,
+                            start_mark=key.start_mark, end_mark=key.end_mark,
+                        ))
+            elif key in ['KeyPolicy', 'PolicyDocument', 'RepositoryPolicyText', 'AccessPolicies']:
+                matches.extend(
+                    cfn.check_value(
+                        obj=properties, key=key,
+                        path=path[:],
+                        check_value=self.check_policy_document,
+                        start_mark=key.start_mark, end_mark=key.end_mark,
+                    ))
 
         return matches
