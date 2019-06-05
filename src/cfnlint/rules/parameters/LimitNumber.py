345200b706be75b2b6e072d650bce313b23b3a87
Frank van Boven <frank@cenotaph.nl>
2019-06-05 11:18:36 +0200

    Fix typo and update rule documentation

--- src/cfnlint/rules/parameters/LimitNumber.py
+++ src/cfnlint/rules/parameters/LimitNumber.py
@@ -23,8 +23,7 @@ class LimitNumber(CloudFormationLintRule):
     """Check if maximum Parameter limit is exceeded"""
     id = 'E2010'
     shortdesc = 'Parameter limit not exceeded'
-    description = 'Check the number of Parameters in the template is less' \
-                  'than the upper limit'
+    description = 'Check the number of Parameters in the template is less than the upper limit'
     source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html'
     tags = ['parameters', 'limits']
 
