2ad9398391c04c8daca754dad6db9b05c1639739
kddejong <kddejong@amazon.com>
2019-06-11 09:43:47 -0500

    Don't error E8002 when resource condition is not a string

--- test/rules/conditions/test_exists.py
+++ test/rules/conditions/test_exists.py
@@ -35,4 +35,4 @@ class TestExistsConditions(BaseRuleTestCase):
 
     def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/conditions.yaml', 3)
+        self.helper_file_negative('test/fixtures/templates/bad/conditions.yaml', 2)
