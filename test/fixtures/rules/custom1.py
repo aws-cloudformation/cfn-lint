523a51938d09e3684b5da6637e8f7dafa595438b
Roger López <zroger@users.noreply.github.com>
2019-11-22 15:16:33 -0500

    Support appending rules from a module

---
+++ test/fixtures/rules/custom1.py
@@ -0,0 +1,13 @@
+"""
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule  # pylint: disable=E0401
+
+class CustomRule1(CloudFormationLintRule):
+    """ Def Rule """
+    id = 'E9001'
+    shortdesc = 'Custom Rule 1'
+    description = 'Test Rule Description'
+    source_url = 'https://github.com/aws-cloudformation/cfn-python-lint/'
+    tags = ['resources']
