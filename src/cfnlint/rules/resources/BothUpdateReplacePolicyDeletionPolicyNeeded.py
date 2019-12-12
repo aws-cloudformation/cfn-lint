b2a8173703bf1ad80e9e19bfebd19e57ac685e94
Pat Myron <pamyron@amazon.com>
2019-12-11 17:38:30 -0800

    generalizing 3011 - checking UpdateReplacePolicy/DeletionPolicy on more resource types

---
+++ src/cfnlint/rules/resources/BothUpdateReplacePolicyDeletionPolicyNeeded.py
@@ -0,0 +1,27 @@
+"""
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules import RuleMatch
+
+
+class UpdateReplacePolicyDeletionPolicy(CloudFormationLintRule):
+    """Check resources with UpdateReplacePolicy/DeletionPolicy have both"""
+    id = 'W3011'
+    shortdesc = 'Check resources with UpdateReplacePolicy/DeletionPolicy have both'
+    description = 'Both UpdateReplacePolicy and DeletionPolicy are needed to protect resources from deletion'
+    source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-attribute-deletionpolicy.html'
+    tags = ['resources', 'updatereplacepolicy', 'deletionpolicy']
+
+    def match(self, cfn):
+        """Check resources with UpdateReplacePolicy/DeletionPolicy have both"""
+        matches = []
+
+        for r_name, r_values in cfn.get_resources().items():
+            if r_values.get('DeletionPolicy') and not r_values.get('UpdateReplacePolicy') or not r_values.get('DeletionPolicy') and r_values.get('UpdateReplacePolicy'):
+                path = ['Resources', r_name]
+                message = 'Both UpdateReplacePolicy and DeletionPolicy are needed to protect resources from deletion'
+                matches.append(RuleMatch(path, message))
+
+        return matches
--- test/unit/rules/resources/dynamodb/test_delete_policy.py
+++ test/unit/rules/resources/dynamodb/test_delete_policy.py
@@ -3,16 +3,16 @@ Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 from test.unit.rules import BaseRuleTestCase
-from cfnlint.rules.resources.dynamodb.TableDeletionPolicy import TableDeletionPolicy  # pylint: disable=E0401
+from cfnlint.rules.resources.UpdateReplacePolicyDeletionPolicyOnStatefulResourceTypes import UpdateReplacePolicyDeletionPolicyOnStatefulResourceTypes  # pylint: disable=E0401
 
 
-class TestTableDeletionPolicy(BaseRuleTestCase):
+class TestUpdateReplacePolicyDeletionPolicyOnStatefulResourceTypes(BaseRuleTestCase):
     """Test StateMachine for Step Functions"""
 
     def setUp(self):
         """Setup"""
-        super(TestTableDeletionPolicy, self).setUp()
-        self.collection.register(TableDeletionPolicy())
+        super(TestUpdateReplacePolicyDeletionPolicyOnStatefulResourceTypes, self).setUp()
+        self.collection.register(UpdateReplacePolicyDeletionPolicyOnStatefulResourceTypes())
         self.success_templates = [
             'test/fixtures/templates/good/resources/dynamodb/delete_policy.yaml'
         ]
