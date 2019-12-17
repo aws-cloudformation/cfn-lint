cbe5206bbd3635906fadf4e05d9d56f31db75ce8
Kevin DeJong <kddejong@amazon.com>
2019-12-17 09:49:23 -0600

    Allow for ephemeral 0-23 in E2504

--- test/unit/rules/resources/ec2/test_ec2_ebs.py
+++ test/unit/rules/resources/ec2/test_ec2_ebs.py
@@ -13,6 +13,9 @@ class TestPropertyEc2Ebs(BaseRuleTestCase):
         """Setup"""
         super(TestPropertyEc2Ebs, self).setUp()
         self.collection.register(Ebs())
+        self.success_templates = [
+            'test/fixtures/templates/good/resources/ec2/ebs.yaml',
+        ]
 
     def test_file_positive(self):
         """Test Positive"""
@@ -20,4 +23,4 @@ class TestPropertyEc2Ebs(BaseRuleTestCase):
 
     def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/properties_ebs.yaml', 3)
+        self.helper_file_negative('test/fixtures/templates/bad/resources/ec2/ebs.yaml', 5)
