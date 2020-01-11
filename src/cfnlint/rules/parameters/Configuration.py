bcae334c2145b7962c55cfca5abcc4cea615f7a0
Kevin DeJong <kddejong@amazon.com>
2020-01-11 07:19:57 -0600

    Build out more parameter configuration logic in rule E2001

--- src/cfnlint/rules/parameters/Configuration.py
+++ src/cfnlint/rules/parameters/Configuration.py
@@ -14,23 +14,91 @@ class Configuration(CloudFormationLintRule):
     source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/parameters-section-structure.html'
     tags = ['parameters']
 
-    valid_keys = [
-        'AllowedPattern',
-        'AllowedValues',
-        'ConstraintDescription',
-        'Default',
-        'Description',
-        'MaxLength',
-        'MaxValue',
-        'MinLength',
-        'MinValue',
-        'NoEcho',
-        'Type',
-    ]
+    valid_keys = {
+        'AllowedPattern': {
+            'Type': 'String'
+        },
+        'AllowedValues': {
+            'Type': 'List',
+            'ItemType': 'String',
+        },
+        'ConstraintDescription': {
+            'Type': 'String'
+        },
+        'Default': {
+            'Type': 'String'
+        },
+        'Description': {
+            'Type': 'String'
+        },
+        'MaxLength': {
+            'Type': 'Integer',
+            'ValidForTypes': ['String']
+        },
+        'MaxValue': {
+            'Type': 'Integer',
+            'ValidForTypes': ['Number']
+        },
+        'MinLength': {
+            'Type': 'Integer',
+            'ValidForTypes': ['String']
+        },
+        'MinValue': {
+            'Type': 'Integer',
+            'ValidForTypes': ['Number']
+        },
+        'NoEcho': {
+            'Type': 'Boolean'
+        },
+        'Type': {
+            'Type': 'String'
+        }
+    }
 
     required_keys = [
         'Type'
     ]
+
+    def check_type(self, value, path, props):
+        """ Check the type and handle recursion with lists """
+        results = []
+        prop_type = props.get('Type')
+        try:
+            if prop_type in ['List']:
+                if isinstance(value, list):
+                    for i, item in enumerate(value):
+                        results.extend(self.check_type(item, path[:] + [i], {
+                            'Type': props.get('ItemType')
+                        }))
+                else:
+                    message = 'Property %s should be of type %s' % (
+                        '/'.join(map(str, path)), prop_type)
+                    results.append(RuleMatch(path, message))
+            if prop_type in ['String']:
+                if isinstance(value, (dict, list)):
+                    message = 'Property %s should be of type %s' % (
+                        '/'.join(map(str, path)), prop_type)
+                    results.append(RuleMatch(path, message))
+                str(value)
+            elif prop_type in ['Boolean']:
+                if not isinstance(value, bool):
+                    if value not in ['True', 'true', 'False', 'false']:
+                        message = 'Property %s should be of type %s' % (
+                            '/'.join(map(str, path)), prop_type)
+                        results.append(RuleMatch(path, message))
+            elif prop_type in ['Integer']:
+                if isinstance(value, bool):
+                    message = 'Property %s should be of type %s' % (
+                        '/'.join(map(str, path)), prop_type)
+                    results.append(RuleMatch(path, message))
+                else:  # has to be a Double
+                    int(value)
+        except Exception:  # pylint: disable=W0703
+            message = 'Property %s should be of type %s' % (
+                '/'.join(map(str, path)), prop_type)
+            results.append(RuleMatch(path, message,))
+
+        return results
 
     def match(self, cfn):
         """Check CloudFormation Parameters"""
@@ -38,13 +106,28 @@ class Configuration(CloudFormationLintRule):
         matches = []
 
         for paramname, paramvalue in cfn.get_parameters().items():
-            for propname, _ in paramvalue.items():
+            for propname, propvalue in paramvalue.items():
                 if propname not in self.valid_keys:
                     message = 'Parameter {0} has invalid property {1}'
                     matches.append(RuleMatch(
                         ['Parameters', paramname, propname],
                         message.format(paramname, propname)
                     ))
+                else:
+                    props = self.valid_keys.get(propname)
+                    prop_path = ['Parameters', paramname, propname]
+                    matches.extend(self.check_type(
+                        propvalue, prop_path, props))
+                    # Check that the property is needed for the current type
+                    valid_for = props.get('ValidForTypes')
+                    if valid_for is not None and paramvalue.get('Type'):
+                        if paramvalue.get('Type') not in valid_for:
+                            message = 'Parameter {0} has property {1} which is only valid for {2}'
+                            matches.append(RuleMatch(
+                                ['Parameters', paramname, propname],
+                                message.format(paramname, propname, valid_for)
+                            ))
+
             for reqname in self.required_keys:
                 if reqname not in paramvalue.keys():
                     message = 'Parameter {0} is missing required property {1}'
