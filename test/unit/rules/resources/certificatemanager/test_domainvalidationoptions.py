3bad321d893f2ca49fa32ab43e3febc29acaa5ac
Sander Bol <s.bol@coolblue.nl>
2020-02-18 16:35:10 +0100

    Validate CertificateManager::Certificate::DomainValidationOptions
    This introduces validation of CertificateManager::Certificate::DomainValidationOptions to ensure that the ValidationDomain is a superdomain of the domain being validated.
    
    Eg: "subdomain.example.com" can be validated by itself or "example.com", but not by "example.net".
    
    Deployment of the stack fails if this condition is not met, so I've introduced this as an error-level rule.

---
+++ test/unit/rules/resources/certificatemanager/test_domainvalidationoptions.py
@@ -0,0 +1,24 @@
+"""
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from test.unit.rules import BaseRuleTestCase
+from cfnlint.rules.resources.certificatemanager.DomainValidationOptions import DomainValidationOptions
+
+class TestDomainValidationOptions(BaseRuleTestCase):
+    """Test ValidationDomainOptions Configuration"""
+
+    def setUp(self):
+        """Setup"""
+        super(TestDomainValidationOptions, self).setUp()
+        self.collection.register(DomainValidationOptions())
+
+    def test_file_positive(self):
+        """Test Positive"""
+        self.helper_file_positive_template(
+            'test/fixtures/templates/good/resources/certificatemanager/domain_validation_options.yaml')
+
+    def test_file_negative_invalid_validationdomain(self):
+        """Test failure"""
+        self.helper_file_negative(
+            'test/fixtures/templates/bad/resources/certificatemanager/domain_validation_options.yaml', 2)
