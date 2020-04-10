81f9043793f50953bbbd101b8e35681518dfc255
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2020-04-10 22:17:58 +0100

    Adding tests for the download metadata methods

---
+++ test/unit/module/helpers/test_downloads_metadata.py
@@ -0,0 +1,75 @@
+"""
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+import sys
+import os
+from test.testlib.testcase import BaseTestCase
+from mock import patch, MagicMock, mock_open
+import cfnlint.helpers
+import json
+
+
+class TestDownloadsMetadata(BaseTestCase):
+    """Test Downloads Metadata"""
+
+    @patch('cfnlint.helpers.os.path.exists')
+    def test_no_file(self, mock_path_exists):
+        """Test success run"""
+
+        mock_path_exists.return_value = False
+
+        filename = 'foo.bar'
+
+        result = cfnlint.helpers.get_download_metadata(filename)
+
+        self.assertEqual(result, {})
+
+    @patch('cfnlint.helpers.os.path.exists')
+    def test_get_download_metadata(self, mock_path_exists):
+        """Test success run"""
+
+        mock_path_exists.return_value = True
+
+        filename = 'foo.bar'
+        file_contents = {
+            'etag': 'foo'
+        }
+
+        if sys.version_info.major == 3:
+            builtin_module_name = 'builtins'
+        else:
+            builtin_module_name = '__builtin__'
+
+        mo = mock_open(read_data=json.dumps(file_contents))
+        with patch('{}.open'.format(builtin_module_name), mo) as mock_builtin_open:
+            result = cfnlint.helpers.get_download_metadata(filename)
+
+            self.assertEqual(result, file_contents)
+
+    @patch('cfnlint.helpers.os.path.exists')
+    @patch('cfnlint.helpers.os.path.dirname')
+    @patch('cfnlint.helpers.os.mkdir')
+    @patch('cfnlint.helpers.json.dump')
+    def test_save_download_metadata(self, mock_json_dump, mock_mkdir, mock_dirname, mock_path_exists):
+        """Test success run"""
+
+        filename = 'foo.bar'
+        filedir = 'foobardir'
+        file_contents = {
+            'etag': 'foo'
+        }
+
+        mock_path_exists.return_value = False
+        mock_dirname.return_value = filedir
+
+        if sys.version_info.major == 3:
+            builtin_module_name = 'builtins'
+        else:
+            builtin_module_name = '__builtin__'
+
+        mo = mock_open()
+        with patch('{}.open'.format(builtin_module_name), mo) as mock_builtin_open:
+            cfnlint.helpers.save_download_metadata(file_contents, filename)
+            mock_mkdir.assert_called_with(filedir)
+            mock_json_dump.assert_called_once
