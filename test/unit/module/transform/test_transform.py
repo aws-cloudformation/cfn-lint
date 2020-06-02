db42bb19fc1cd5e9010fd878377b6dc449e02da5
Kevin DeJong <kevin.dejong@gmail.com>
2020-06-02 10:21:52 -0500

    Updates to support state machine with local files

--- test/unit/module/transform/test_transform.py
+++ test/unit/module/transform/test_transform.py
@@ -28,6 +28,21 @@ class TestTransform(BaseTestCase):
                 'FunctionVersion': {'Fn::GetAtt': ['SkillFunctionVersion55ff35af87', 'Version']}
             })
 
+    def test_conversion_of_step_function_definition_uri(self):
+        """ Tests that the a serverless step function can convert a local path to a s3 path """
+        filename = 'test/fixtures/templates/good/transform/step_function_local_definition.yaml'
+        region = 'us-east-1'
+        template = cfn_yaml.load(filename)
+        transformed_template = Transform(filename, template, region)
+        transformed_template.transform_template()
+        self.assertDictEqual(
+            transformed_template._template.get('Resources').get(
+                'StateMachine').get('Properties').get('DefinitionS3Location'),
+            {
+                'Bucket': 'bucket',
+                'Key': 'value'
+            })
+
     def test_parameter_for_autopublish_version_bad(self):
         """Test Parameter is created for autopublish version run"""
         filename = 'test/fixtures/templates/bad/transform/auto_publish_alias.yaml'
