07cd22291804009b9623f0408b697049f2dfb3a2
TaeHun Kang <taehkang@amazon.com>
2020-06-02 10:43:42 -0700

    Resolving most warnings, adding details to error message

--- src/cfnlint/custom_rules.py
+++ src/cfnlint/custom_rules.py
@@ -1,9 +1,16 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+# pylint: disable=W0108
+# pylint: disable=W0622
 import logging
 import cfnlint
 import cfnlint.customRules.Operators
 
 LOGGER = logging.getLogger(__name__)
-Operator = {'EQUALS': lambda x, y: cfnlint.customRules.Operators.equalsOp(x, y)}
+Operator = {'EQUALS': lambda x, y: cfnlint.customRules.Operators.equalsOp(x, y),
+            'PLACEHOLDER': lambda x, y: LOGGER.debug('Placeholder Op')}
 
 def check(filename, template):
     """ Process custom rule file """
@@ -21,8 +28,9 @@ def check(filename, template):
                     result = Operator[rule[2]](rule, template.get_resource_properties([resource_type]))
                     if result != operator:
                         matches.append(cfnlint.rules.Match(count, '0', '0', '0', filename, CustomRule('E9999'), result, None))
-                except KeyError as e:
-                    matches.append(cfnlint.rules.Match(count, '0', '0', '0', filename, CustomRule('E9999'), 'Error - Invalid Operator', None))
+                except KeyError:
+                    matches.append(cfnlint.rules.Match(count, '0', '0', '0', filename, CustomRule('E9999'),
+                                                       str(rule[2]) + ' not in supported operators: [EQUALS] at ' + str(line), None))
             count += 1
     return matches
 
@@ -30,7 +38,7 @@ def getProperty(json, nestedProperties):
     """ Converts dot format strings to resultant values -
     i.e inputting 'Value.InstanceSize' to nestedProperties will output the value of that specific property from json"""
     nestedProperties = 'Value.' + str(nestedProperties)
-    properties = nestedProperties.split(".")
+    properties = nestedProperties.split('.')
     for prop in properties:
         try:
             json = json[prop]
@@ -39,7 +47,6 @@ def getProperty(json, nestedProperties):
     return str(json)
 
 class CustomRule(object):
+    """ Allows creation of match objects"""
     def __init__(self, id):
-        self.id = id
-
-
+        self.__name__ = id
