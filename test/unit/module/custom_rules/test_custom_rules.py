c50d4265d89e575f1b92b90fc78124ec4ed7a067
TaeHun Kang <taehkang@amazon.com>
2020-06-03 14:30:00 -0700

    More unit tests

--- test/unit/module/custom_rules/test_custom_rules.py
+++ test/unit/module/custom_rules/test_custom_rules.py
@@ -30,6 +30,8 @@ class TestCustomRuleParsing(BaseTestCase):
         self.valid_rule = 'test/fixtures/templates/good/custom_rule_test.txt'
         self.invalid_op = 'test/fixtures/templates/bad/custom_rule_invalid_op.txt'
         self.invalid_prop = 'test/fixtures/templates/bad/custom_rule_invalid_prop.txt'
+        self.invalid_propkey = 'test/fixtures/templates/bad/custom_rule_invalid_propkey.txt'
+        self.invalid_rt = 'test/fixtures/templates/bad/custom_rule_invalid_rt.txt'
 
     def test_success_parse(self):
         """Test Successful YAML Parsing"""
@@ -59,4 +61,24 @@ class TestCustomRuleParsing(BaseTestCase):
             template = cfnlint.decode.cfn_yaml.load(filename)
             cfn = Template(filename, template, ['us-east-1'])
             matches = cfnlint.custom_rules.check(self.invalid_prop, cfn)
+            assert (matches[0].message.find('property was not found') > -1)
+
+    def test_invalid_propKey(self):
+        """Test Successful YAML Parsing"""
+        for _, values in self.filenames.items():
+            filename = values.get('filename')
+            failures = values.get('failures')
+            template = cfnlint.decode.cfn_yaml.load(filename)
+            cfn = Template(filename, template, ['us-east-1'])
+            matches = cfnlint.custom_rules.check(self.invalid_propkey, cfn)
+            assert (matches[0].message.find('property was not found') > -1)
+
+    def test_invalid_resource_type(self):
+        """Test Successful YAML Parsing"""
+        for _, values in self.filenames.items():
+            filename = values.get('filename')
+            failures = values.get('failures')
+            template = cfnlint.decode.cfn_yaml.load(filename)
+            cfn = Template(filename, template, ['us-east-1'])
+            matches = cfnlint.custom_rules.check(self.invalid_rt, cfn)
             assert (matches[0].message.find('property was not found') > -1)
