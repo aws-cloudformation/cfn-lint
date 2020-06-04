6ed175a2da5dee8c6fa2a46e7c3eeebf48bbebe6
TaeHun Kang <taehkang@amazon.com>
2020-06-03 17:58:55 -0700

    Rephrased error messages for clarity

--- src/cfnlint/customRules/Operators.py
+++ src/cfnlint/customRules/Operators.py
@@ -3,26 +3,28 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
+
 def equalsOp(rule, propertyList):
     """ Process EQUALS operators """
     if not propertyList:
-        return 'Error - Resource type not found ' + rule.resourceType
+        return 'EQUALS'  # Resource type not found
     for prop in propertyList:
-        actualValue = getProperty(prop, rule.prop)
+        actualValue = getProperty(prop, rule)
         if actualValue.strip() != str(rule.value).strip():
             return 'Not Equal as ' + actualValue + ' does not equal ' + rule.value
     return 'EQUALS'
 
-def getProperty(json, nestedProperties):
+
+def getProperty(json, rule):
     """ Converts dot format strings to resultant values -
     i.e inputting 'Value.InstanceSize' to nestedProperties will output the value of that specific property from json"""
-    nestedProperties = 'Value.' + str(nestedProperties)
+    nestedProperties = 'Value.' + str(rule.prop)
     properties = nestedProperties.split('.')
     for prop in properties:
         try:
             json = json[prop]
         except KeyError:
-            return 'The following property was not found - "%s"' % nestedProperties
+            return rule.value  # Property type not found
         except TypeError:
-            return 'The following property was not found - "%s"' % nestedProperties
+            return rule.value  # Property type not found
     return str(json)
