b871bff2f3fd235402393335b76d30f50d7da970
TaeHun Kang <taehkang@amazon.com>
2020-06-26 19:02:32 -0700

    Initial commit - NoEcho Rule

---
+++ src/cfnlint/rules/resources/NoEcho.py
@@ -0,0 +1,43 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules import RuleMatch
+from ast import literal_eval
+
+
+class NoEcho(CloudFormationLintRule):
+    """Check maximum Resource limit"""
+    id = 'W9919' #To Be Assigned
+    shortdesc = 'Check for NoEcho References'
+    description = 'Check if there is a NoEcho enabled parameter referenced within a resource'
+    source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/dne.html'
+    tags = ['resources', 'NoEcho']
+
+    def match(self, cfn):
+        matches = []
+        no_echo_params = []
+        parameters = cfn.get_parameters()
+
+        for parameter_name, parameter_value in parameters.items():
+            parameter_properties = literal_eval(str(parameter_value))
+            for parameter_properties_name, parameter_properties_value in parameter_properties.items():
+                if parameter_properties_name == 'NoEcho' and parameter_properties_value is True:
+                    no_echo_params.append(parameter_name)
+
+        resource_properties = cfn.get_resources()
+
+        for resource_name, resource_values in resource_properties.items():
+
+            resource = literal_eval(str(resource_values))
+            for key, value in resource.items():
+                if key == 'Metadata':
+                    metadata = literal_eval(str(value))
+                    for prop_name, prop_value in metadata.items():
+                        path = ['Resources', resource_name, 'Metadata', prop_name]
+                        matches.append(RuleMatch(path, 'As the resource "metadata" section contains reference to a '
+                                                       '"NoEcho" parameter, CFN will display the parameters '
+                                                       'description in plaintext'))
+
+        return matches
