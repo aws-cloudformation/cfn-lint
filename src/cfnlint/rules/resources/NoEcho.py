12d091a0d74a4079af2a2c6e655abc0a15135283
Kevin DeJong <kevin.dejong@gmail.com>
2020-07-16 11:57:32 -0500

    Update rule W4002 to only look at Ref and Sub

--- src/cfnlint/rules/resources/NoEcho.py
+++ src/cfnlint/rules/resources/NoEcho.py
@@ -2,6 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
+import six
 from cfnlint.helpers import bool_compare
 from cfnlint.rules import CloudFormationLintRule
 from cfnlint.rules import RuleMatch
@@ -14,34 +15,58 @@ class NoEcho(CloudFormationLintRule):
     source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/parameters-section-structure.html#parameters-section-structure-properties'
     tags = ['resources', 'NoEcho']
 
-    def match(self, cfn):
-        matches = []
+    def _get_no_echo_params(self, cfn):
+        """ Get no Echo Params"""
         no_echo_params = []
-        parameters = cfn.get_parameters()
-        for parameter_name, parameter_value in parameters.items():
+        for parameter_name, parameter_value in cfn.get_parameters().items():
             noecho = parameter_value.get('NoEcho', default=False)
             if bool_compare(noecho, True):
                 no_echo_params.append(parameter_name)
 
-        if not no_echo_params:
-            return no_echo_params
+        return no_echo_params
 
-        resource_properties = cfn.get_resources()
-        resource_dict = {key: resource_properties[key] for key in resource_properties if
-                         isinstance(resource_properties[key], dict)}
-        for resource_name, resource_values in resource_dict.items():
-            resource_values = {key: resource_values[key] for key in resource_values if
-                               isinstance(resource_values[key], dict)}
-            metadata = resource_values.get('Metadata', {})
-            if metadata is not None:
-                for prop_name, properties in metadata.items():
-                    if isinstance(properties, dict):
-                        for property_value in properties.values():
-                            for param in no_echo_params and no_echo_params:
-                                if str(property_value).find(str(param)) > -1:
-                                    path = ['Resources', resource_name, 'Metadata', prop_name]
-                                    matches.append(RuleMatch(path, 'As the resource "metadata" section contains '
-                                                                   'reference to a "NoEcho" parameter ' + str(param)
-                                                             + ', CloudFormation will display the parameter value in '
-                                                               'plaintext'))
+    def _check_ref(self, cfn, no_echo_params):
+        """ Check Refs """
+        matches = []
+        refs = cfn.search_deep_keys('Ref')
+        for ref in refs:
+            if ref[-1] in no_echo_params:
+                if len(ref) > 3:
+                    if ref[0] == 'Resources' and ref[2] == 'Metadata':
+                        matches.append(RuleMatch(ref, 'As the resource "metadata" section contains ' +
+                                                 'reference to a "NoEcho" parameter ' +
+                                                 str(ref[-1]) +
+                                                 ', CloudFormation will display the parameter value in ' +
+                                                 'plaintext'))
+
+        return matches
+
+    def _check_sub(self, cfn, no_echo_params):
+        """ Check Subs """
+        matches = []
+        subs = cfn.search_deep_keys('Fn::Sub')
+        for sub in subs:
+            if isinstance(sub[-1], six.string_types):
+                params = cfn.get_sub_parameters(sub[-1])
+                for param in params:
+                    if param in no_echo_params:
+                        if len(sub) > 2:
+                            if sub[0] == 'Resources' and sub[2] == 'Metadata':
+
+                                matches.append(RuleMatch(sub[:-1], 'As the resource "metadata" section contains ' +
+                                                         'reference to a "NoEcho" parameter ' +
+                                                         str(param) +
+                                                         ', CloudFormation will display the parameter value in ' +
+                                                         'plaintext'))
+
+        return matches
+
+    def match(self, cfn):
+        matches = []
+        no_echo_params = self._get_no_echo_params(cfn)
+        if not no_echo_params:
+            return matches
+        matches.extend(self._check_ref(cfn, no_echo_params))
+        matches.extend(self._check_sub(cfn, no_echo_params))
+
         return matches
