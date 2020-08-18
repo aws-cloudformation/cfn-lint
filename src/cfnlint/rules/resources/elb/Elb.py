09132aa4f924e4f896877bc4bf12a1cb72a22ec7
Pat Myron <PatMyron@users.noreply.github.com>
2020-08-18 12:01:45 -0700

    AWS::ElasticLoadBalancingV2::LoadBalancer.LoadBalancerAttribute routing.http.desync_mitigation_mode (#1660)
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-elasticloadbalancingv2-loadbalancer-loadbalancerattributes.html#cfn-elasticloadbalancingv2-loadbalancer-loadbalancerattributes-key

--- src/cfnlint/rules/resources/elb/Elb.py
+++ src/cfnlint/rules/resources/elb/Elb.py
@@ -89,6 +89,7 @@ HTTPS has certificate HTTP has no certificate'
             ],
             'application': [
                 'idle_timeout.timeout_seconds',
+                'routing.http.desync_mitigation_mode',
                 'routing.http.drop_invalid_header_fields.enabled',
                 'routing.http2.enabled'
             ],
