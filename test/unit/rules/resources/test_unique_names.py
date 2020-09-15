01dad53f66297a267f86be02c0783bcf5bb0646c
Pat Myron <PatMyron@users.noreply.github.com>
2020-09-15 13:33:57 -0700

    E3007: new rule for unique resource and parameter names (#1698)
    * E3007: new rule for unique resource and parameter names

---
+++ test/unit/rules/resources/test_unique_names.py
@@ -0,0 +1,18 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from test.unit.rules import BaseRuleTestCase
+from cfnlint.rules.resources.UniqueNames import UniqueNames  # pylint: disable=E0401
+
+
+class TestUniqueNames(BaseRuleTestCase):
+    def setUp(self):
+        super(TestUniqueNames, self).setUp()
+        self.collection.register(UniqueNames())
+
+    def test_file_positive(self):
+        self.helper_file_positive()
+
+    def test_file_negative(self):
+        self.helper_file_negative('test/fixtures/templates/bad/resources/uniqueNames.yaml', 1)
