32afec66f57692565469b04c24569cb357b9ca0d
Pat Myron <pamyron@amazon.com>
2020-09-29 19:12:16 -0700

    link source code in rule documentation
    closes https://github.com/aws-cloudformation/cfn-python-lint/issues/1641
    
    also included experimental rules in main table with asterisks
    
    relative links work online and locally in editors

--- src/cfnlint/maintenance.py
+++ src/cfnlint/maintenance.py
@@ -7,6 +7,7 @@ import json
 import logging
 import multiprocessing
 import os
+import subprocess
 import jsonpatch
 import cfnlint
 from cfnlint.helpers import get_url_content, url_has_newer_version
@@ -125,40 +126,17 @@ def update_documentation(rules):
             '| Rule ID  | Title | Description | Config<br />(Name:Type:Default) | Source | Tags |\n')
         new_file.write('| -------- | ----- | ----------- | ---------- | ------ | ---- |\n')
 
-        rule_output = '| {0}<a name="{0}"></a> | {1} | {2} | {3} | [Source]({4}) | {5} |\n'
+        rule_output = '| [{0}<a name="{0}"></a>]({6}) | {1} | {2} | {3} | [Source]({4}) | {5} |\n'
 
-        # Add system Errors (hardcoded)
-        for error in [cfnlint.rules.ParseError(), cfnlint.rules.TransformError(), cfnlint.rules.RuleError()]:
-            tags = ','.join('`{0}`'.format(tag) for tag in error.tags)
-            new_file.write(rule_output.format(error.id, error.shortdesc, error.description, '', '', tags))
-
-        # Separate the experimental rules
-        experimental_rules = []
-
-        for rule in sorted_rules:
-
-            if rule.experimental:
-                experimental_rules.append(rule)
-                continue
-
+        for rule in [cfnlint.rules.ParseError(), cfnlint.rules.TransformError(), cfnlint.rules.RuleError()] + sorted_rules:
+            file = '../' + subprocess.check_output(["git", "grep", "-l", "id = '" + rule.id + "'", "src/cfnlint/rules/"]).decode('ascii').strip()
+            rule_id = rule.id + '*' if rule.experimental else rule.id
             tags = ','.join('`{0}`'.format(tag) for tag in rule.tags)
             config = '<br />'.join('{0}:{1}:{2}'.format(key, values.get('type'), values.get('default'))
                                    for key, values in rule.config_definition.items())
-            new_file.write(rule_output.format(rule.id, rule.shortdesc,
-                                              rule.description, config, rule.source_url, tags))
-
-        # Output the experimental rules (if any)
-        if experimental_rules:
-            new_file.write('### Experimental rules\n')
-            new_file.write('| Rule ID  | Title | Description | Source | Tags |\n')
-            new_file.write('| -------- | ----- | ----------- | ------ | ---- |\n')
-
-            for rule in experimental_rules:
-                tags = ','.join('`{0}`'.format(tag) for tag in rule.tags)
-                config = '<br />'.join('{0}:{1}:{2}'.format(key, values.get('type'), values.get('default'))
-                                       for key, values in rule.config_definition.items())
-                new_file.write(rule_output.format(rule.id, rule.shortdesc,
-                                                  rule.description, config, rule.source_url, tags))
+            new_file.write(rule_output.format(rule_id, rule.shortdesc,
+                                              rule.description, config, rule.source_url, tags, file))
+        new_file.write('\n\\* experimental rules\n')
 
 
 def patch_spec(content, region):
