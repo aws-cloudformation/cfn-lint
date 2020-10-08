a706916456cce302ae8bc7e5c1e95e9b763efaff
Pat Myron <pamyron@amazon.com>
2020-10-08 13:53:09 -0700

    CloudFormation Registry resource schema support
    https://aws.amazon.com/blogs/aws/cloudformation-update-cli-third-party-resource-support-registry/
    https://github.com/aws-cloudformation/aws-cloudformation-resource-schema/
    https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/resource-type-schemas.html

---
+++ src/cfnlint/rules/resources/ResourceSchema.py
@@ -0,0 +1,20 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.helpers import load_resource
+from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules import RuleMatch
+from cfnlint.data import CloudformationSchema
+
+class MyNewRule(CloudFormationLintRule):
+    id = 'E3000'
+    shortdesc = ''
+    description = ''
+    source_url = ''
+    tags = []
+
+    def match(self, cfn):
+        matches = []
+        print(load_resource(CloudformationSchema, 'aws-logs-loggroup.json'))
+        return matches
