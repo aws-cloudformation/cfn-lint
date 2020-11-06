79d4c8ae4c9c67a3ffe2010ae7c55c2a8868c1ad
Kevin DeJong <kddejong@amazon.com>
2020-11-06 08:49:42 -0800

    Support numbers, boolean when doing a Sub parameter value

--- test/unit/rules/functions/test_sub.py
+++ test/unit/rules/functions/test_sub.py
@@ -23,4 +23,4 @@ class TestRulesSub(BaseRuleTestCase):
 
     def test_file_negative(self):
         """Test failure"""
-        self.helper_file_negative('test/fixtures/templates/bad/functions/sub.yaml', 15)
+        self.helper_file_negative('test/fixtures/templates/bad/functions/sub.yaml', 16)
