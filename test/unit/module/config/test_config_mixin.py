33ec3403a6ef34066cac6a4e577e861f015aa161
John Newbigin <john.newbigin@rea-group.com>
2020-12-01 23:44:51 +1100

    Add Sid validation

--- test/unit/module/config/test_config_mixin.py
+++ test/unit/module/config/test_config_mixin.py
@@ -158,4 +158,4 @@ class TestConfigMixIn(BaseTestCase):
         # test defaults
         self.assertNotIn(
             'test/fixtures/templates/bad/resources/iam/resource_policy.yaml', config.templates)
-        self.assertEqual(len(config.templates), 4)
+        self.assertEqual(len(config.templates), 6)
