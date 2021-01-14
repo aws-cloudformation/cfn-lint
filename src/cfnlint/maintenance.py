5cea4f039547aa80f13a7eae540454eba942ca38
Kevin DeJong <kddejong@amazon.com>
2021-01-14 07:38:31 -0800

    Switch to add registry schema as part of udpate-specs

--- src/cfnlint/maintenance.py
+++ src/cfnlint/maintenance.py
@@ -9,6 +9,13 @@ import multiprocessing
 import os
 import subprocess
 import jsonpatch
+import zipfile
+import re
+from io import BytesIO, TextIOWrapper
+try:
+    from urllib.request import urlopen, Request
+except ImportError:
+    from urllib2 import urlopen
 import cfnlint
 from cfnlint.helpers import get_url_content, url_has_newer_version
 from cfnlint.helpers import SPEC_REGIONS
@@ -17,6 +24,8 @@ import cfnlint.data.AdditionalSpecs
 
 
 LOGGER = logging.getLogger(__name__)
+
+REGISTRY_SCHEMA_ZIP = 'https://schema.cloudformation.us-east-1.amazonaws.com/CloudformationSchema.zip'
 
 
 def update_resource_specs():
@@ -33,6 +42,7 @@ def update_resource_specs():
         for region, url in SPEC_REGIONS.items():
             update_resource_spec(region, url)
 
+
 def update_resource_spec(region, url):
     """ Update a single resource spec """
     filename = os.path.join(os.path.dirname(cfnlint.__file__), 'data/CloudSpecs/%s.json' % region)
@@ -47,12 +57,35 @@ def update_resource_spec(region, url):
 
     spec_content = get_url_content(url, caching=True)
 
-    multiprocessing_logger.debug('A more recent version of %s was found, and will be downloaded to %s', url, filename)
+    multiprocessing_logger.debug(
+        'A more recent version of %s was found, and will be downloaded to %s', url, filename)
     spec = json.loads(spec_content)
 
     # Patch the files
     spec = patch_spec(spec, 'all')
     spec = patch_spec(spec, region)
+
+    # Patch from registry schema
+    schema_cache = get_schema_value_types()
+    # do each patch individually so we can ignore errors
+    for patch in schema_cache:
+        try:
+            # since there could be patched in values to ValueTypes
+            # Ref/GetAtt as an example.  So we want to add new
+            # ValueTypes that don't exist
+            path_details = patch.get('path').split("/")
+            if path_details[1] == 'ValueTypes':
+                if not spec.get('ValueTypes').get(path_details[2]):
+                    spec['ValueTypes'][path_details[2]] = {}
+
+            # do the patch
+            jsonpatch.JsonPatch([patch]).apply(spec, in_place=True)
+        except jsonpatch.JsonPatchConflict:
+            LOGGER.debug('Patch (%s) not applied in region %s', patch, region)
+        except jsonpatch.JsonPointerException:
+            # Debug as the parent element isn't supported in the region
+            LOGGER.debug('Parent element not found for patch (%s) in region %s',
+                         patch, region)
 
     botocore_cache = {}
 
@@ -65,8 +98,10 @@ def update_resource_spec(region, url):
                     service = '/'.join(service_and_type[:-1])
                     botocore_type = service_and_type[-1]
                     if service not in botocore_cache:
-                        botocore_cache[service] = json.loads(get_url_content('https://raw.githubusercontent.com/boto/botocore/master/botocore/data/' + service + '/service-2.json'))
-                    new_obj['AllowedValues'] = sorted(botocore_cache[service]['shapes'][botocore_type]['enum'])
+                        botocore_cache[service] = json.loads(get_url_content(
+                            'https://raw.githubusercontent.com/boto/botocore/master/botocore/data/' + service + '/service-2.json'))
+                    new_obj['AllowedValues'] = sorted(
+                        botocore_cache[service]['shapes'][botocore_type]['enum'])
                 else:
                     new_obj[key] = search_and_replace_botocore_types(value)
             return new_obj
@@ -83,6 +118,7 @@ def update_resource_spec(region, url):
 
     with open(filename, 'w') as f:
         json.dump(spec, f, indent=2, sort_keys=True, separators=(',', ': '))
+
 
 def update_documentation(rules):
     # Update the overview of all rules in the linter
@@ -125,7 +161,8 @@ def update_documentation(rules):
         rule_output = '| [{0}<a name="{0}"></a>]({6}) | {1} | {2} | {3} | [Source]({4}) | {5} |\n'
 
         for rule in [cfnlint.rules.ParseError(), cfnlint.rules.TransformError(), cfnlint.rules.RuleError()] + sorted_rules:
-            rule_source_code_file = '../' + subprocess.check_output(['git', 'grep', '-l', "id = '" + rule.id + "'", 'src/cfnlint/rules/']).decode('ascii').strip()  # pylint: disable=invalid-string-quote
+            rule_source_code_file = '../' + subprocess.check_output(['git', 'grep', '-l', "id = '" + rule.id + "'", 'src/cfnlint/rules/']).decode(
+                'ascii').strip()  # pylint: disable=invalid-string-quote
             rule_id = rule.id + '*' if rule.experimental else rule.id
             tags = ','.join('`{0}`'.format(tag) for tag in rule.tags)
             config = '<br />'.join('{0}:{1}:{2}'.format(key, values.get('type'), values.get('default'))
@@ -186,3 +223,150 @@ def update_iam_policies():
 
     with open(filename, 'w') as f:
         json.dump(content, f, indent=2, sort_keys=True, separators=(',', ': '))
+
+
+def get_schema_value_types():
+
+    def resolve_refs(properties, schema):
+        results = {}
+        name = None
+
+        if properties.get('$ref'):
+            name = properties.get('$ref').split("/")[-1]
+            subname, results = resolve_refs(schema.get('definitions').get(name), schema)
+            if subname:
+                name = subname
+            properties = schema.get('definitions').get(name)
+
+        if properties.get('type') == 'array':
+            results = properties.get('items')
+
+        if results:
+            properties = results
+
+        if results and results.get('$ref'):
+            name, results = resolve_refs(results, schema)
+
+        if not results:
+            return name, properties
+
+        return name, results
+
+    def get_object_details(names, properties, schema):
+        results = {}
+        for propname, propdetails in properties.items():
+            subname, propdetails = resolve_refs(propdetails, schema)
+            t = propdetails.get('type')
+            if not t:
+                continue
+            if t in ['object']:
+                if subname is None:
+                    subname = propname
+                if propdetails.get('properties'):
+                    if subname not in names:
+                        results.update(get_object_details(
+                            names + [subname], propdetails.get('properties'), schema))
+                elif propdetails.get('oneOf') or propdetails.get('anyOf') or propdetails.get('allOf'):
+                    LOGGER.info(
+                        "Type %s object for %s has only oneOf,anyOf, or allOf properties", names[0], propname)
+                    continue
+            elif t not in ['string', 'integer', 'number', 'boolean']:
+                if propdetails.get('$ref'):
+                    results.update(get_object_details(
+                        names + [propname], schema.get('definitions').get(t.get('$ref').split("/")[-1]), schema))
+                elif isinstance(t, list):
+                    LOGGER.info("Type for %s object and %s property is a list", names[0], propname)
+                else:
+                    LOGGER.info("Unable to handle %s object for %s property", names[0], propname)
+            elif t == 'string':
+                if not results.get('.'.join(names + [propname])):
+                    if propdetails.get('pattern') or (propdetails.get('minLength') and propdetails.get('maxLength')) or propdetails.get('enum'):
+                        results['.'.join(names + [propname])] = {}
+                if propdetails.get('pattern'):
+                    p = propdetails.get('pattern')
+                    try:
+                        re.compile(p, re.UNICODE)
+                        results['.'.join(names + [propname])].update({
+                            'AllowedPatternRegex': p
+                        })
+                    except:
+                        LOGGER.info(
+                            "Unable to handle regex for type %s and property %s with regex %s", names[0], propname, p)
+                if propdetails.get('minLength') and propdetails.get('maxLength'):
+                    results['.'.join(names + [propname])].update({
+                        'StringMin': propdetails.get('minLength'),
+                        'StringMax': propdetails.get('maxLength'),
+                    })
+                if propdetails.get('enum'):
+                    results['.'.join(names + [propname])].update({
+                        'AllowedValues': propdetails.get('enum')
+                    })
+            elif t in ['number', 'integer']:
+                if not results.get('.'.join(names + [propname])):
+                    if propdetails.get('minimum') and propdetails.get('maximum'):
+                        results['.'.join(names + [propname])] = {}
+                if propdetails.get('minimum') and propdetails.get('maximum'):
+                    results['.'.join(names + [propname])].update({
+                        'NumberMin': propdetails.get('minimum'),
+                        'NumberMax': propdetails.get('maximum'),
+                    })
+
+        return results
+
+    def process_schema(schema):
+        results = get_object_details([schema.get('typeName')], schema.get('properties'), schema)
+
+        # Remove duplicates
+        vtypes = {}
+        for n, v in results.items():
+            if n.count('.') > 1:
+                s = n.split('.')
+                vtypes[s[0] + '.' + '.'.join(s[-2:])] = v
+            else:
+                vtypes[n] = v
+
+        valuetypes = []
+        propvalues = []
+        for n, v in vtypes.items():
+            if v:
+                for s, vs in v.items():
+                    element = {
+                        'op': 'add',
+                        'path': '/ValueTypes/%s/%s' % (n, s),
+                        'value': vs,
+                    }
+                    valuetypes.append(element)
+                if n.count('.') == 2:
+                    element = {
+                        'op': 'add',
+                        'path': '/PropertyTypes/%s/Properties/%s/Value' % (n.split('.')[0], n.split('.')[1]),
+                        'value': {
+                            'ValueType': n,
+                        },
+                    }
+                else:
+                    element = {
+                        'op': 'add',
+                        'path': '/ResourceTypes/%s/Properties/%s/Value' % (n.split('.')[0], n.split('.')[1]),
+                        'value': {
+                            'ValueType': n,
+                        },
+                    }
+                propvalues.append(element)
+
+        return valuetypes, propvalues
+
+    req = Request(REGISTRY_SCHEMA_ZIP)
+    res = urlopen(req)
+
+    results = []
+
+    with zipfile.ZipFile(BytesIO(res.read())) as z:
+        for f in z.namelist():
+            with z.open(f) as d:
+                schema = json.load(d)
+                fvaluetypes, fpropvalues = process_schema(schema)
+                results.extend(fvaluetypes)
+                results.extend(fpropvalues)
+
+    return results
