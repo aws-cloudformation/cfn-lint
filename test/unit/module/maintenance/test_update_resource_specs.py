eb0939f0174cb717d74e9f852789305bc98becb2
Kevin DeJong <kddejong@amazon.com>
2021-01-14 15:25:40 -0800

    update tests for registy types

--- test/unit/module/maintenance/test_update_resource_specs.py
+++ test/unit/module/maintenance/test_update_resource_specs.py
@@ -4,9 +4,14 @@ SPDX-License-Identifier: MIT-0
 """
 import sys
 import logging
+import zipfile
 from test.testlib.testcase import BaseTestCase
-from mock import patch, MagicMock
+from mock import patch, MagicMock, Mock
 import cfnlint.maintenance
+try:
+    from urllib.request import urlopen, Request
+except ImportError:
+    from urllib2 import urlopen
 
 LOGGER = logging.getLogger('cfnlint.maintenance')
 LOGGER.addHandler(logging.NullHandler())
@@ -19,7 +24,8 @@ class TestUpdateResourceSpecs(BaseTestCase):
     @patch('cfnlint.maintenance.json.dump')
     @patch('cfnlint.maintenance.patch_spec')
     @patch('cfnlint.maintenance.SPEC_REGIONS', {'us-east-1': 'http://foo.badurl'})
-    def test_update_resource_spec(self, mock_patch_spec, mock_json_dump, mock_content, mock_url_newer_version):
+    @patch('cfnlint.maintenance.urlopen')
+    def test_update_resource_spec(self, mock_urlopen, mock_patch_spec, mock_json_dump, mock_content, mock_url_newer_version):
         """Success update resource spec"""
 
         mock_url_newer_version.return_value = True
@@ -35,10 +41,16 @@ class TestUpdateResourceSpecs(BaseTestCase):
                 'ResourceTypes': {},
                 'ValueTypes': {
                     'AWS::EC2::Instance.Types': [
-                        'm2.medium']
+                        'm2.medium'],
                 }
             },
         ]
+
+        mock_urlresponse = Mock()
+        with open("test/fixtures/registry/schema.zip", 'rb') as f:
+            byte = f.read()
+            mock_urlresponse.read.side_effect = [byte]
+            mock_urlopen.return_value = mock_urlresponse
 
         if sys.version_info.major == 3:
             builtin_module_name = 'builtins'
@@ -54,7 +66,21 @@ class TestUpdateResourceSpecs(BaseTestCase):
                     'ValueTypes': {
                         'AWS::EC2::Instance.Types': [
                             'm2.medium'
-                        ]
+                        ],
+                        'AWS::Lambda::CodeSigningConfig.AllowedPublishers.SigningProfileVersionArns': {
+                            'AllowedPatternRegex': 'arn:(aws[a-zA-Z0-9-]*):([a-zA-Z0-9\\-])+:([a-z]{2}(-gov)?-[a-z]+-\\d{1})?:(\\d{12})?:(.*)',
+                            'StringMin': 12,
+                            'StringMax': 1024
+                        },
+                        'AWS::Lambda::CodeSigningConfig.CodeSigningPolicies.UntrustedArtifactOnDeployment': {
+                            'AllowedValues': ['Warn', 'Enforce']
+                        },
+                        'AWS::Lambda::CodeSigningConfig.CodeSigningConfigId': {
+                            'AllowedPatternRegex': 'csc-[a-zA-Z0-9-_\\.]{17}'
+                        },
+                        'AWS::Lambda::CodeSigningConfig.CodeSigningConfigArn': {
+                            'AllowedPatternRegex': 'arn:(aws[a-zA-Z-]*)?:lambda:[a-z]{2}((-gov)|(-iso(b?)))?-[a-z]+-\\d{1}:\\d{12}:code-signing-config:csc-[a-z0-9]{17}'
+                        }
                     }
                 },
                 mock_builtin_open.return_value.__enter__.return_value,
