e51ec129cc6eb2d5280d9597032cca253d158aca
Kevin DeJong <kddejong@amazon.com>
2021-03-11 08:46:00 -0800

    Osaka was upgraded from local to a full region

--- scripts/update_specs_from_pricing.py
+++ scripts/update_specs_from_pricing.py
@@ -23,7 +23,7 @@ region_map = {
     'Africa (Cape Town)': 'af-south-1',
     'Asia Pacific (Hong Kong)': 'ap-east-1',
     'Asia Pacific (Mumbai)': 'ap-south-1',
-    'Asia Pacific (Osaka-Local)': 'ap-northeast-3',
+    'Asia Pacific (Osaka)': 'ap-northeast-3',
     'Asia Pacific (Seoul)': 'ap-northeast-2',
     'Asia Pacific (Singapore)': 'ap-southeast-1',
     'Asia Pacific (Sydney)': 'ap-southeast-2',
