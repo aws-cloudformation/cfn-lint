14d5d416e1172563f3453e977cf3d711bf61c7dc
Kevin DeJong <kddejong@amazon.com>
2021-05-26 14:52:48 -0700

    Ignore key issues when using Fn::Transform (#2017)

--- src/cfnlint/rules/mappings/KeyName.py
+++ src/cfnlint/rules/mappings/KeyName.py
@@ -35,7 +35,7 @@ class KeyName(CloudFormationLintRule):
         if not isinstance(key, six.string_types):
             message = 'Mapping key ({0}) has to be a string.'
             matches.append(RuleMatch(path[:], message.format(key)))
-        elif not re.match('^[a-zA-Z0-9.-]{1,255}$', key):
+        elif not re.match('^[a-zA-Z0-9.-]{1,255}$', key) and key != 'Fn::Transform':
             message = 'Mapping key ({0}) has invalid name. Name has to be alphanumeric, \'-\' or \'.\''
             matches.append(RuleMatch(path[:], message.format(key)))
 
