5d15a270eff1c4ddf33c251435a519a7ee6abd49
Nhien Ton <nhienton07@gmail.com>
2021-07-07 10:35:27 -0400

    Created new rule to validate module properties against schema.json with jsonschema

--- src/cfnlint/rules/modules/ModuleSchema.py
+++ src/cfnlint/rules/modules/ModuleSchema.py
@@ -2,35 +2,33 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-import re
 import json
-from jsonschema import validate, ValidationError, SchemaError
-from cfnlint.helpers import REGEX_DYN_REF, PSEUDOPARAMS, FN_PREFIX, UNCONVERTED_SUFFIXES, REGISTRY_SCHEMAS
+from jsonschema import validate, ValidationError
 from cfnlint.rules import CloudFormationLintRule
 from cfnlint.rules import RuleMatch
+from cfnlint.helpers import MODULE_SCHEMAS
 
-class ModuleSchema(object):
-    id = 'E3000'
-    shortdesc = 'Module schema'
+class ModuleSchema(CloudFormationLintRule):
+    id = 'E5002'
+    shortdesc = 'Check that Module properties are valid'
     description = 'CloudFormation Registry module schema validation'
     source_url = 'https://github.com/aws-cloudformation/aws-cloudformation-resource-schema/'
-    tags = ['resources']
+    tags = ['resources', 'modules']
 
-    def __init__(self, template, path):
-        self.path = path
-        self.template = template
-
-    def match(self):
+    def match(self, cfn):
         matches = []
-        f = open(self.path + '/schema.json', 'r')
-        schema = json.loads(json.loads(f.read())['Schema'])
-        name = schema['typeName']
-        for resource_name, resource_values in self.template.get_resources([name]).items():
-            properties = resource_values.get('Properties', {})
-            parameters = schema['properties']['Parameters']
-            try:
-                validate(properties, parameters)
-            except ValidationError as e:
-                matches.append(RuleMatch(['Modules', resource_name, 'Properties'], e.message))
-                raise e
-        return matches
+        for path in MODULE_SCHEMAS:
+            f = open(path + '/schema.json', 'r')
+            schema = json.loads(json.loads(f.read())['Schema'])
+            name = schema['typeName']
+            for resource_name, resource_values in cfn.get_resources([name]).items():
+                properties = resource_values.get('Properties', {})
+                parameters = schema['properties']['Parameters']
+                try:
+                    validate(properties, parameters)
+                except ValidationError as e:
+                    path = ['Resources', resource_name, 'Properties']
+                    for element in e.path:
+                        path.append(element)
+                    matches.append(RuleMatch(path, e.message))
+            return matches
