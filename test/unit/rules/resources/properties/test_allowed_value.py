ac3ac0f2dc6a61891f5cb6ae4cf1dcf00f4e2b26
Kevin DeJong <kddejong@amazon.com>
2022-03-07 08:57:12 -0800

    Remove Config resource type allowed values

--- test/unit/rules/resources/properties/test_allowed_value.py
+++ test/unit/rules/resources/properties/test_allowed_value.py
@@ -24,4 +24,4 @@ class TestAllowedValue(BaseRuleTestCase):
     def test_file_negative(self):
         """Test failure"""
         self.helper_file_negative(
-            'test/fixtures/templates/bad/resources/properties/allowed_values.yaml', 218)
+            'test/fixtures/templates/bad/resources/properties/allowed_values.yaml', 216)
