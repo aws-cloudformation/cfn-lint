31545bcfc38b82a7c907bf33c07f80bb621de7e3
Pat Myron <PatMyron@users.noreply.github.com>
2022-05-26 10:22:10 -0700

    flag previous generation instance types (#2267)
    * flag previous generation instance types
    
    https://github.com/aws-cloudformation/cfn-lint/issues/2213
    https://aws.amazon.com/ec2/previous-generation/
    https://aws.amazon.com/rds/previous-generation/
    https://aws.amazon.com/elasticache/previous-generation/
    https://docs.aws.amazon.com/opensearch-service/latest/developerguide/supported-instance-types.html
    https://leaderboard.vantage.sh/

---
+++ test/unit/rules/resources/test_previous_generation_instance_type.py
@@ -0,0 +1,18 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules.resources.PreviousGenerationInstanceType import PreviousGenerationInstanceType  # pylint: disable=E0401
+from .. import BaseRuleTestCase
+
+
+class TestPreviousGenerationInstanceType(BaseRuleTestCase):
+    def setUp(self):
+        super(TestPreviousGenerationInstanceType, self).setUp()
+        self.collection.register(PreviousGenerationInstanceType())
+
+    def test_file_positive(self):
+        self.helper_file_positive()
+
+    def test_file_negative(self):
+        self.helper_file_negative('test/fixtures/templates/bad/previous_generation_instances.yaml', 4)
