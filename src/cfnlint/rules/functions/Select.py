3f3eb042f460f5c653e18aa7e293598dcd7137c6
Malik <60721392+MalikAtalla-AWS@users.noreply.github.com>
2022-06-10 04:39:00 -0700

    Fix a couple of rule descriptions (#2284)

--- src/cfnlint/rules/functions/Select.py
+++ src/cfnlint/rules/functions/Select.py
@@ -10,7 +10,7 @@ class Select(CloudFormationLintRule):
     """Check if Select values are correct"""
     id = 'E1017'
     shortdesc = 'Select validation of parameters'
-    description = 'Making sure the function not is of list'
+    description = 'Making sure the Select function is properly configured'
     source_url = 'https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-select.html'
     tags = ['functions', 'select']
 
