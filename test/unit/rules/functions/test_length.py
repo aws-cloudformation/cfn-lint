d6c7d81db483217493c15860fd37771cc982ba19
Malik Atalla <atalmali@amazon.com>
2022-06-28 14:26:49 +0000

    LTT-LI-62 Add validations for Fn::Length

--- test/unit/rules/functions/test_length.py
+++ test/unit/rules/functions/test_length.py
@@ -7,7 +7,6 @@ from cfnlint.rules.functions.Length import Length
 
 
 class TestRulesLength(BaseRuleTestCase):
-    """Test Rules Get Att """
 
     def setUp(self):
         super(TestRulesLength, self).setUp()
@@ -19,5 +18,8 @@ class TestRulesLength(BaseRuleTestCase):
     def test_file_positive(self):
         self.helper_file_positive()
 
-    def test_file_negative(self):
+    def test_file_negative_missing_transform(self):
         self.helper_file_negative('test/fixtures/templates/bad/functions/lengthWithoutTransform.yaml', 1)
+
+    def test_file_negative_wrong_types(self):
+        self.helper_file_negative('test/fixtures/templates/bad/functions/lengthWrongTypes.yaml', 7)
