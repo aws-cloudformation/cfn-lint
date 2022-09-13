f8e95c2b9007a425c07c9b931d2623288afc5bf8
Adam Johnson <me@adamj.eu>
2022-09-13 21:49:41 +0100

    Support Python 3.10 (#2365)

--- setup.py
+++ setup.py
@@ -70,5 +70,6 @@ that could potentially be improved'),
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
