7acc54587ec1c6d0ae5a7c04849c138a70fa27a2
Kevin DeJong <kddejong@amazon.com>
2022-10-10 11:58:27 -0700

    Allow over ride of location in rule matching

---
+++ test/unit/module/rule/test_matching.py
@@ -0,0 +1,30 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from typing import Any
+from test.testlib.testcase import BaseTestCase
+from cfnlint.rules import CloudFormationLintRule, Match, RuleMatch, matching
+
+
+class rule(CloudFormationLintRule):
+    id: str = 'ETest'
+    pass
+
+rule_match = RuleMatch([], 'Message', rule=rule(), location=(0, 0, 0, 0))
+
+class TestMatching(BaseTestCase):
+    """Test Matching Wrapper"""
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test(self: Any, *args: Any, **kwargs: Any):
+        return [rule_match]
+
+    def test_matching_location(self):
+
+        f = matching('test')
+        r = f(self.test)
+        t = r(self, '', None)
+        self.assertEqual(t, [Match(1, 1, 1, 1, '', rule(), 'Message', rule_match)])
