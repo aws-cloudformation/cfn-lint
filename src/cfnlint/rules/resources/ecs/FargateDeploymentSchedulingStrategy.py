8d68ccda50cacab675c1d2dd49eee6ceabffbe11
Jiati Le <lejiati@amazon.com>
2023-01-18 13:26:55 -0500

    Validate scheduling strategy for Fargate services

---
+++ src/cfnlint/rules/resources/ecs/FargateDeploymentSchedulingStrategy.py
@@ -0,0 +1,28 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule, RuleMatch
+
+
+class FargateDeploymentSchedulingStrategy(CloudFormationLintRule):
+    id = "E3044"
+    shortdesc = "Check Fargate service scheduling strategy"
+    description = "Check that Fargate service scheduling strategy is REPLICA"
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ecs-service.html#cfn-ecs-service-schedulingstrategy"
+    tags = ["properties", "ecs", "service", "container", "fargate"]
+
+    def match(self, cfn):
+        matches = []
+        ecs_services = cfn.get_resource_properties(["AWS::ECS::Service"])
+
+        for ecs_service in ecs_services:
+            path = ecs_service["Path"]
+            properties = ecs_service["Value"]
+            if isinstance(properties, dict):
+                if properties.get("LaunchType", None) != "Fargate":
+                    continue
+                if properties.get("SchedulingStrategy", None) != "REPLICA":
+                    error_message = f"Fargate service only support REPLICA as scheduling strategy at {'/'.join(map(str, path))}"
+                    matches.append(RuleMatch(path, error_message))
+        return matches
