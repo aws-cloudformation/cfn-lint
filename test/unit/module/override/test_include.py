fd2cb8a6f3c93f46b951362dca5d201fb6700971
Kevin DeJong <kddejong@amazon.com>
2023-02-13 08:34:37 -0800

    Convert to using CloudFormation provider schemas

--- test/unit/module/override/test_include.py
+++ test/unit/module/override/test_include.py
@@ -5,10 +5,11 @@ SPDX-License-Identifier: MIT-0
 import json
 from test.testlib.testcase import BaseTestCase
 
-import cfnlint.helpers
 from cfnlint.rules import RulesCollection
 from cfnlint.rules.resources.Configuration import Configuration  # pylint: disable=E0401
 from cfnlint.runner import Runner
+from cfnlint.schema.manager import PROVIDER_SCHEMA_MANAGER
+from cfnlint.schema.patch import SchemaPatch
 
 
 class TestInclude(BaseTestCase):
@@ -18,21 +19,22 @@ class TestInclude(BaseTestCase):
         """Setup"""
         self.collection = RulesCollection()
         self.collection.register(Configuration())
+        self.region = "us-east-1"
 
     def tearDown(self):
         """Tear Down"""
         # Reset the Spec override to prevent other tests to fail
-        cfnlint.helpers.initialize_specs()
+        PROVIDER_SCHEMA_MANAGER.reset()
 
     def test_fail_run(self):
         """Failure test required"""
         filename = "test/fixtures/templates/bad/override/include.yaml"
         template = self.load_template(filename)
 
-        with open("test/fixtures/templates/override_spec/include.json") as fp:
-            custom_spec = json.load(fp)
-        cfnlint.helpers.set_specs(custom_spec)
+        PROVIDER_SCHEMA_MANAGER.patch(
+            "test/fixtures/templates/override_spec/include.json", regions=[self.region]
+        )
 
-        bad_runner = Runner(self.collection, filename, template, ["us-east-1"], [])
+        bad_runner = Runner(self.collection, filename, template, [self.region], [])
         errs = bad_runner.run()
         self.assertEqual(2, len(errs))
