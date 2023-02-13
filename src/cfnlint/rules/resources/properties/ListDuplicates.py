fa73bc22da4ee6072191131c6df2195026cb08eb
Kevin DeJong <kddejong@amazon.com>
2023-02-13 08:34:37 -0800

    Convert to using CloudFormation provider schemas

--- src/cfnlint/rules/resources/properties/ListDuplicates.py
+++ src/cfnlint/rules/resources/properties/ListDuplicates.py
@@ -3,11 +3,11 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-import hashlib
-import json
+import itertools
+from typing import Mapping, Sequence
 
-from cfnlint.helpers import RESOURCE_SPECS
-from cfnlint.rules import CloudFormationLintRule, RuleMatch
+from cfnlint.jsonschema import ValidationError
+from cfnlint.rules import CloudFormationLintRule
 
 
 class ListDuplicates(CloudFormationLintRule):
@@ -21,106 +21,65 @@ class ListDuplicates(CloudFormationLintRule):
     )
     source_url = "https://github.com/aws-cloudformation/cfn-python-lint/blob/main/docs/cfn-resource-specification.md#allowedvalue"
     tags = ["resources", "property", "list"]
+    child_rules = {
+        "I3037": None,
+    }
 
-    def initialize(self, cfn):
-        """Initialize the rule"""
-        for resource_type_spec in RESOURCE_SPECS.get(cfn.regions[0]).get(
-            "ResourceTypes"
-        ):
-            self.resource_property_types.append(resource_type_spec)
-        for property_type_spec in RESOURCE_SPECS.get(cfn.regions[0]).get(
-            "PropertyTypes"
-        ):
-            self.resource_sub_property_types.append(property_type_spec)
+    def _unbool(self, element, true=object(), false=object()):
+        if element is True:
+            return true
+        if element is False:
+            return false
+        return element
 
-    def _check_duplicates(self, values, path, scenario=None):
-        """Check for Duplicates"""
-        matches = []
+    def _mapping_equal(self, one, two):
+        if len(one) != len(two):
+            return False
+        return all(
+            key in two and self._equal(value, two[key]) for key, value in one.items()
+        )
 
-        list_items = []
-        if isinstance(values, list):
-            for index, value in enumerate(values):
-                value_hash = hashlib.sha1(
-                    json.dumps(value, sort_keys=True, default=str).encode("utf-8")
-                ).hexdigest()
-                if value_hash in list_items:
-                    if not scenario:
-                        message = "List has a duplicate value at {0}"
-                        matches.append(
-                            RuleMatch(
-                                path + [index],
-                                message.format("/".join(map(str, path + [index]))),
-                            )
-                        )
-                    else:
-                        scenario_text = " and ".join(
-                            [f'condition "{k}" is {v}' for (k, v) in scenario.items()]
-                        )
-                        message = f'List has a duplicate value at {"/".join(map(str, path))} when {scenario_text}'
-                        matches.append(RuleMatch(path, message))
+    def _sequence_equal(self, one, two):
+        if len(one) != len(two):
+            return False
+        return all(self._equal(i, j) for i, j in zip(one, two))
 
-                list_items.append(value_hash)
+    def _equal(self, one, two):
+        if isinstance(one, str) or isinstance(two, str):
+            return one == two
+        if isinstance(one, Sequence) and isinstance(two, Sequence):
+            return self._sequence_equal(one, two)
+        if isinstance(one, Mapping) and isinstance(two, Mapping):
+            return self._mapping_equal(one, two)
+        return self._unbool(one) == self._unbool(two)
 
-        return matches
+    def _uniq(self, container):
+        try:
+            sort = sorted(self._unbool(i) for i in container)
+            sliced = itertools.islice(sort, 1, None)
 
-    def check_duplicates(self, values, path, cfn):
-        """Check for duplicates"""
-        matches = []
+            for i, j in zip(sort, sliced):
+                if self._equal(i, j):
+                    return False
 
-        if isinstance(values, list):
-            matches.extend(self._check_duplicates(values, path))
-        elif isinstance(values, dict):
-            props = cfn.get_object_without_conditions(values)
-            for prop in props:
-                matches.extend(
-                    self._check_duplicates(
-                        prop.get("Object"), path, prop.get("Scenario")
-                    )
+        except (NotImplementedError, TypeError):
+            seen = []
+            for e in container:
+                e = self._unbool(e)
+                for i in seen:
+                    if self._equal(i, e):
+                        return False
+
+                seen.append(e)
+        return True
+
+    # pylint: disable=unused-argument
+    def uniqueItems(self, validator, uI, instance, schema):
+        if validator.is_type(instance, "array") and not self._uniq(instance):
+            if uI:
+                yield ValidationError(f"{instance!r} has non-unique elements")
+            else:
+                yield ValidationError(
+                    f"{instance!r} has non-unique elements",
+                    rule=self.child_rules["I3037"],
                 )
-
-        return matches
-
-    def check(self, cfn, properties, value_specs, path):
-        """Check itself"""
-        matches = []
-        for p_value, p_path in properties.items_safe(path[:]):
-            for prop in p_value:
-                if prop in value_specs:
-                    property_type = value_specs.get(prop).get("Type")
-                    duplicates_allowed = value_specs.get(prop).get(
-                        "DuplicatesAllowed", True
-                    )
-                    if property_type == "List" and not duplicates_allowed:
-                        matches.extend(
-                            self.check_duplicates(p_value[prop], p_path + [prop], cfn)
-                        )
-
-        return matches
-
-    def match_resource_sub_properties(self, properties, property_type, path, cfn):
-        """Match for sub properties"""
-        matches = []
-
-        specs = (
-            RESOURCE_SPECS.get(cfn.regions[0])
-            .get("PropertyTypes")
-            .get(property_type, {})
-            .get("Properties", {})
-        )
-        matches.extend(self.check(cfn, properties, specs, path))
-
-        return matches
-
-    def match_resource_properties(self, properties, resource_type, path, cfn):
-        """Check CloudFormation Properties"""
-        matches = []
-
-        specs = (
-            RESOURCE_SPECS.get(cfn.regions[0])
-            .get("ResourceTypes")
-            .get(resource_type, {})
-            .get("Properties", {})
-        )
-        matches.extend(self.check(cfn, properties, specs, path))
-
-        return matches
