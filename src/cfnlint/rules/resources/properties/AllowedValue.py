afc5f0c216db4aafce5dab7356b122b0e6ff0cf6
Kevin DeJong <kddejong@amazon.com>
2023-02-13 08:34:37 -0800

    Convert to using CloudFormation provider schemas

--- src/cfnlint/rules/resources/properties/AllowedValue.py
+++ src/cfnlint/rules/resources/properties/AllowedValue.py
@@ -2,10 +2,8 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-import json
-
-from cfnlint.helpers import RESOURCE_SPECS
-from cfnlint.rules import CloudFormationLintRule, RuleMatch
+from cfnlint.jsonschema import ValidationError
+from cfnlint.rules import CloudFormationLintRule
 
 
 class AllowedValue(CloudFormationLintRule):
@@ -16,107 +14,28 @@ class AllowedValue(CloudFormationLintRule):
     description = "Check if properties have a valid value in case of an enumator"
     source_url = "https://github.com/aws-cloudformation/cfn-python-lint/blob/main/docs/cfn-resource-specification.md#allowedvalue"
     tags = ["resources", "property", "allowed value"]
+    child_rules = {
+        "W2030": None,
+    }
 
-    def initialize(self, cfn):
-        """Initialize the rule"""
-        for resource_type_spec in RESOURCE_SPECS.get(cfn.regions[0]).get(
-            "ResourceTypes"
-        ):
-            self.resource_property_types.append(resource_type_spec)
-        for property_type_spec in RESOURCE_SPECS.get(cfn.regions[0]).get(
-            "PropertyTypes"
-        ):
-            self.resource_sub_property_types.append(property_type_spec)
+    def _unbool(self, element, true=object(), false=object()):
+        if element is True:
+            return true
+        if element is False:
+            return false
+        return element
 
-    def check_value(self, value, path, property_name, **kwargs):
-        """Check Value"""
-        matches = []
-        allowed_value_specs = kwargs.get("value_specs", {}).get("AllowedValues", {})
-
-        if allowed_value_specs:
-            # Ignore values with dynamic references. Simple check to prevent false-positives
-            # See: https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/dynamic-references.html
-            if "{{resolve:" not in str(value):
-                # Always compare the allowed value as a string, strict typing is not of concern for this rule
-                if str(value) not in allowed_value_specs:
-                    message = "You must specify a valid value for {0} ({1}). {2}"
-                    description = f"Valid values are {json.dumps(allowed_value_specs)}"
-                    matches.append(
-                        RuleMatch(
-                            path, message.format(property_name, value, description)
-                        )
-                    )
-
-        return matches
-
-    def check(self, cfn, properties, value_specs, property_specs, path):
-        """Check itself"""
-        matches = []
-        for p_value, p_path in properties.items_safe(path[:]):
-            for prop in p_value:
-                if prop in value_specs:
-                    value = value_specs.get(prop).get("Value", {})
-                    if value:
-                        value_type = value.get("ValueType", "")
-                        property_type = (
-                            property_specs.get("Properties").get(prop).get("Type")
-                        )
-                        value_type_details = (
-                            RESOURCE_SPECS.get(cfn.regions[0])
-                            .get("ValueTypes")
-                            .get(value_type, {})
-                        )
-                        if value_type_details == "CACHED":
-                            value_type_details = (
-                                RESOURCE_SPECS.get("us-east-1")
-                                .get("ValueTypes")
-                                .get(value_type, {})
-                            )
-                        matches.extend(
-                            cfn.check_value(
-                                p_value,
-                                prop,
-                                p_path,
-                                check_value=self.check_value,
-                                value_specs=value_type_details,
-                                cfn=cfn,
-                                property_type=property_type,
-                                property_name=prop,
-                            )
-                        )
-
-        return matches
-
-    def match_resource_sub_properties(self, properties, property_type, path, cfn):
-        """Match for sub properties"""
-        matches = []
-
-        specs = (
-            RESOURCE_SPECS.get(cfn.regions[0])
-            .get("PropertyTypes")
-            .get(property_type, {})
-            .get("Properties", {})
-        )
-        property_specs = (
-            RESOURCE_SPECS.get(cfn.regions[0]).get("PropertyTypes").get(property_type)
-        )
-        matches.extend(self.check(cfn, properties, specs, property_specs, path))
-
-        return matches
-
-    def match_resource_properties(self, properties, resource_type, path, cfn):
-        """Check CloudFormation Properties"""
-        matches = []
-
-        specs = (
-            RESOURCE_SPECS.get(cfn.regions[0])
-            .get("ResourceTypes")
-            .get(resource_type, {})
-            .get("Properties", {})
-        )
-        resource_specs = (
-            RESOURCE_SPECS.get(cfn.regions[0]).get("ResourceTypes").get(resource_type)
-        )
-        matches.extend(self.check(cfn, properties, specs, resource_specs, path))
-
-        return matches
+    # pylint: disable=unused-argument
+    def enum(self, validator, enums, instance, schema):
+        if isinstance(instance, dict):
+            if len(instance) == 1:
+                for k, v in instance.items():
+                    if k == "Ref":
+                        yield from self.child_rules["W2030"].validate(v, enums)
+                        return
+        if instance in (0, 1):
+            unbooled = self._unbool(instance)
+            if all(unbooled != self._unbool(each) for each in enums):
+                yield ValidationError(f"{instance!r} is not one of {enums!r}")
+        elif instance not in enums:
+            yield ValidationError(f"{instance!r} is not one of {enums!r}")
--- test/unit/rules/resources/properties/test_properties.py
+++ test/unit/rules/resources/properties/test_properties.py
@@ -2,88 +2,143 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-import json
 from test.unit.rules import BaseRuleTestCase
 
-import cfnlint.core
-import cfnlint.helpers
+from jsonschema import Draft7Validator
+
 from cfnlint.rules.resources.properties.Properties import (
     Properties,  # pylint: disable=E0401
 )
 
 
-class TestResourceProperties(BaseRuleTestCase):
-    """Test Resource Properties"""
+class TestAdditionalProperties(BaseRuleTestCase):
+    """Test Allowed Value Property Configuration"""
 
-    def setUp(self):
-        """Setup"""
-        super(TestResourceProperties, self).setUp()
-        self.collection.register(Properties())
-        self.success_templates = [
-            "test/fixtures/templates/good/resource_properties.yaml",
-            "test/fixtures/templates/good/resources/properties/templated_code.yaml",
-            "test/fixtures/templates/good/resources/properties/properties_nested_if.yaml",
-        ]
-
-    def test_file_positive(self):
+    def test_additional_properties(self):
         """Test Positive"""
-        self.helper_file_positive()
+        rule = Properties()
+        validator = Draft7Validator({})
 
-    def test_file_negative(self):
-        """Test failure"""
-        self.helper_file_negative("test/fixtures/templates/bad/generic.yaml", 9)
+        base_object = {
+            "a": "a",
+            "b": "b",
+        }
 
-    def test_file_negative_2(self):
-        """Failure test"""
-        self.helper_file_negative(
-            "test/fixtures/templates/bad/object_should_be_list.yaml", 4
+        schema = {
+            "type": "object",
+            "properties": {
+                "a": {"type": "string"},
+                "b": {"type": "string"},
+            },
+        }
+
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        False,
+                        base_object,
+                        schema,
+                    )
+                )
+            ),
+            0,
+        )
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        False,
+                        {**base_object, **{"c": "c"}},
+                        schema,
+                    )
+                )
+            ),
+            1,
         )
 
-    def test_file_negative_3(self):
-        """Failure test"""
-        self.helper_file_negative(
-            "test/fixtures/templates/bad/resource_properties.yaml", 8
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        False,
+                        {**base_object, **{"c": "c", "d": "d"}},
+                        schema,
+                    )
+                )
+            ),
+            2,
         )
 
-    def test_E3012_in_bad_template(self):
-        """Test E3012 in known-bad template"""
-        filename = "test/fixtures/templates/bad/resource_properties.yaml"
-        (args, _, _) = cfnlint.core.get_args_filenames(["--template", filename])
-        (template, rules, _) = cfnlint.core.get_template_rules(filename, args)
-        results = cfnlint.core.run_checks(filename, template, rules, ["us-east-1"])
-        matched_rule_ids = [r.rule.id for r in results]
-        self.assertIn("E3012", matched_rule_ids)
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        True,
+                        {**base_object, **{"c": "c"}},
+                        schema,
+                    )
+                )
+            ),
+            0,
+        )
 
-    def test_E3012_match_has_extra_attributes(self):
-        """Test E3012 in has custom attributes"""
-        filename = "test/fixtures/templates/bad/resource_properties.yaml"
-        (args, _, _) = cfnlint.core.get_args_filenames(["--template", filename])
-        (template, rules, _) = cfnlint.core.get_template_rules(filename, args)
-        results = cfnlint.core.run_checks(filename, template, rules, ["us-east-1"])
-        custom_attrs = ["actual_type", "expected_type"]
-        for r in results:
-            if r.rule.id == "E3012":
-                for ca in custom_attrs:
-                    assert hasattr(r, ca), "Attribute {} was not found".format(ca)
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        {
+                            "type": "object",
+                            "properties": {
+                                "c": {
+                                    "type": "string",
+                                },
+                            },
+                        },
+                        {**base_object, **{"c": {"c": "c"}}},
+                        schema,
+                    )
+                )
+            ),
+            0,
+        )
 
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        False,
+                        {**base_object, **{"c": "c"}},
+                        {**schema, **{"patternProperties": "^c$"}},
+                    )
+                )
+            ),
+            0,
+        )
 
-class TestSpecifiedCustomResourceProperties(TestResourceProperties):
-    """Repeat Resource Properties tests with Custom Resource override spec provided"""
-
-    def setUp(self):
-        """Setup"""
-        super(TestSpecifiedCustomResourceProperties, self).setUp()
-        # Add a Spec override that specifies the Custom::SpecifiedCustomResource type
-        with open("test/fixtures/templates/override_spec/custom.json") as fp:
-            custom_spec = json.load(fp)
-        cfnlint.helpers.set_specs(custom_spec)
-        # Reset Spec override after test
-        self.addCleanup(cfnlint.helpers.initialize_specs)
-
-    # ... all TestResourceProperties test cases are re-run with override spec ...
-
-    def test_file_negative_custom(self):
-        """Additional failure test for specified Custom Resource validation"""
-        self.helper_file_negative(
-            "test/fixtures/templates/bad/resources/properties/custom.yaml", 2
+        self.assertEqual(
+            len(
+                list(
+                    rule.additionalProperties(
+                        validator,
+                        False,
+                        {**base_object, **{"c": "c"}},
+                        {
+                            **schema,
+                            **{
+                                "patternProperties": {
+                                    "^d$": {"type": "string"},
+                                }
+                            },
+                        },
+                    )
+                )
+            ),
+            1,
         )
