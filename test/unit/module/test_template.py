54712ce94ee14e7ff73ee6ac402304879d15c04a
Kevin DeJong <kddejong@amazon.com>
2023-02-13 08:34:37 -0800

    Convert to using CloudFormation provider schemas

--- test/unit/module/test_template.py
+++ test/unit/module/test_template.py
@@ -2,11 +2,10 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-import json
 import os
 from test.testlib.testcase import BaseTestCase
 
-from cfnlint.decode import cfn_yaml, convert_dict
+from cfnlint.decode import convert_dict
 from cfnlint.template import Template  # pylint: disable=E0401
 
 
@@ -182,7 +181,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_is_resource_available(self):
         """Test is resource available"""
+<<<<<<< HEAD
         temp_obj = convert_dict(
+=======
+        temp_obj = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Mappings": {"location": {"us-east-1": {"primary": "True"}}},
                 "Conditions": {
@@ -346,7 +349,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_is_resource_not_available(self):
         """Test is resource available"""
+<<<<<<< HEAD
         temp_obj = convert_dict(
+=======
+        temp_obj = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Mappings": {"location": {"us-east-1": {"primary": "True"}}},
                 "Conditions": {
@@ -441,7 +448,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_get_conditions_from_path(self):
         """Test is resource available"""
+<<<<<<< HEAD
         temp_obj = convert_dict(
+=======
+        temp_obj = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Resources": {
                     "AMIIDLookup": {
@@ -543,7 +554,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_failure_get_conditions_from_path(self):
         """Test get conditions from path when things arne't formatted correctly"""
+<<<<<<< HEAD
         temp_obj = convert_dict(
+=======
+        temp_obj = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Resources": {
                     "AMIIDLookup": {
@@ -716,7 +731,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_get_object_without_conditions(self):
         """Test Getting condition names in an object/list"""
+<<<<<<< HEAD
         template = convert_dict(
+=======
+        template = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Conditions": {
                     "useAmiId": {"Fn::Not": [{"Fn::Equals": [{"Ref": "myAmiId"}, ""]}]}
@@ -824,7 +843,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_get_object_without_conditions_for_list(self):
         """Test Getting condition names in an object/list"""
+<<<<<<< HEAD
         template = convert_dict(
+=======
+        template = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Conditions": {
                     "CreateAppVolume": {"Fn::Equals": [{"Ref": "CreateVolums"}, "true"]}
@@ -948,7 +971,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
 
     def test_get_object_without_nested_conditions(self):
         """Test Getting condition names in an object/list"""
+<<<<<<< HEAD
         template = convert_dict(
+=======
+        template = cfnlint.decode.node.convert_dict(
+>>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
             {
                 "Conditions": {
                     "isProduction": {"Fn::Equals": [{"Ref": "myEnvironment"}, "prod"]},
@@ -1103,13 +1130,11 @@ ElasticLoadBalancer -> MyEC2Instance  [color=black, key=0, label=Ref, source_pat
             template.template.get("Resources", {}).get("Test", {}),
             ["Resources", "Test"],
         )
-        self.assertEqual(results, [])
+        self.assertEqual(results, [{"Object": {}, "Scenario": None}])
         results = template.get_object_without_nested_conditions(
             template.template.get("Resources", {}), ["Resources"]
         )
-        self.assertEqual(
-            results, [{"Object": {"Test": {"Ref": "AWS::NoValue"}}, "Scenario": None}]
-        )
+        self.assertEqual(results, [{"Object": {}, "Scenario": None}])
 
     def test_get_condition_scenarios_below_path(self):
         """Test Getting condition names in an object/list"""
