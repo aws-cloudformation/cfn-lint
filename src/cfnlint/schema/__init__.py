c316ed7331e0eb199182cc8ab152577bdd6c9f98
Kevin DeJong <kddejong@amazon.com>
2023-03-20 10:55:50 -0700

    Allow for all getatts

--- src/cfnlint/schema/__init__.py
+++ src/cfnlint/schema/__init__.py
@@ -1,3 +1,4 @@
 from cfnlint.schema.exceptions import ResourceNotFoundError
+from cfnlint.schema.getatts import GetAtt, GetAttType
 from cfnlint.schema.manager import PROVIDER_SCHEMA_MANAGER
 from cfnlint.schema.schema import Schema
