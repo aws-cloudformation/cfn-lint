6a70aeccbd632e642985f96f60cbdc6fd2d14fda
Kevin DeJong <kddejong@amazon.com>
2023-03-27 16:33:51 -0700

    Test format of json files (#2651)

--- scripts/update_serverless_aws_policies.py
+++ scripts/update_serverless_aws_policies.py
@@ -4,26 +4,34 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-import logging
-import boto3
 import json
+import logging
+
+import boto3
 from samtranslator.translator.managed_policy_translator import ManagedPolicyLoader
-LOGGER = logging.getLogger('cfnlint')
+
+LOGGER = logging.getLogger("cfnlint")
 
 
 def main():
     session = boto3.session.Session()
-    client = session.client('iam', region_name='us-east-1')
+    client = session.client("iam", region_name="us-east-1")
 
     policyLoader = ManagedPolicyLoader(client)
     policyLoader.load()
 
-    filename = 'src/cfnlint/data/Serverless/ManagedPolicies.json'
-    with open(filename, 'w+', encoding='utf-8') as f:
-        json.dump(policyLoader._policy_map, f, indent=1, sort_keys=True, separators=(',', ': '))
+    filename = "src/cfnlint/data/Serverless/ManagedPolicies.json"
+    with open(filename, "w+", encoding="utf-8") as f:
+        json.dump(
+            policyLoader._policy_map,
+            f,
+            indent=1,
+            sort_keys=True,
+            separators=(",", ": "),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except (ValueError, TypeError):
