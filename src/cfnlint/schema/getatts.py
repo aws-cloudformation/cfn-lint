04ad0f41cfb885ea2f66fb314c0a2f61692fa39b
Kevin DeJong <kddejong@amazon.com>
2023-04-07 10:54:33 -0700

    V1 - cleaning up more rules (#2670)
    * Add back in logic for rule W2031
    * Validate conditions when checking resource types for a region

--- src/cfnlint/schema/getatts.py
+++ src/cfnlint/schema/getatts.py
@@ -5,8 +5,41 @@ SPDX-License-Identifier: MIT-0
 import enum
 from typing import Any, Dict, Optional
 
-from cfnlint.schema._constants import _all_property_types
 from cfnlint.schema._pointer import resolve_pointer
+
+_all_property_types = [
+    "AWS::Amplify::Branch",
+    "AWS::Amplify::Domain",
+    "AWS::AppSync::DomainName",
+    "AWS::Backup::BackupSelection",
+    "AWS::Backup::BackupVault",
+    "AWS::CodeArtifact::Domain",
+    "AWS::CodeArtifact::Repository",
+    "AWS::EC2::CapacityReservation",
+    "AWS::EC2::Subnet",
+    "AWS::EC2::VPC",
+    "AWS::EFS::MountTarget",
+    "AWS::EKS::Nodegroup",
+    "AWS::ImageBuilder::Component",
+    "AWS::ImageBuilder::ContainerRecipe",
+    "AWS::ImageBuilder::DistributionConfiguration",
+    "AWS::ImageBuilder::ImagePipeline",
+    "AWS::ImageBuilder::ImageRecipe",
+    "AWS::ImageBuilder::InfrastructureConfiguration",
+    "AWS::RDS::DBParameterGroup",
+    "AWS::RoboMaker::RobotApplication",
+    "AWS::RoboMaker::SimulationApplication",
+    "AWS::Route53Resolver::ResolverRule",
+    "AWS::Route53Resolver::ResolverRuleAssociation",
+    "AWS::SNS::Topic",
+    "AWS::SQS::Queue",
+    "AWS::SageMaker::DataQualityJobDefinition",
+    "AWS::SageMaker::ModelBiasJobDefinition",
+    "AWS::SageMaker::ModelExplainabilityJobDefinition",
+    "AWS::SageMaker::ModelQualityJobDefinition",
+    "AWS::SageMaker::MonitoringSchedule",
+    "AWS::StepFunctions::Activity",
+]
 
 
 class GetAttType(enum.Enum):
