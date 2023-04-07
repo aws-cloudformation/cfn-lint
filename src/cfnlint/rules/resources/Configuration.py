bdd34b9bcfd69eb529a79a024fa7a0ec098396a6
Kevin DeJong <kddejong@amazon.com>
2023-04-07 10:54:33 -0700

    V1 - cleaning up more rules (#2670)
    * Add back in logic for rule W2031
    * Validate conditions when checking resource types for a region

--- src/cfnlint/rules/resources/Configuration.py
+++ src/cfnlint/rules/resources/Configuration.py
@@ -32,27 +32,40 @@ class Configuration(CloudFormationLintRule):
             cfn=None,
             rules=None,
         )(schema=schema)
+        self.cfn = None
 
     def initialize(self, cfn):
         super().initialize(cfn)
         self.regions = cfn.regions
+        self.cfn = cfn
 
     # pylint: disable=unused-argument
     def _awsType(self, validator, iT, instance, schema):
-        if not validator.is_type(instance, "string"):
+        resource_type = instance.get("Type")
+        if not validator.is_type(resource_type, "string"):
             return
+
+        resource_condition = instance.get("Condition")
+
         for region in self.regions:
-            if instance in PROVIDER_SCHEMA_MANAGER.get_resource_types(region=region):
-                return
-            if not instance.startswith(
+            if validator.is_type(resource_condition, "string"):
+                if False in self.cfn.conditions.build_scenerios_on_region(
+                    resource_condition, region
+                ):
+                    continue
+            if resource_type in PROVIDER_SCHEMA_MANAGER.get_resource_types(
+                region=region
+            ):
+                continue
+            if not resource_type.startswith(
                 ("Custom::", "AWS::Serverless::")
-            ) and not instance.endswith("::MODULE"):
+            ) and not resource_type.endswith("::MODULE"):
                 yield ValidationError(
-                    f"Resource type `{instance}` does not exist in '{region}'"
+                    f"Resource type `{resource_type}` does not exist in '{region}'"
                 )
 
     # pylint: disable=unused-argument
-    def _check_resource(self, cfn, resource_name, resource_values):
+    def _check_resource(self, resource_name, resource_values):
         """Check Resource"""
         matches = []
 
@@ -97,8 +110,6 @@ class Configuration(CloudFormationLintRule):
                 self.logger.debug(
                     "Validating resource %s base configuration", resource_name
                 )
-                matches.extend(
-                    self._check_resource(cfn, resource_name, resource_values)
-                )
+                matches.extend(self._check_resource(resource_name, resource_values))
 
         return matches
