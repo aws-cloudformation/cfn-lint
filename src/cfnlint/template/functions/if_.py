e7cc67d3b31de4ab6d1b2c1aa59e67f5016901ca
Kevin DeJong <kddejong@amazon.com>
2023-06-13 08:40:50 -0700

    V1 - Filter jsonschema checks for ref novalue (#2760)
    * Handle conditions in object checks
    * Start with ref novalue
    * Setup integration tests
    * Move around context and simplify
    * Switch to pytest

--- src/cfnlint/template/functions/if_.py
+++ src/cfnlint/template/functions/if_.py
@@ -1,8 +1,12 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
 from typing import Any
 
 from cfnlint.template.functions.fn import Fn
 
 
 class FnIf(Fn):
-    def __init__(self, instance: Any) -> None:
+    def __init__(self, instance: Any, template: Any) -> None:
         super().__init__(instance)
