b86048c363d25ea5e57547ff42b46791a01e3451
Kevin DeJong <kddejong@amazon.com>
2023-06-16 10:02:14 -0700

    V1 cleanup/remove/validate ref (#2767)
    * Fix ref rule checks for enum, pattern
    * Test StringSize
    * Drop python 3.7 in v1

--- test/integration/test_mandatory_checks.py
+++ test/integration/test_mandatory_checks.py
@@ -34,7 +34,7 @@ class TestDirectives(BaseCliTestCase):
                         ],
                         "Start": {"ColumnNumber": 7, "LineNumber": 17},
                     },
-                    "Message": "Additional properties are not allowed ('BucketName1' was unexpected)",
+                    "Message": "Additional properties are not allowed ('BucketName1' was unexpected. Did you mean 'BucketName'?)",
                     "Rule": {
                         "Description": (
                             "Making sure that resources properties are properly"
@@ -166,7 +166,7 @@ class TestDirectives(BaseCliTestCase):
                         ],
                         "Start": {"ColumnNumber": 7, "LineNumber": 13},
                     },
-                    "Message": "Additional properties are not allowed ('BucketName1' was unexpected)",
+                    "Message": "Additional properties are not allowed ('BucketName1' was unexpected. Did you mean 'BucketName'?)",
                     "Rule": {
                         "Description": (
                             "Making sure that resources properties are properly"
