b7bc622d556d4b98fa97226700174e05bc9cd669
tal66 <77445020+tal66@users.noreply.github.com>
2023-07-04 00:17:05 +0300

    New rule for Lambda zip deployment (#2682)
    * New rule for Lambda zip deployment
    ---------
    
    Co-authored-by: Kevin DeJong <kddejong@amazon.com>

---
+++ src/cfnlint/rules/resources/lmbd/ZipPackageRequiredProperties.py
@@ -0,0 +1,76 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule, RuleMatch
+
+
+class ZipPackageRequiredProperties(CloudFormationLintRule):
+    id = "W2533"
+    shortdesc = (
+        "Check required properties for Lambda if the deployment package is a .zip file"
+    )
+    description = (
+        "When the package type is Zip, "
+        "you must also specify the `handler` and `runtime` properties."
+    )
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lambda-function.html"
+    tags = ["resources", "lambda"]
+
+    def match(self, cfn):
+        matches = []
+        required_properties = [
+            "Handler",
+            "Runtime",
+        ]  # required if package is a .zip file
+
+        resources = cfn.get_resources(["AWS::Lambda::Function"])
+
+        for resource_name, resource in resources.items():
+            properties = resource.get("Properties")
+            if not isinstance(properties, dict):
+                continue
+
+            for scenario in cfn.get_object_without_conditions(
+                properties, ["PackageType", "Code", "Handler", "Runtime"]
+            ):
+                props = scenario.get("Object")
+                path = ["Resources", resource_name, "Properties"]
+
+                # check is zip deployment
+                is_zip_deployment = True
+                code = props.get("Code")
+
+                if props.get("PackageType") == "Zip":
+                    path.append("PackageType")
+                elif isinstance(code, dict) and (
+                    code.get("ZipFile") or code.get("S3Key")
+                ):
+                    path.append("Code")
+                else:
+                    is_zip_deployment = False
+
+                if not is_zip_deployment:
+                    continue
+
+                # check required properties for zip deployment
+                missing_properties = []
+                for p in required_properties:
+                    if props.get(p) is None:
+                        missing_properties.append(p)
+
+                if len(missing_properties) > 0:
+                    message = "Properties {0} missing for zip file deployment at {1}"
+                    matches.append(
+                        RuleMatch(
+                            path,
+                            message.format(
+                                missing_properties,
+                                "/".join(
+                                    map(str, ["Resources", resource_name, "Properties"])
+                                ),
+                            ),
+                        )
+                    )
+
+        return matches
---
+++ test/unit/rules/resources/lmbd/test_zip_package_required_properties.py
@@ -0,0 +1,29 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from test.unit.rules import BaseRuleTestCase
+
+from cfnlint.rules.resources.lmbd.ZipPackageRequiredProperties import (
+    ZipPackageRequiredProperties,
+)
+
+
+class TestZipPackageRequiredProperties(BaseRuleTestCase):
+    """Test required properties"""
+
+    def setUp(self):
+        super(TestZipPackageRequiredProperties, self).setUp()
+        self.collection.register(ZipPackageRequiredProperties())
+        self.success_templates = [
+            "test/fixtures/templates/good/resources/lambda/required_properties.yaml"
+        ]
+
+    def test_file_positive(self):
+        self.helper_file_positive()
+
+    def test_file_negative(self):
+        self.helper_file_negative(
+            "test/fixtures/templates/bad/resources/lambda/required_properties.yaml",
+            err_count=3,
+        )
