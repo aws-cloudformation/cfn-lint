c2965169bda878ac4bdee3e2373baa3684f081e8
Kevin DeJong <kddejong@amazon.com>
2023-07-26 15:41:26 -0700

    Merge in upstream changes

--- src/cfnlint/rules/functions/GetAtt.py
+++ src/cfnlint/rules/functions/GetAtt.py
@@ -72,68 +72,7 @@ class GetAtt(CloudFormationLintRule):
                         )(schema=validator_schema)
                         err = best_match(validator.iter_errors(instance=v))
 
-<<<<<<< HEAD
-            # only check resname if its set.  if it isn't it is because of bad structure
-            # and an error is already provided
-            if resname:
-                if not isinstance(resname, str):
-                    message = "Invalid GetAtt structure {0} at {1}"
-                    matches.append(
-                        RuleMatch(
-                            getatt,
-                            message.format(getatt[-1], "/".join(map(str, getatt[:-1]))),
-                        )
-                    )
-                    continue
-                if resname in valid_getatts:
-                    if restype is not None:
-                        # Check for maps
-                        restypeparts = restype.split(".")
-                        if (
-                            restypeparts[0] in valid_getatts[resname]
-                            and len(restypeparts) >= 2
-                        ):
-                            if (
-                                valid_getatts[resname][restypeparts[0]].get("Type")
-                                != "Map"
-                            ):
-                                message = "Invalid GetAtt {0}.{1} for resource {2}"
-                                matches.append(
-                                    RuleMatch(
-                                        getatt[:-1],
-                                        message.format(resname, restype, getatt[1]),
-                                    )
-                                )
-                            else:
-                                continue
-                        if (
-                            restype not in valid_getatts[resname]
-                            and "*" not in valid_getatts[resname]
-                        ):
-                            message = "Invalid GetAtt {0}.{1} for resource {2}"
-                            matches.append(
-                                RuleMatch(
-                                    getatt[:-1],
-                                    message.format(resname, restype, getatt[1]),
-                                )
-                            )
-                else:
-                    modules = cfn.get_modules()
-                    if any(resname.startswith(s) for s in modules.keys()):
-                        LOGGER.debug(
-                            "Will consider valid getatt %s because it references a resource within a module",
-                            resname,
-                        )
-                    else:
-                        message = "Invalid GetAtt {0}.{1} for resource {2}"
-                        matches.append(
-                            RuleMatch(
-                                getatt, message.format(resname, restype, getatt[1])
-                            )
-                        )
-=======
                 if err:
                     matches.append(RuleMatch(path=getatt[:-1], message=err.message))
->>>>>>> 83f57c754 (Convert to using CloudFormation provider schemas)
 
         return matches
