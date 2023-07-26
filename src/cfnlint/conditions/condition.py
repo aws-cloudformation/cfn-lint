7dade224bdda3cb5b13bde19dc9978d48a260331
Kevin DeJong <kddejong@amazon.com>
2023-07-26 15:41:26 -0700

    Merge in upstream changes

--- src/cfnlint/conditions/condition.py
+++ src/cfnlint/conditions/condition.py
@@ -3,7 +3,7 @@ Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from typing import Any, Dict, List, Mapping, Optional, Union, Sequence
+from typing import Any, Dict, List, Mapping, Optional, Sequence, Union
 
 from sympy import And, Not, Or, Symbol
 from sympy.logic.boolalg import BooleanFunction
