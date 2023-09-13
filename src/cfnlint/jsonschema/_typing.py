4d6b7a0e2f5ea0d44b768f3db4ec39608ba78686
Kevin DeJong <kddejong@amazon.com>
2023-09-12 19:38:32 -0700

    Provide value based validation when using fns

--- src/cfnlint/jsonschema/_typing.py
+++ src/cfnlint/jsonschema/_typing.py
@@ -4,8 +4,9 @@ SPDX-License-Identifier: MIT-0
 """
 # Code is taken from jsonschema package and adapted CloudFormation use
 # https://github.com/python-jsonschema/jsonschema
-from typing import Any, Callable, Dict, Iterator, Optional
+from typing import Any, Callable, Dict, Iterator, Optional, Union
 
 from cfnlint.jsonschema.exceptions import ValidationError
 
-V = Optional[Callable[[Any, Any, Any, Dict[str, Any]], Iterator[ValidationError]]]
+ValidationResult = Iterator[ValidationError]
+V = Optional[Callable[[Any, Any, Any, Dict[str, Any]], ValidationResult]]
