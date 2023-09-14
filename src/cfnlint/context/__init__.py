2bde7605fa47474be51c7eacdc5844c5cc19d504
Kevin DeJong <kddejong@amazon.com>
2023-09-14 14:29:31 -0700

    Support condition functions

--- src/cfnlint/context/__init__.py
+++ src/cfnlint/context/__init__.py
@@ -1,5 +1,6 @@
 from cfnlint.context.context import (
     Context,
+    create_context_for_conditions,
     create_context_for_outputs,
     create_context_for_resource_properties,
     create_context_for_resources,
