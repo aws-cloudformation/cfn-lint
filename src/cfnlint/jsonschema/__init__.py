5e3478eb61fa2a457455c9ec81e6b1158bd53abe
Kevin DeJong <kddejong@amazon.com>
2023-09-14 14:29:31 -0700

    Support condition functions

--- src/cfnlint/jsonschema/__init__.py
+++ src/cfnlint/jsonschema/__init__.py
@@ -1,4 +1,5 @@
 from cfnlint.jsonschema._resolver import RefResolver
+from cfnlint.jsonschema._typing import ValidationResult
 from cfnlint.jsonschema.exceptions import ValidationError
 from cfnlint.jsonschema.protocols import Validator
 from cfnlint.jsonschema.validators import CfnTemplateValidator, StandardValidator
