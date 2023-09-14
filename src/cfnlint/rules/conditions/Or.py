9e20fabefcf1c852e61d087ff0873e91f990fd11
Kevin DeJong <kddejong@amazon.com>
2023-09-14 14:29:31 -0700

    Support condition functions

--- src/cfnlint/rules/conditions/Or.py
+++ src/cfnlint/rules/conditions/Or.py
@@ -4,7 +4,6 @@ SPDX-License-Identifier: MIT-0
 """
 
 from cfnlint.rules import CloudFormationLintRule
-from cfnlint.rules.conditions.common import check_condition_list
 
 
 class Or(CloudFormationLintRule):
@@ -15,6 +14,3 @@ class Or(CloudFormationLintRule):
     description = "Check Fn::Or is a list of two elements"
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-conditions.html#intrinsic-function-reference-conditions-or"
     tags = ["functions", "or"]
-
-    def match(self, cfn):
-        return check_condition_list(cfn, "Fn::Or")
