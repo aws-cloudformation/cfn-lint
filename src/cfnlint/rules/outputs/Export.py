72f6de3e6691fcaab936438b4f004af41fa327c6
Kevin DeJong <kddejong@amazon.com>
2023-09-27 13:09:51 -0700

    Update specs and fix test errors

---
+++ src/cfnlint/rules/outputs/Export.py
@@ -0,0 +1,29 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.helpers import FUNCTIONS
+from cfnlint.rules import CloudFormationLintRule
+
+
+class Value(CloudFormationLintRule):
+    """Check if Output Export values"""
+
+    id = "E6102"
+    shortdesc = "Outputs have values of strings"
+    description = "Making sure the outputs have strings as values"
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/outputs-section-structure.html"
+    tags = ["outputs"]
+
+    def outputexport(self, validator, tS, instance, schema):
+        validator = validator.evolve(
+            context=validator.context.evolve(
+                resources={},
+                functions=FUNCTIONS,
+            )
+        )
+
+        for err in validator.descend(instance, {"type": "string"}):
+            if not err.validator.startswith("fn"):
+                err.rule = self
+            yield err
