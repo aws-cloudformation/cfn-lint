23c90aa9414b6feeec8b65101cc31e74771ac42f
Kevin DeJong <kddejong@amazon.com>
2023-11-09 08:25:41 -0800

    Update GetAtts for all attributes

--- src/cfnlint/context/context.py
+++ src/cfnlint/context/context.py
@@ -197,7 +197,7 @@ class Parameter(_Ref):
             return
 
         if self.type == "CommaDelimitedList" or self.type.startswith("List<"):
-            self.default = parameter.get("Default").split(",")
+            self.default = parameter.get("Default", "").split(",")
             for allowed_value in parameter.get("AllowedValues", []):
                 self.allowed_values.append(allowed_value.split(","))
         else:
