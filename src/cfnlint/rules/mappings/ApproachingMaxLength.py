ad703928765ce0c1f3826f6437dad83277a78288
Kevin DeJong <kddejong@amazon.com>
2023-11-11 09:17:20 -0800

    Convert mappings to JSON Schema validation

--- src/cfnlint/rules/mappings/ApproachingMaxLength.py
+++ src/cfnlint/rules/mappings/ApproachingMaxLength.py
@@ -4,19 +4,15 @@ SPDX-License-Identifier: MIT-0
 """
 
 from cfnlint.rules import CloudFormationLintRule
-from cfnlint.rules.common import approaching_name_limit
 
 
-class LimitName(CloudFormationLintRule):
+class ApproachingMaxLength(CloudFormationLintRule):
     """Check maximum Mapping name size limit"""
 
-    id = "I7011"
+    id = "I7002"
     shortdesc = "Mapping name limit"
     description = (
         "Check the size of Mapping names in the template is approaching the upper limit"
     )
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
     tags = ["mappings", "limits"]
-
-    def match(self, cfn):
-        return approaching_name_limit(cfn, "Mappings")
---
+++ src/cfnlint/rules/mappings/JsonSchema.py
@@ -0,0 +1,53 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.data.schemas.other import mappings as schema_mappings
+from cfnlint.helpers import REGION_PRIMARY, load_resource
+from cfnlint.jsonschema import CfnTemplateValidator
+from cfnlint.rules.jsonschema.base import BaseJsonSchema
+
+
+class JsonSchema(BaseJsonSchema):
+    """Check Base Outputs Configuration"""
+
+    id = "E7000"
+    shortdesc = "JSON Schema validation of the CloudFormation mappings"
+    description = "Making sure that mappings comply with the JSON schema"
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/mappings-section-structure.html"
+    tags = ["mappings"]
+
+    def __init__(self):
+        """Init"""
+        super().__init__()
+        self.rule_set = {
+            "additionalProperties": "E7001",
+            "patternProperties": "E7001",
+            "properties": "E7001",
+            "propertyNames": "E7002",
+            "maxProperties": "E7010",
+            "minProperties": "E7001",
+            "required": "E7001",
+            "type": "E7001",
+        }
+        self.child_rules = dict.fromkeys(list(self.rule_set.values()))
+        self.regions = [REGION_PRIMARY]
+        self.schema = load_resource(schema_mappings, "configuration.json")
+
+    def match(self, cfn):
+        """Check CloudFormation Outputs"""
+        matches = []
+
+        mappings = cfn.template.get("Mappings", {})
+
+        cfn_validator = self.setup_validator(
+            validator=CfnTemplateValidator,
+            schema=self.schema,
+            context=cfn.context.create_context_for_mappings(cfn.regions[0]),
+        ).evolve(
+            cfn=cfn,
+        )
+
+        matches.extend(self.json_schema_validate(cfn_validator, mappings, ["Outputs"]))
+
+        return matches
