878a7e85fab429c4847e2f01441e53bb21b5c2c2
Kevin DeJong <kddejong@amazon.com>
2023-11-13 10:12:03 -0800

    Add Parameters to JSON Schema validation

---
+++ src/cfnlint/rules/parameters/MaxLength.py
@@ -0,0 +1,18 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+
+
+class MaxLength(CloudFormationLintRule):
+    """Check if maximum Parameter value size limit is exceeded"""
+
+    id = "E2012"
+    shortdesc = "Parameter value limit not exceeded"
+    description = (
+        "Check if the size of Parameter values in the template is less than the upper"
+        " limit"
+    )
+    source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
+    tags = ["parameters", "limits"]
