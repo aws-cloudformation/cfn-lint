bd2ff4e90863697b26ef9c706d8f19e36c4fc27d
Kevin DeJong <kddejong@amazon.com>
2023-11-13 10:12:03 -0800

    Add Parameters to JSON Schema validation

--- src/cfnlint/rules/parameters/Types.py
+++ src/cfnlint/rules/parameters/Types.py
@@ -2,8 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
-from cfnlint.helpers import VALID_PARAMETER_TYPES
-from cfnlint.rules import CloudFormationLintRule, RuleMatch
+from cfnlint.rules import CloudFormationLintRule
 
 
 class Types(CloudFormationLintRule):
@@ -14,22 +13,3 @@ class Types(CloudFormationLintRule):
     description = "Making sure the parameters have a correct type"
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/best-practices.html#parmtypes"
     tags = ["parameters"]
-
-    def match(self, cfn):
-        matches = []
-
-        for paramname, paramvalue in cfn.get_parameters_valid().items():
-            # If the type isn't found we create a valid one
-            # this test isn't about missing required properties for a
-            # parameter.
-            paramtype = paramvalue.get("Type", "String")
-            if paramtype not in VALID_PARAMETER_TYPES:
-                message = "Parameter {0} has invalid type {1}"
-                matches.append(
-                    RuleMatch(
-                        ["Parameters", paramname, "Type"],
-                        message.format(paramname, paramtype),
-                    )
-                )
-
-        return matches
