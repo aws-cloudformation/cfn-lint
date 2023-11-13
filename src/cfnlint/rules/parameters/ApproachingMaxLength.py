d8d7d10262a919856247bb720d9f42208f182613
Kevin DeJong <kddejong@amazon.com>
2023-11-13 10:12:03 -0800

    Add Parameters to JSON Schema validation

--- src/cfnlint/rules/parameters/ApproachingMaxLength.py
+++ src/cfnlint/rules/parameters/ApproachingMaxLength.py
@@ -3,13 +3,12 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 from cfnlint.rules import CloudFormationLintRule
-from cfnlint.rules.common import approaching_name_limit
 
 
-class LimitName(CloudFormationLintRule):
+class ApproachingMaxLength(CloudFormationLintRule):
     """Check maximum Parameter name size limit"""
 
-    id = "I2011"
+    id = "I2012"
     shortdesc = "Parameter name limit"
     description = (
         "Check the size of Parameter names in the template is approaching the upper"
@@ -17,6 +16,3 @@ class LimitName(CloudFormationLintRule):
     )
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
     tags = ["parameters", "limits"]
-
-    def match(self, cfn):
-        return approaching_name_limit(cfn, "Parameters")
