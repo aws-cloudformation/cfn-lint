faf6d5d8ba8b0285bc44c5c5a3f29bd0a4005529
Kevin DeJong <kddejong@amazon.com>
2023-11-15 09:37:32 -0800

    Update json schema MIT licensing

--- src/cfnlint/jsonschema/exceptions.py
+++ src/cfnlint/jsonschema/exceptions.py
@@ -1,9 +1,20 @@
 """
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: MIT-0
+Copyright (c) 2013 Julian Berman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+SPDX-License-Identifier: MIT
 """
 # Code is taken from jsonschema package and adapted CloudFormation use
-# https://github.com/python-jsonschema/jsonschema/blob/main/jsonschema/validators.py
+# https://github.com/python-jsonschema/jsonschema
 from __future__ import annotations
 
 import heapq
