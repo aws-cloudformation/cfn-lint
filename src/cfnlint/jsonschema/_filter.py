31ca2f1a32449b17d1148cd038f124ff37887193
Kevin DeJong <kddejong@amazon.com>
2023-11-15 09:37:32 -0800

    Update json schema MIT licensing

--- src/cfnlint/jsonschema/_filter.py
+++ src/cfnlint/jsonschema/_filter.py
@@ -3,9 +3,6 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 from dataclasses import dataclass, field, fields
-
-# Code is taken from jsonschema package and adapted CloudFormation use
-# https://github.com/python-jsonschema/jsonschema
 from typing import Any, Sequence, Tuple
 
 from cfnlint.helpers import ToPy
