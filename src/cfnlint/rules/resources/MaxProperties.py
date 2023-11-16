ca5470aa23aa98c7fab6f6c4cf46b7fdbd60c4e6
Kevin DeJong <kddejong@amazon.com>
2023-11-16 08:48:10 -0800

    Clean up AWS::CloudFormation::Init validation

--- src/cfnlint/rules/resources/MaxProperties.py
+++ src/cfnlint/rules/resources/MaxProperties.py
@@ -3,11 +3,10 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from cfnlint.rules import CloudFormationLintRule
-from cfnlint.rules.common import number_limit
+from cfnlint.rules.jsonschema.MaxProperties import MaxProperties as ParentMaxProperties
 
 
-class LimitNumber(CloudFormationLintRule):
+class MaxProperties(ParentMaxProperties):
     """Check if maximum Resource limit is exceeded"""
 
     id = "E3010"
@@ -18,5 +17,5 @@ class LimitNumber(CloudFormationLintRule):
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
     tags = ["resources", "limits"]
 
-    def match(self, cfn):
-        return number_limit(cfn, "Resources")
+    def __init__(self) -> None:
+        super().__init__("I3010")
