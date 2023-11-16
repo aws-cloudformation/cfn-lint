d5c906310008769c5abad945c61ca44da28206fd
Kevin DeJong <kddejong@amazon.com>
2023-11-16 08:48:10 -0800

    Clean up AWS::CloudFormation::Init validation

--- src/cfnlint/rules/resources/ApproachingMaxProperties.py
+++ src/cfnlint/rules/resources/ApproachingMaxProperties.py
@@ -4,10 +4,9 @@ SPDX-License-Identifier: MIT-0
 """
 
 from cfnlint.rules import CloudFormationLintRule
-from cfnlint.rules.common import approaching_number_limit
 
 
-class LimitNumber(CloudFormationLintRule):
+class ApproachingMaxProperties(CloudFormationLintRule):
     """Check maximum Resource limit"""
 
     id = "I3010"
@@ -17,6 +16,3 @@ class LimitNumber(CloudFormationLintRule):
     )
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cloudformation-limits.html"
     tags = ["resources", "limits"]
-
-    def match(self, cfn):
-        return approaching_number_limit(cfn, "Resources")
