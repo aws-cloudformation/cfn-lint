85edce0a4ad5d3f079b899fbf0c8545687b56270
Kevin DeJong <kddejong@amazon.com>
2023-11-17 09:24:50 -0800

    Rework rules and remove some backwards compatibility items

--- src/cfnlint/__main__.py
+++ src/cfnlint/__main__.py
@@ -13,48 +13,20 @@ LOGGER = logging.getLogger("cfnlint")
 
 
 def main():
-    if sys.version_info[:2] == (3, 4):
-        warnings.warn(
-            (
-                "Python 3.4 has reached end of life. "
-                "cfn-lint has ended support for python 3.4 as of July 1st, 2020."
-            ),
-            Warning,
-            stacklevel=3,
-        )
-    elif sys.version_info[:2] == (3, 5):
-        warnings.warn(
-            (
-                "Python 3.5 has reached end of life. "
-                "cfn-lint has ended support for python 3.5 as of August 1st, 2021."
-            ),
-            Warning,
-            stacklevel=3,
-        )
-    elif sys.version_info[:2] == (2, 7):
-        warnings.warn(
-            (
-                "Python 2.7 has reached end of life. "
-                "cfn-lint will end support for python 2.7 on June 1st, 2020."
-            ),
-            Warning,
-            stacklevel=3,
-        )
-
     try:
-        (args, filenames, formatter) = cfnlint.core.get_args_filenames(sys.argv[1:])
-        matches = list(cfnlint.core.get_matches(filenames, args))
+        (config, filenames, formatter) = cfnlint.core.get_args_filenames(sys.argv[1:])
+        matches = list(cfnlint.core.get_matches(filenames, config))
         rules = cfnlint.core.get_used_rules()
         matches_output = formatter.print_matches(matches, rules, filenames)
 
         if matches_output:
-            if args.output_file:
-                with open(args.output_file, "w", encoding="utf-8") as output_file:
+            if config.output_file:
+                with open(config.output_file, "w", encoding="utf-8") as output_file:
                     output_file.write(matches_output)
             else:
                 print(matches_output)
 
-        return cfnlint.core.get_exit_code(matches, args.non_zero_exit_code)
+        return cfnlint.core.get_exit_code(matches, config.non_zero_exit_code)
     except cfnlint.core.CfnLintExitException as e:
         LOGGER.error(str(e))
         return e.exit_code
