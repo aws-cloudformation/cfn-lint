d1d828d0dfd0053b0caebb7a613e864214772081
Kevin DeJong <kddejong@amazon.com>
2023-12-05 15:04:51 -0800

    Update specs to 23/12/5

---
+++ src/cfnlint/data/schemas/patches/providers/all/aws_cloudtrail_eventdatastore/__init__.py
@@ -0,0 +1,15 @@
+[
+    {
+        "op": "test",
+        "path": "/createOnlyProperties",
+        "value": [
+            "/properties/Tags",
+            "/properties/ExperimentOptions/properties/AccountTargeting",
+        ],
+    },
+    {
+        "op": "replace",
+        "path": "/createOnlyProperties",
+        "value": ["/properties/Tags", "/properties/ExperimentOptions/AccountTargeting"],
+    },
+]
