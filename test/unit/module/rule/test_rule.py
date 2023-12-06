f42a7d8e8f8c65b2f41d6901c8f33c6918c67b58
Kevin DeJong <kddejong@amazon.com>
2023-12-06 15:24:02 -0800

    Increase rules file test coverage

--- test/unit/module/rule/test_rule.py
+++ test/unit/module/rule/test_rule.py
@@ -50,6 +50,21 @@ class TestCloudFormationRule(BaseTestCase):
                     "testBoolean": {"default": True, "type": "boolean"},
                     "testString": {"default": "default", "type": "string"},
                     "testInteger": {"default": 1, "type": "integer"},
+                    "testListBoolean": {
+                        "type": "list",
+                        "itemtype": "boolean",
+                        "default": [False],
+                    },
+                    "testListString": {
+                        "type": "list",
+                        "itemtype": "string",
+                        "default": ["bar"],
+                    },
+                    "testListInteger": {
+                        "type": "list",
+                        "itemtype": "integer",
+                        "default": [0],
+                    },
                 }
                 self.configure()
 
@@ -62,9 +77,72 @@ class TestCloudFormationRule(BaseTestCase):
         self.assertTrue(config.get("testBoolean"))
         self.assertEqual(config.get("testString"), "default")
         self.assertEqual(config.get("testInteger"), 1)
+        self.assertListEqual(config.get("testListBoolean"), [False])
+        self.assertListEqual(config.get("testListString"), ["bar"])
+        self.assertListEqual(config.get("testListInteger"), [0])
 
-        rule.configure({"testBoolean": "false", "testString": "new", "testInteger": 2})
+        rule.configure(
+            {
+                "testBoolean": "false",
+                "testString": "new",
+                "testInteger": 2,
+                "testListBoolean": ["true"],
+                "testListString": ["foo"],
+                "testListInteger": [1],
+            }
+        )
 
         self.assertFalse(config.get("testBoolean"))
         self.assertEqual(config.get("testString"), "new")
         self.assertEqual(config.get("testInteger"), 2)
+        self.assertListEqual(config.get("testListBoolean"), [True])
+        self.assertListEqual(config.get("testListString"), ["foo"])
+        self.assertListEqual(config.get("testListInteger"), [1])
+
+    def test_experimental(self):
+        """Test Configuration"""
+
+        class TestRule(CloudFormationLintRule):
+            """Def Rule"""
+
+            id = "E1000"
+            shortdesc = "Test Rule"
+            description = "Test Rule"
+            source_url = "https://github.com/aws-cloudformation/cfn-python-lint/"
+            tags = ["resources"]
+
+            def __init__(self):
+                """Init"""
+                super(TestRule, self).__init__()
+                self.config_definition = {
+                    "experimental": {"default": True, "type": "boolean"},
+                }
+                self.configure()
+
+            def get_config(self):
+                """Get the Config"""
+                return self.config
+
+        rule = TestRule()
+        rule.configure({}, True)
+        config = rule.get_config()
+        self.assertTrue(config.get("experimental"))
+
+        rule.configure({}, False)
+        config = rule.get_config()
+        self.assertFalse(config.get("experimental"))
+
+    def test_non_configured_experimental(self):
+        class TestRule(CloudFormationLintRule):
+            """Def Rule"""
+
+            id = "E1000"
+            shortdesc = "Test Rule"
+            description = "Test Rule"
+            source_url = "https://github.com/aws-cloudformation/cfn-python-lint/"
+            tags = ["resources"]
+
+        rule = TestRule()
+
+        rule.configure({}, True)
+        self.assertFalse(rule.config.get("experimental"))
