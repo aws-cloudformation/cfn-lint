93ec48f5fa3d7d654581197f9f62628b07ac4f14
Kevin DeJong <kddejong@amazon.com>
2023-12-07 12:23:28 -0800

    Maintain backwards compatibility with API

--- test/unit/module/test_api.py
+++ test/unit/module/test_api.py
@@ -9,6 +9,8 @@ from unittest import TestCase
 
 from cfnlint import lint, lint_all
 from cfnlint.config import ManualArgs
+from cfnlint.core import get_rules
+from cfnlint.helpers import REGIONS
 
 
 class TestLint(TestCase):
@@ -16,11 +18,11 @@ class TestLint(TestCase):
         self, filename=None, config: ManualArgs | None = None
     ):
         with open(filename, "r") as f:
-            return list(lint(f.read(), config=config))
+            return lint(f.read(), config=config)
 
     def helper_lint_string_from_file_all(self, filename):
         with open(filename, "r") as f:
-            return list(lint_all(f.read()))
+            return lint_all(f.read())
 
     def test_noecho_yaml_template(self):
         filename = "test/fixtures/templates/bad/noecho.yaml"
@@ -104,3 +106,52 @@ class TestLint(TestCase):
         filename = "test/fixtures/templates/good/transform/list_transform_many.yaml"
         matches = self.helper_lint_string_from_file(filename)
         self.assertEqual([], matches, f"Got matches: {matches!r}")
+
+
+class TestV0Usage(TestCase):
+    def helper_lint_string_from_file(
+        self,
+        filename,
+        rules=get_rules([], [], ["I", "W", "E"], include_experimental=True),
+        regions=REGIONS,
+    ):
+        with open(filename, "r") as f:
+            return lint(f.read(), rules, regions)
+
+    def test_noecho_yaml_template(self):
+        filename = "test/fixtures/templates/bad/noecho.yaml"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual(["W4002", "W4002"], [match.rule.id for match in matches])
+
+    def test_noecho_yaml_template_warnings_ignored(self):
+        filename = "test/fixtures/templates/bad/noecho.yaml"
+        rules = get_rules([], ["W", "I"], [])
+        matches = self.helper_lint_string_from_file(filename, rules=rules)
+        self.assertEqual([], matches)
+
+    def test_duplicate_json_template(self):
+        filename = "test/fixtures/templates/bad/duplicate.json"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual(
+            ["E0000", "E0000", "E0000"], [match.rule.id for match in matches]
+        )
+
+    def test_invalid_yaml_template(self):
+        filename = "test/fixtures/templates/bad/core/config_invalid_yaml.yaml"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual(["E0000"], [match.rule.id for match in matches])
+
+    def test_invalid_json_template(self):
+        filename = "test/fixtures/templates/bad/core/config_invalid_json.json"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual(["E0000"], [match.rule.id for match in matches])
+
+    def test_issues_template(self):
+        filename = "test/fixtures/templates/bad/issues.yaml"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual(["E1020"], [match.rule.id for match in matches])
+
+    def test_sam_template(self):
+        filename = "test/fixtures/templates/good/transform/list_transform_many.yaml"
+        matches = self.helper_lint_string_from_file(filename)
+        self.assertEqual([], matches)
