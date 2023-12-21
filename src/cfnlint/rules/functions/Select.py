be380a91db7a275c158a23979d76ba57a73835bb
Kevin DeJong <kddejong@amazon.com>
2023-12-21 11:29:23 -0800

    V1 - Cleanup some of the function rules (#2983)
    * Cleanup rules and reduce redundant code
    * Cleanup Fn::Cidr function
    * Add errors to resolvers when values can't be resolved
    * Increase test coverage

--- src/cfnlint/rules/functions/Select.py
+++ src/cfnlint/rules/functions/Select.py
@@ -14,13 +14,3 @@ class Select(CloudFormationLintRule):
     description = "Making sure the Select function is properly configured"
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-select.html"
     tags = ["functions", "select"]
-    supported_functions = [
-        "Fn::FindInMap",
-        "Fn::GetAtt",
-        "Fn::GetAZs",
-        "Fn::If",
-        "Fn::Split",
-        "Fn::Cidr",
-        "Fn::Select",  # issue: 2895
-        "Ref",
-    ]
