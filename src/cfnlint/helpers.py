d3bf236b5bfda8701285e9e35cc0f649282d5ded
Kevin DeJong <kddejong@amazon.com>
2023-12-22 08:56:12 -0800

    Update specs for 2023/12/22

--- src/cfnlint/helpers.py
+++ src/cfnlint/helpers.py
@@ -41,11 +41,19 @@ AVAILABILITY_ZONES = {
     ],
     "ap-northeast-3": ["ap-northeast-3a", "ap-northeast-3b", "ap-northeast-3c"],
     "ap-south-1": ["ap-south-1a", "ap-south-1b", "ap-south-1c"],
-    # "ap-south-2",  # no schemas yet
+    "ap-south-2": [
+        "ap-south-2a",
+        "ap-south-2b",
+        "ap-south-2c",
+    ],
     "ap-southeast-1": ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"],
     "ap-southeast-2": ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"],
     "ap-southeast-3": ["ap-southeast-3a", "ap-southeast-3b", "ap-southeast-3c"],
-    # "ap-southeast-4",  no schemas yet
+    "ap-southeast-4": [
+        "ap-southeast-4a",
+        "ap-southeast-4b",
+        "ap-southeast-4c",
+    ],
     "ca-west-1": [
         "ca-west-1a",
         "ca-west-1b",
@@ -60,15 +68,27 @@ AVAILABILITY_ZONES = {
     "cn-north-1": ["cn-north-1a", "cn-north-1b", "cn-north-1c"],
     "cn-northwest-1": ["cn-northwest-1a", "cn-northwest-1b", "cn-northwest-1c"],
     "eu-central-1": ["eu-central-1a", "eu-central-1b", "eu-central-1c"],
-    # "eu-central-2",  no schemas yet
+    "eu-central-2": [
+        "eu-central-2a",
+        "eu-central-2b",
+        "eu-central-2c",
+    ],
     "eu-north-1": ["eu-north-1a", "eu-north-1b", "eu-north-1c"],
     "eu-south-1": ["eu-south-1a", "eu-south-1b", "eu-south-1c"],
-    # "eu-south-2",  no schemas yet
+    "eu-south-2": [
+        "eu-south-2a",
+        "eu-south-2b",
+        "eu-south-2c",
+    ],
     "eu-west-1": ["eu-west-1a", "eu-west-1b", "eu-west-1c"],
     "eu-west-2": ["eu-west-2a", "eu-west-2b", "eu-west-2c"],
     "eu-west-3": ["eu-west-3a", "eu-west-3b", "eu-west-3c"],
     "me-south-1": ["me-south-1a", "me-south-1b", "me-south-1c"],
-    # "me-central-1",  no schemas yet
+    "me-central-1": [
+        "me-central-1a",
+        "me-central-1b",
+        "me-central-1c",
+    ],
     "sa-east-1": ["sa-east-1a", "sa-east-1b", "sa-east-1c"],
     "us-east-1": [
         "us-east-1a",
