a69ee275a5ec8beda530f41aabbaa1831f373a0b
Kevin DeJong <kddejong@amazon.com>
2024-01-04 13:37:45 -0800

    V1 iam/policy/validation (#2997)
    * Clean up IAM policy validation
    * New rules for required xor and required at least one
    * New rule for props are specified none or one time
    * Improve testing of new validators

---
+++ src/cfnlint/rules/resources/properties/RequiredXor.py
@@ -0,0 +1,15 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+
+
+class RequiredXor(CloudFormationLintRule):
+    """Check Required Resource Configuration"""
+
+    id = "E3014"
+    shortdesc = "Validate only one of a set of required properties are specified"
+    description = "Making sure that Resources properties that are required exist"
+    source_url = "https://github.com/aws-cloudformation/cfn-python-lint/blob/main/docs/cfn-resource-specification.md#required"
+    tags = ["resources"]
