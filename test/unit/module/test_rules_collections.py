c831d510edf0e0b9aa0d6034afa89b5df32b67b8
Kevin DeJong <kddejong@amazon.com>
2024-01-04 13:37:45 -0800

    V1 iam/policy/validation (#2997)
    * Clean up IAM policy validation
    * New rules for required xor and required at least one
    * New rule for props are specified none or one time
    * Improve testing of new validators

--- test/unit/module/test_rules_collections.py
+++ test/unit/module/test_rules_collections.py
@@ -54,7 +54,7 @@ class TestRulesCollection(BaseTestCase):
         filename = "test/fixtures/templates/bad/generic.yaml"
         template = cfnlint.decode.cfn_yaml.load(filename)
         cfn = Template(filename, template, ["us-east-1"])
-        expected_err_count = 42
+        expected_err_count = 36
         matches = []
         matches.extend(self.rules.run(filename, cfn))
         assert (
