01d87292be41a634bd1590b8c2eae09e33bf2efa
Kevin DeJong <kddejong@amazon.com>
2024-01-05 10:08:21 -0800

    V1 - Updates to IAM resource policy validations (#2998)
    * Add IAM resource policy validation
    * Format rules output

---
+++ src/cfnlint/rules/resources/iam/ResourcePolicy.py
@@ -0,0 +1,24 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules.resources.iam.Policy import Policy
+
+
+class ResourcePolicy(Policy):
+    """Check IAM resource Policies"""
+
+    id = "E3512"
+    shortdesc = "Validate resource based IAM polices"
+    description = (
+        "IAM resources polices are embedded JSON in CloudFormation. "
+        "This rule validates those embedded policies."
+    )
+    source_url = "https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_identity-vs-resource.html"
+    tags = ["resources", "iam"]
+
+    def __init__(self):
+        super().__init__("resource", "policy_resource.json")
+
+        # remap validator to the correct function name
+        self.iamresourcepolicy = self.validator
