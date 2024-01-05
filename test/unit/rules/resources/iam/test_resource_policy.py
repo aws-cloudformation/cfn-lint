9d9184a607d87fcbbffdd65a5e5a67259c3814d7
Kevin DeJong <kddejong@amazon.com>
2024-01-05 10:08:21 -0800

    V1 - Updates to IAM resource policy validations (#2998)
    * Add IAM resource policy validation
    * Format rules output

---
+++ test/unit/rules/resources/iam/test_resource_policy.py
@@ -0,0 +1,157 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from unittest import TestCase
+
+from cfnlint.context import Context
+from cfnlint.helpers import FUNCTIONS
+from cfnlint.jsonschema import CfnTemplateValidator
+from cfnlint.rules.resources.iam.ResourcePolicy import ResourcePolicy
+
+
+class TestResourcePolicy(TestCase):
+    """Test IAM identity Policies"""
+
+    def setUp(self):
+        """Setup"""
+        self.rule = ResourcePolicy()
+
+    def test_object_basic(self):
+        """Test Positive"""
+        validator = CfnTemplateValidator()
+
+        policy = {"Version": "2012-10-18"}
+
+        errs = list(
+            self.rule.iamresourcepolicy(
+                validator=validator, policy=policy, schema={}, policy_type=None
+            )
+        )
+        self.assertEqual(len(errs), 2, errs)
+        self.assertEqual(
+            errs[0].message, "'2012-10-18' is not one of ['2008-10-17', '2012-10-17']"
+        )
+        self.assertListEqual(list(errs[0].path), ["Version"])
+        self.assertEqual(errs[1].message, "'Statement' is a required property")
+        self.assertListEqual(list(errs[1].path), [])
+
+    def test_object_multiple_effect(self):
+        validator = CfnTemplateValidator()
+
+        policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "NotAction": "*",
+                    "Action": [
+                        "cloudformation:*",
+                    ],
+                    "Resource": "*",
+                }
+            ],
+        }
+
+        errs = list(
+            self.rule.iamresourcepolicy(
+                validator=validator, policy=policy, schema={}, policy_type=None
+            )
+        )
+        self.assertEqual(len(errs), 2, errs)
+        self.assertEqual(
+            errs[0].message,
+            ("Only one of ['Action', 'NotAction'] is a required property"),
+        )
+        self.assertEqual(
+            errs[1].message,
+            ("Only one of ['Action', 'NotAction'] is a required property"),
+        )
+        self.assertIn(
+            ["Statement", 0, "NotAction"], [list(errs[0].path), list(errs[1].path)]
+        )
+        self.assertIn(
+            ["Statement", 0, "Action"], [list(errs[0].path), list(errs[1].path)]
+        )
+
+    def test_object_statements(self):
+        validator = CfnTemplateValidator(context=Context(functions=FUNCTIONS))
+
+        policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "NotAllow",
+                    "Action": [
+                        "cloudformation:Describe*",
+                        "cloudformation:List*",
+                        "cloudformation:Get*",
+                    ],
+                    "Resource": [
+                        {
+                            "Fn::Sub": "arn:${AWS::Partition}:iam::123456789012:role/object-role"
+                        },
+                        {
+                            "NotValid": [
+                                "arn:${AWS::Partition}:iam::123456789012:role/object-role"
+                            ]
+                        },
+                    ],
+                }
+            ],
+        }
+
+        errs = list(
+            self.rule.iamresourcepolicy(
+                validator=validator, policy=policy, schema={}, policy_type=None
+            )
+        )
+        self.assertEqual(len(errs), 2, errs)
+        self.assertEqual(errs[0].message, "'NotAllow' is not one of ['Allow', 'Deny']")
+        self.assertListEqual(list(errs[0].path), ["Statement", 0, "Effect"])
+        self.assertEqual(
+            errs[1].message,
+            "{'NotValid': ['arn:${AWS::Partition}:iam::123456789012:role/object-role']} is not of type 'string'",
+        )
+        self.assertListEqual(list(errs[1].path), ["Statement", 0, "Resource", 1])
+
+    def test_string_statements(self):
+        """Test Positive"""
+        validator = CfnTemplateValidator()
+
+        # ruff: noqa: E501
+        policy = """
+            {
+                "Version": "2012-10-18",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "cloudformation:Describe*",
+                            "cloudformation:List*",
+                            "cloudformation:Get*"
+                        ],
+                        "Resource": [
+                            "*",
+                            {"Fn::Sub": ["arn:${AWS::Partition}:iam::123456789012/role/string-role"]}
+                        ]
+                    }
+                ]
+            }
+        """
+
+        errs = list(
+            self.rule.iamresourcepolicy(
+                validator=validator, policy=policy, schema={}, policy_type=None
+            )
+        )
+        self.assertEqual(len(errs), 2, errs)
+        self.assertEqual(
+            errs[0].message,
+            "{'Fn::Sub': ['arn:${AWS::Partition}:iam::123456789012/role/string-role']} is not of type 'string'",
+        )
+        self.assertListEqual(list(errs[0].path), ["Statement", 0, "Resource", 1])
+        self.assertEqual(
+            errs[1].message, "'2012-10-18' is not one of ['2008-10-17', '2012-10-17']"
+        )
+        self.assertListEqual(list(errs[1].path), ["Version"])
