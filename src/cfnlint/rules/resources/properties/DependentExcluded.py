061d663adb2214c7a2f053885a0f90c481aa0e3c
Kevin DeJong <kddejong@amazon.com>
2024-01-07 10:06:26 -0800

    Add dependentExcluded and cleanup (#3002)

---
+++ src/cfnlint/rules/resources/properties/DependentExcluded.py
@@ -0,0 +1,19 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+from cfnlint.rules import CloudFormationLintRule
+
+
+class DependentExcluded(CloudFormationLintRule):
+    """Check Required Resource Configuration"""
+
+    id = "E3023"
+    shortdesc = (
+        "Validate that when a property is specified another property should be excluded"
+    )
+    description = (
+        "When certain properties are specified other properties should not be included"
+    )
+    source_url = "https://github.com/aws-cloudformation/cfn-python-lint/blob/main/docs/cfn-resource-specification.md"
+    tags = ["resources"]
