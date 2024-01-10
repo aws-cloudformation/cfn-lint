593670702f805b63238d31a7c58a586e17b97b18
Kevin DeJong <kddejong@amazon.com>
2024-01-10 08:22:11 -0800

    Make context more effecient (#3007)

--- test/unit/module/context/test_context.py
+++ test/unit/module/context/test_context.py
@@ -11,30 +11,30 @@ from cfnlint.context import Context
 class TestCfnContext(unittest.TestCase):
     def test_class(self):
         context = Context()
-        self.assertEqual(context.region, "us-east-1")
+        self.assertEqual(context.regions, ["us-east-1"])
         self.assertEqual(context.conditions, {})
         self.assertEqual(context.path, deque())
 
         # Test evolve
         evolved = context.evolve(
-            region="us-west-2",
+            regions=["us-west-2"],
             conditions={"Foo": True},
             path=1,
         )
-        self.assertEqual(evolved.region, "us-west-2")
-        self.assertEqual(evolved.conditions, {"Foo": True})
+        self.assertListEqual(evolved.regions, ["us-west-2"])
+        self.assertDictEqual(evolved.conditions, {"Foo": True})
         self.assertEqual(evolved.path, deque([1]))
 
         evolved_again = evolved.evolve(path=2)
-        self.assertEqual(evolved_again.region, "us-west-2")
-        self.assertEqual(evolved_again.conditions, {"Foo": True})
+        self.assertListEqual(evolved_again.regions, ["us-west-2"])
+        self.assertDictEqual(evolved_again.conditions, {"Foo": True})
         self.assertEqual(evolved_again.path, deque([1, 2]))
         # no changes to the original
-        self.assertEqual(context.region, "us-east-1")
-        self.assertEqual(context.conditions, {})
+        self.assertListEqual(context.regions, ["us-east-1"])
+        self.assertDictEqual(context.conditions, {})
         self.assertEqual(context.path, deque())
 
-        no_path = context.evolve(region="us-east-2")
-        self.assertEqual(no_path.region, "us-east-2")
-        self.assertEqual(no_path.conditions, {})
+        no_path = context.evolve(regions=["us-east-2"])
+        self.assertListEqual(no_path.regions, ["us-east-2"])
+        self.assertDictEqual(no_path.conditions, {})
         self.assertEqual(no_path.path, deque())
