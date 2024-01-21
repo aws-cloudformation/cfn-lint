f673ceb8e509781548778a4e84a0abf5dbaa995f
Kevin DeJong <kddejong@amazon.com>
2024-01-21 09:12:16 -0800

    Make sure resource types are strings before assuming (#3025)

--- src/cfnlint/rules/resources/properties/Properties.py
+++ src/cfnlint/rules/resources/properties/Properties.py
@@ -72,7 +72,7 @@ class Properties(BaseJsonSchema):
         )
 
         t = instance.get("Type")
-        if not t:
+        if not validator.is_type(t, "string"):
             return
 
         if t.startswith("Custom::"):
