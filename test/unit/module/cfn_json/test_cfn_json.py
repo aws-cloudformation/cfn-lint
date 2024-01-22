d9b337e0867bd435a4375d579634e2720f5cabdd
Kevin DeJong <kddejong@amazon.com>
2024-01-22 08:38:00 -0800

    Add rules for security group validation (#3026)
    * Add rule E3687 to validate ToPort and FromPort
    * Add rule W3687 to validate ToPort and FromPort
    * Add rule E3688 to validate both ToPort,FromPort are -1
    * Apply the rules to Ingress and Egress resources

--- test/unit/module/cfn_json/test_cfn_json.py
+++ test/unit/module/cfn_json/test_cfn_json.py
@@ -43,7 +43,7 @@ class TestCfnJson(BaseTestCase):
             },
             "vpc": {
                 "filename": "test/fixtures/templates/quickstart/vpc.json",
-                "failures": 4,
+                "failures": 5,
             },
             "poller": {
                 "filename": "test/fixtures/templates/public/lambda-poller.json",
