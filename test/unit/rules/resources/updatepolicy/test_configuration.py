0a1d85ce025e5852f89a1bd17cbc1d617ca2fe9e
Kevin DeJong <kddejong@amazon.com>
2024-01-25 08:01:00 -0800

    Rewrite a few more rules and cleanup

--- test/unit/rules/resources/updatepolicy/test_configuration.py
+++ test/unit/rules/resources/updatepolicy/test_configuration.py
@@ -3,30 +3,132 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from test.unit.rules import BaseRuleTestCase
+import pytest
 
-from cfnlint.rules.resources.updatepolicy.Configuration import (
-    Configuration,  # pylint: disable=E0401
+from cfnlint.jsonschema import CfnTemplateValidator, ValidationError
+from cfnlint.rules.resources.updatepolicy.Configuration import Configuration
+
+
+@pytest.fixture(scope="module")
+def rule():
+    rule = Configuration()
+    yield rule
+
+
+@pytest.fixture(scope="module")
+def validator():
+    yield CfnTemplateValidator(schema={})
+
+
+@pytest.mark.parametrize(
+    "name,instance,expected",
+    [
+        ("Valid with a non updatable type", {"Type": "AWS::Foo::Bar"}, []),
+        (
+            "Invalid with autoscaling group wrong type",
+            {"Type": "AWS::AutoScaling::AutoScalingGroup", "UpdatePolicy": []},
+            [
+                ValidationError(
+                    "[] is not of type 'object'",
+                    path=["UpdatePolicy"],
+                    rule=Configuration(),
+                    instance=[],
+                    validator="type",
+                    validator_value="object",
+                    schema_path=[
+                        "allOf",
+                        0,
+                        "then",
+                        "properties",
+                        "UpdatePolicy",
+                        "type",
+                    ],
+                )
+            ],
+        ),
+        (
+            "Invalid with autoscaling group wrong property",
+            {
+                "Type": "AWS::AutoScaling::AutoScalingGroup",
+                "UpdatePolicy": {"Foo": "Bar"},
+            },
+            [
+                ValidationError(
+                    "Additional properties are not allowed ('Foo' was unexpected)",
+                    path=["UpdatePolicy", "Foo"],
+                    rule=Configuration(),
+                    instance={"Foo": "Bar"},
+                    validator="additionalProperties",
+                    validator_value=False,
+                    schema_path=[
+                        "allOf",
+                        0,
+                        "then",
+                        "properties",
+                        "UpdatePolicy",
+                        "additionalProperties",
+                    ],
+                )
+            ],
+        ),
+        (
+            "Valid with autoscaling group",
+            {
+                "Type": "AWS::AutoScaling::AutoScalingGroup",
+                "UpdatePolicy": {
+                    "AutoScalingReplacingUpdate": {
+                        "WillReplace": True,
+                    },
+                    "AutoScalingRollingUpdate": {
+                        "MaxBatchSize": 1,
+                        "MinInstancesInService": 1,
+                        "MinSuccessfulInstancesPercent": 100,
+                        "PauseTime": "PT1M",
+                        "WaitOnResourceSignals": True,
+                        "SuspendProcesses": ["AZRebalance"],
+                    },
+                    "AutoScalingScheduledAction": {
+                        "IgnoreUnmodifiedGroupSizeProperties": True,
+                    },
+                },
+            },
+            [],
+        ),
+        (
+            "Valid with lambda function",
+            {
+                "Type": "AWS::Lambda::Alias",
+                "UpdatePolicy": {
+                    "CodeDeployLambdaAliasUpdate": {
+                        "AfterAllowTrafficHook": "Foo",
+                        "ApplicationName": "Foo",
+                        "BeforeAllowTrafficHook": "Foo",
+                        "DeploymentGroupName": "Foo",
+                    }
+                },
+            },
+            [],
+        ),
+        (
+            "Valid with search resource",
+            {
+                "Type": "AWS::Elasticsearch::Domain",
+                "UpdatePolicy": {
+                    "EnableVersionUpgrade": True,
+                },
+            },
+            [],
+        ),
+        (
+            "Valid with ElastiCache resource",
+            {
+                "Type": "AWS::ElastiCache::ReplicationGroup",
+                "UpdatePolicy": {"UseOnlineResharding": True},
+            },
+            [],
+        ),
+    ],
 )
-
-
-class TestConfiguration(BaseRuleTestCase):
-    """Test Update Policy Configuration"""
-
-    def setUp(self):
-        """Setup"""
-        super(TestConfiguration, self).setUp()
-        self.collection.register(Configuration())
-        self.success_templates = [
-            "test/fixtures/templates/good/resources/updatepolicy/config.yaml"
-        ]
-
-    def test_file_positive(self):
-        """Test Positive"""
-        self.helper_file_positive()
-
-    def test_file_negative_alias(self):
-        """Test failure"""
-        self.helper_file_negative(
-            "test/fixtures/templates/bad/resources/updatepolicy/config.yaml", 13
-        )
+def test_update_policy_configuration(name, instance, expected, rule, validator):
+    errors = list(rule.validate(validator, {}, instance, {}))
+    assert errors == expected, f"Test {name!r} got {errors!r}"
