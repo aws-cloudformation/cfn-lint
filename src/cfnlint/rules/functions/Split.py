75149ade5bd6361ea27fac1c3953fd8d73d306c0
Kevin DeJong <kddejong@amazon.com>
2024-02-13 11:33:39 -0800

    V1 - Updates (#3055)
    * Move CFN validators to the rule
    * Remove rule W1011

--- src/cfnlint/rules/functions/Split.py
+++ src/cfnlint/rules/functions/Split.py
@@ -3,10 +3,17 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from cfnlint.rules import CloudFormationLintRule
+import json
+from typing import Any, Dict
+
+import regex as re
+
+from cfnlint.helpers import REGEX_DYN_REF
+from cfnlint.jsonschema import ValidationError, ValidationResult, Validator
+from cfnlint.rules.functions._BaseFn import BaseFn
 
 
-class Split(CloudFormationLintRule):
+class Split(BaseFn):
     """Check if Split values are correct"""
 
     id = "E1018"
@@ -14,3 +21,53 @@ class Split(CloudFormationLintRule):
     description = "Making sure the split function is properly configured"
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-split.html"
     tags = ["functions", "split"]
+
+    def __init__(self) -> None:
+        super().__init__("Fn::Split", ("array",))
+
+    def schema(self, validator: Validator, instance: Any) -> Dict[str, Any]:
+        return {
+            "type": "array",
+            "maxItems": 2,
+            "minItems": 2,
+            "fn_items": [
+                {
+                    "schema": {
+                        "type": ["string"],
+                    }
+                },
+                {
+                    "functions": [
+                        "Fn::Base64",
+                        "Fn::FindInMap",
+                        "Fn::GetAtt",
+                        "Fn::GetAZs",
+                        "Fn::If",
+                        "Fn::ImportValue",
+                        "Fn::Join",
+                        "Fn::Select",
+                        "Fn::Sub",
+                        "Ref",
+                    ],
+                    "schema": {
+                        "type": ["string"],
+                    },
+                },
+            ],
+        }
+
+    def fn_split(
+        self, validator: Validator, s: Any, instance: Any, schema: Any
+    ) -> ValidationResult:
+        errs = list(super().validate(validator, s, instance, schema))
+        if errs:
+            yield from iter(errs)
+            return
+
+        key, value = self.key_value(instance)
+        if re.fullmatch(REGEX_DYN_REF, json.dumps(value[1])):
+            yield ValidationError(
+                f"{self.fn.name} does not support dynamic references",
+                validator=self.fn.name,
+                path=[key, 1],
+            )
