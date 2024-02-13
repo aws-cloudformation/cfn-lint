eb8d837742eb7bf901489c40705bb460b1fcd684
Kevin DeJong <kddejong@amazon.com>
2024-02-13 11:33:39 -0800

    V1 - Updates (#3055)
    * Move CFN validators to the rule
    * Remove rule W1011

--- src/cfnlint/rules/functions/If.py
+++ src/cfnlint/rules/functions/If.py
@@ -3,10 +3,13 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from cfnlint.rules import CloudFormationLintRule
+from typing import Any, Dict
+
+from cfnlint.jsonschema import ValidationResult, Validator
+from cfnlint.rules.functions._BaseFn import BaseFn, all_types
 
 
-class If(CloudFormationLintRule):
+class If(BaseFn):
     """Check if Condition exists"""
 
     id = "E1028"
@@ -14,3 +17,68 @@ class If(CloudFormationLintRule):
     description = "Check Fn::If to make sure its valid.  Condition has to be a string."
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-conditions.html#intrinsic-function-reference-conditions-if"
     tags = ["functions", "if"]
+
+    def __init__(self) -> None:
+        super().__init__("Fn::If", all_types)
+
+    def schema(self, validator, instance) -> Dict[str, Any]:
+        return {
+            "type": ["array"],
+            "minItems": 3,
+            "maxItems": 3,
+            "fn_items": [
+                {
+                    "schema": {
+                        "type": ["string"],
+                    }
+                },
+            ],
+        }
+
+    def fn_if(
+        self, validator: Validator, s: Any, instance: Any, schema: Any
+    ) -> ValidationResult:
+        # validate this function will return the correct type
+        errs = list(self.validate_fn_output_types(validator, s, instance))
+
+        key, value = self.key_value(instance)
+        errs.extend(
+            list(
+                self.fix_errors(
+                    self.validator(validator).descend(
+                        value, self.schema(validator, instance), path=key
+                    )
+                )
+            )
+        )
+
+        if errs:
+            yield from iter(errs)
+            return
+
+        found_errs = False
+        for err in validator.descend(value[0], {"awsType": "CfnCondition"}, 0):
+            err.path.appendleft(key)
+            yield err
+            found_errs = True
+
+        for err in validator.descend(value[1], s, 1):
+            err.path.appendleft(key)
+            yield err
+            found_errs = True
+        for err in validator.descend(value[2], s, 2):
+            err.path.appendleft(key)
+            yield err
+            found_errs = True
+
+        if not found_errs:
+            yield from self.resolve(validator, s, instance, schema)
+
+    def resolve(
+        self, validator: Validator, s: Any, instance: Any, schema: Any
+    ) -> ValidationResult:
+        key, _ = self.key_value(instance)
+        for value, v, _ in validator.resolve_value(instance):
+            for err in v.descend(value, s, key):
+                err.path.extend(v.context.value_path)
+                yield err
---
+++ test/unit/rules/functions/test_base64.py
@@ -0,0 +1,90 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from collections import deque
+
+import pytest
+
+from cfnlint.context import Context
+from cfnlint.jsonschema import CfnTemplateValidator, ValidationError
+from cfnlint.rules.functions.Base64 import Base64
+
+
+@pytest.fixture(scope="module")
+def rule():
+    rule = Base64()
+    yield rule
+
+
+@pytest.fixture(scope="module")
+def validator():
+    context = Context(
+        regions=["us-east-1"],
+        path=deque([]),
+        resources={},
+        parameters={},
+    )
+    yield CfnTemplateValidator(context=context)
+
+
+@pytest.mark.parametrize(
+    "name,instance,schema,expected",
+    [
+        (
+            "Valid Fn::Base64 string",
+            {"Fn::Base64": "foo"},
+            {"type": "string"},
+            [],
+        ),
+        (
+            "Invalid Fn::Base64 for wrong output type",
+            {"Fn::Base64": "foo"},
+            {"type": "array"},
+            [
+                ValidationError(
+                    "{'Fn::Base64': 'foo'} is not of type 'array'",
+                    path=deque([]),
+                    schema_path=deque([]),
+                    validator="fn_base64",
+                ),
+            ],
+        ),
+        (
+            "Invalid Fn::Base64 is NOT a string",
+            {"Fn::Base64": ["foo", "bar"]},
+            {"type": "string"},
+            [
+                ValidationError(
+                    "['foo', 'bar'] is not of type 'string'",
+                    path=deque(["Fn::Base64"]),
+                    schema_path=deque(["type"]),
+                    validator="fn_base64",
+                ),
+            ],
+        ),
+        (
+            "Invalid Fn::Base64 using an invalid function",
+            {"Fn::Base64": {"foo": "bar"}},
+            {"type": "string"},
+            [
+                ValidationError(
+                    "{'foo': 'bar'} is not of type 'string'",
+                    path=deque(["Fn::Base64"]),
+                    schema_path=deque(["type"]),
+                    validator="fn_base64",
+                ),
+            ],
+        ),
+        (
+            "Valid Fn::Base64 with a valid function",
+            {"Fn::Base64": {"Fn::Sub": "foo"}},
+            {"type": "string"},
+            [],
+        ),
+    ],
+)
+def test_validate(name, instance, schema, expected, rule, validator):
+    errs = list(rule.fn_base64(validator, schema, instance, {}))
+    assert errs == expected, f"Test {name!r} got {errs!r}"
