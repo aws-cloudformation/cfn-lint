3f53979ac5f008342e2ebde15dbdd7dd1d95955c
Kevin DeJong <kddejong@amazon.com>
2024-02-15 14:03:45 -0800

    Clean up fn tests (#3057)

--- test/unit/rules/functions/test_ref.py
+++ test/unit/rules/functions/test_ref.py
@@ -7,10 +7,12 @@ from collections import deque
 
 import pytest
 
-from cfnlint.context import Context
-from cfnlint.context.context import Parameter, Resource
+from cfnlint.context import create_context_for_template
+from cfnlint.context.context import Transforms
 from cfnlint.jsonschema import CfnTemplateValidator, ValidationError
+from cfnlint.jsonschema._validators_cfn import FnItems
 from cfnlint.rules.functions.Ref import Ref
+from cfnlint.template import Template
 
 
 @pytest.fixture(scope="module")
@@ -20,41 +22,54 @@ def rule():
 
 
 @pytest.fixture(scope="module")
-def validator():
-    context = Context(
+def cfn():
+    return Template(
+        "",
+        {
+            "Resources": {
+                "MyVolume": {"Type": "AWS::EC2::Volume"},
+                "MyInstance": {"Type": "AWS::EC2::Instance"},
+            },
+            "Parameters": {
+                "MyParameter": {
+                    "Type": "String",
+                },
+                "MyArrayParameter": {
+                    "Type": "CommaDelimitedList",
+                },
+            },
+        },
         regions=["us-east-1"],
-        path=deque([]),
-        resources={
-            "MyVolume": Resource({"Type": "AWS::EC2::Volume"}),
-            "MyInstance": Resource({"Type": "AWS::EC2::Instance"}),
-        },
-        parameters={
-            "MyParameter": Parameter({"Type": "String"}),
-            "MyArrayParameter": Parameter({"Type": "CommaDelimitedList"}),
-        },
     )
-    yield CfnTemplateValidator(context=context)
+
+
+@pytest.fixture(scope="module")
+def context(cfn):
+    return create_context_for_template(cfn)
 
 
 @pytest.mark.parametrize(
-    "name,instance,schema,expected",
+    "name,instance,schema,context_evolve,expected",
     [
         (
             "Valid ref",
             {"Ref": "MyVolume"},
             {"type": "string"},
+            {},
             [],
         ),
         (
             "Valid array ref",
             {"Ref": "MyArrayParameter"},
             {"type": "array"},
+            {},
             [],
         ),
         (
             "Invalid Ref with bad type",
             {"Ref": ["foo"]},
             {"type": "string"},
+            {},
             [
                 ValidationError(
                     "['foo'] is not of type 'string'",
@@ -65,9 +80,10 @@ def validator():
                 ValidationError(
                     (
                         "['foo'] is not one of ['MyParameter', 'MyArrayParameter', "
-                        "'MyVolume', 'MyInstance', 'AWS::NoValue', 'AWS::AccountId', "
-                        "'AWS::Partition', 'AWS::Region', 'AWS::StackId', "
-                        "'AWS::StackName', 'AWS::URLSuffix', 'AWS::NotificationARNs']"
+                        "'MyVolume', 'MyInstance', 'AWS::AccountId', "
+                        "'AWS::NoValue', 'AWS::NotificationARNs', "
+                        "'AWS::Partition', 'AWS::Region', "
+                        "'AWS::StackId', 'AWS::StackName', 'AWS::URLSuffix']"
                     ),
                     path=deque(["Ref"]),
                     schema_path=deque(["enum"]),
@@ -79,19 +95,22 @@ def validator():
             "Invalid Ref with a type that doesn't match array schema",
             {"Ref": "MyParameter"},
             {"type": "array"},
+            {},
             [ValidationError("{'Ref': 'MyParameter'} is not of type 'array'")],
         ),
         (
             "Invalid Ref with a type that doesn't match singular schema",
             {"Ref": "Foo"},
             {"type": "string"},
+            {},
             [
                 ValidationError(
                     (
                         "'Foo' is not one of ['MyParameter', 'MyArrayParameter', "
-                        "'MyVolume', 'MyInstance', 'AWS::NoValue', 'AWS::AccountId', "
-                        "'AWS::Partition', 'AWS::Region', 'AWS::StackId', "
-                        "'AWS::StackName', 'AWS::URLSuffix', 'AWS::NotificationARNs']"
+                        "'MyVolume', 'MyInstance', 'AWS::AccountId', "
+                        "'AWS::NoValue', 'AWS::NotificationARNs', "
+                        "'AWS::Partition', 'AWS::Region', "
+                        "'AWS::StackId', 'AWS::StackName', 'AWS::URLSuffix']"
                     ),
                     path=deque(["Ref"]),
                     schema_path=deque(["enum"]),
@@ -99,8 +118,36 @@ def validator():
                 ),
             ],
         ),
+        (
+            "Valid ref",
+            {"Ref": {"Ref": "MyParameter"}},
+            {"type": "string"},
+            {"transforms": Transforms(["AWS::LanguageExtensions"])},
+            [],
+        ),
+        (
+            "Invalid second ref with array return",
+            {"Ref": {"Ref": "MyArrayParameter"}},
+            {"type": "array"},
+            {"transforms": Transforms(["AWS::LanguageExtensions"])},
+            [
+                ValidationError(
+                    "{'Ref': 'MyArrayParameter'} is not of type 'string'",
+                    path=deque(["Ref"]),
+                    validator="ref",
+                    schema_path=deque(["ref"]),
+                ),
+            ],
+        ),
     ],
 )
-def test_validate(name, instance, schema, expected, rule, validator):
+def test_validate(name, instance, schema, context_evolve, expected, rule, context, cfn):
+    context = context.evolve(**context_evolve)
+    validator = CfnTemplateValidator({}).extend(
+        validators={
+            "fn_items": FnItems().validate,
+            "ref": Ref().ref,
+        }
+    )(context=context, cfn=cfn)
     errs = list(rule.ref(validator, schema, instance, {}))
     assert errs == expected, f"Test {name!r} got {errs!r}"
