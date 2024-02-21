891c345eb86fb3f42509be3a7918c3582e1a47f4
Kevin DeJong <kddejong@amazon.com>
2024-02-21 09:17:00 -0800

    V1 - Better dynamic reference support (#3064)
    * Additional cleanup
    * Add better support for dynamic references

--- src/cfnlint/conditions/__init__.py
+++ src/cfnlint/conditions/__init__.py
@@ -3,4 +3,6 @@ Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
+__all__ = ["Conditions"]
+
 from cfnlint.conditions.conditions import Conditions
--- src/cfnlint/rules/functions/ForEach.py
+++ src/cfnlint/rules/functions/ForEach.py
@@ -5,8 +5,7 @@ SPDX-License-Identifier: MIT-0
 
 import logging
 
-from cfnlint.languageExtensions import LanguageExtensions
-from cfnlint.rules import CloudFormationLintRule
+from cfnlint.rules import CloudFormationLintRule, RuleMatch
 
 LOGGER = logging.getLogger("cfnlint")
 
@@ -18,6 +17,20 @@ class ForEach(CloudFormationLintRule):
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-getatt.html"
     tags = ["functions", "foreach"]
 
+    def validate_transform_is_declared(
+        self, has_language_extensions_transform, tree, intrinsic_function
+    ):
+        matches = []
+        if not has_language_extensions_transform:
+            message = (
+                "Missing Transform: Declare the AWS::LanguageExtensions Transform"
+                " globally to enable use of the intrinsic function "
+                + intrinsic_function
+                + " at {0}"
+            )
+            matches.append(RuleMatch(tree[:], message.format("/".join(map(str, tree)))))
+        return matches
+
     # pylint: disable=unused-argument
     def match(self, cfn):
         matches = []
@@ -27,11 +40,12 @@ class ForEach(CloudFormationLintRule):
         for for_each in for_eaches:
             has_language_extensions_transform = cfn.has_language_extensions_transform()
 
-            LanguageExtensions().validate_transform_is_declared(
-                has_language_extensions_transform,
-                matches,
-                for_each[:-1],
-                intrinsic_function,
+            matches.extend(
+                self.validate_transform_is_declared(
+                    has_language_extensions_transform,
+                    for_each[:-1],
+                    intrinsic_function,
+                )
             )
 
         return matches
