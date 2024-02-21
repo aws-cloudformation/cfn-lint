8ee96af7069ac9e5aa501e05382915ef09ea8961
Kevin DeJong <kddejong@amazon.com>
2024-02-21 09:17:00 -0800

    V1 - Better dynamic reference support (#3064)
    * Additional cleanup
    * Add better support for dynamic references

--- test/unit/rules/functions/test_dynamic_reference.py
+++ test/unit/rules/functions/test_dynamic_reference.py
@@ -3,30 +3,100 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-from test.unit.rules import BaseRuleTestCase
+import pytest
 
-from cfnlint.rules.functions.DynamicReferenceSecureString import (
-    DynamicReferenceSecureString,  # pylint: disable=E0401
+from cfnlint.context import create_context_for_template
+from cfnlint.jsonschema import CfnTemplateValidator, ValidationError
+from cfnlint.rules.functions.DynamicReference import DynamicReference
+from cfnlint.template import Template
+
+
+@pytest.fixture(scope="module")
+def rule():
+    rule = DynamicReference()
+    yield rule
+
+
+@pytest.fixture(scope="module")
+def cfn():
+    return Template(
+        "",
+        {},
+        regions=["us-east-1"],
+    )
+
+
+@pytest.fixture(scope="module")
+def context(cfn):
+    return create_context_for_template(cfn)
+
+
+@pytest.mark.parametrize(
+    "name,instance,schema,expected",
+    [
+        (
+            "Valid SSM Parameter",
+            "{{resolve:ssm:Parameter}}",
+            {"type": "test"},
+            [],
+        ),
+        (
+            "Valid SSM Parameter with integer",
+            "{{resolve:ssm:Parameter:1}}",
+            {"type": "test"},
+            [],
+        ),
+        (
+            "Valid when item isn't a string",
+            {},
+            {"type": "test"},
+            [],
+        ),
+        (
+            "Basic error when wrong length",
+            "{{resolve:ssm}}",
+            {"type": "test"},
+            [
+                ValidationError(
+                    "['resolve', 'ssm'] is too short (3)",
+                    validator="minItems",
+                    rule=DynamicReference(),
+                )
+            ],
+        ),
+        (
+            "Invalid SSM Parameter with string version",
+            "{{resolve:ssm:Parameter:a}}",
+            {"type": "test"},
+            [
+                ValidationError(
+                    "'a' does not match '\\\\d+'",
+                    validator="pattern",
+                    rule=DynamicReference(),
+                )
+            ],
+        ),
+        (
+            "Valid secret manager",
+            "{{resolve:secretsmanager:Secret}}",
+            {"type": "test"},
+            [],
+        ),
+        (
+            "Valid secret manager with secretstring",
+            "{{resolve:secretsmanager:Secret:SecretString}}",
+            {"type": "test"},
+            [],
+        ),
+        (
+            "Valid secret manager from another account",
+            "{{resolve:secretsmanager:arn:aws:secretsmanager:us-west-2:123456789012:secret:MySecret-a1b2c3:SecretString:password}}",
+            {"type": "test"},
+            [],
+        ),
+    ],
 )
-
-
-class TestDynamicReferenceSecureString(BaseRuleTestCase):
-    """Test Rules Dynamic References exists"""
-
-    def setUp(self):
-        """Setup"""
-        super(TestDynamicReferenceSecureString, self).setUp()
-        self.collection.register(DynamicReferenceSecureString())
-        self.success_templates = [
-            "test/fixtures/templates/good/functions/dynamic_reference.yaml"
-        ]
-
-    def test_file_positive(self):
-        """Test Positive"""
-        self.helper_file_positive()
-
-    def test_file_negative(self):
-        """Test failure"""
-        self.helper_file_negative(
-            "test/fixtures/templates/bad/functions/dynamic_reference.yaml", 4
-        )
+def test_validate(name, instance, schema, expected, rule, context, cfn):
+    validator = CfnTemplateValidator(context=context, cfn=cfn)
+    errs = list(rule.dynamicReference(validator, schema, instance, {}))
+    assert errs == expected, f"Test {name!r} got {errs!r}"
