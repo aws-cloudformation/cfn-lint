0c9d3c5fd7354086029d9617275e28facac38dc3
Kevin DeJong <kddejong@amazon.com>
2024-02-29 12:49:48 -0800

    V1 - Update testing configuration and convert from codecov (#3083)
    * Update testing file and configuration

--- test/unit/module/formatters/test_formatters.py
+++ test/unit/module/formatters/test_formatters.py
@@ -39,8 +39,9 @@ class TestFormatters(BaseTestCase):
     def setUp(self) -> None:
         super().setUp()
         self.rules = Rules.create_from_directory(cfnlint.config._DEFAULT_RULESDIR)
-        self.config = ConfigMixIn()
         self.filename = str(Path("test/fixtures/templates/bad/formatters.yaml"))
+        self.config = ConfigMixIn(cli_args=["--", self.filename])
+
         self.results = [
             Match(
                 6,
