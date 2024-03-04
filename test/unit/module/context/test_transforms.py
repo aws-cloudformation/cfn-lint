419c1b5a796b4ad9120697b4e262aaf4e739f559
Kevin DeJong <kddejong@amazon.com>
2024-03-04 10:55:40 -0800

    Add more testing (#3087)

---
+++ test/unit/module/context/test_transforms.py
@@ -0,0 +1,36 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+import pytest
+
+from cfnlint.context.context import Transforms
+
+
+@pytest.mark.parametrize(
+    "name,instance,expected",
+    [
+        ("Valid transforms", "AWS::LanguageExtensions", True),
+        ("Valid transforms lists", ["AWS::LanguageExtensions"], True),
+        ("Valid transforms lists", None, False),
+        ("Valid transforms lists", "", False),
+    ],
+)
+def test_transforms(name, instance, expected):
+    transforms = Transforms(instance)
+
+    assert (
+        expected == transforms.has_language_extensions_transform()
+    ), f"{name!r} test got {transforms.has_language_extensions_transform()}"
+
+
+@pytest.mark.parametrize(
+    "name,instance",
+    [
+        ("Invalid Type", {}),
+    ],
+)
+def test_errors(name, instance):
+    with pytest.raises(ValueError):
+        Transforms(instance)
