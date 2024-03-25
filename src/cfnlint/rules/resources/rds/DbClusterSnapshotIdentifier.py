5dbd378083447dd33ed1d5f6bc75170a3c3563ac
Kevin DeJong <kddejong@amazon.com>
2024-03-25 12:45:51 -0700

    Update specs and create rules W3689,W3688 (#3118)

---
+++ src/cfnlint/rules/resources/rds/DbClusterSnapshotIdentifier.py
@@ -0,0 +1,34 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+import cfnlint.data.schemas.extensions.aws_rds_dbcluster
+from cfnlint.jsonschema import ValidationError
+from cfnlint.rules.jsonschema.CfnLintJsonSchema import CfnLintJsonSchema, SchemaDetails
+
+
+class DbClusterSnapshotIdentifier(CfnLintJsonSchema):
+    id = "W3688"
+    shortdesc = "When restoring DBCluster certain properties are ignored"
+    description = (
+        "When creating a DBCluster extra properties are ignored and could "
+        "result in drift"
+    )
+    tags = ["resources", "rds"]
+
+    def __init__(self) -> None:
+        super().__init__(
+            keywords=["AWS::RDS::DBCluster/Properties"],
+            schema_details=SchemaDetails(
+                module=cfnlint.data.schemas.extensions.aws_rds_dbcluster,
+                filename="snapshotidentifier.json",
+            ),
+        )
+
+    def message(self, instance: Any, err: ValidationError) -> str:
+        return "Additional properties are ignored ('MasterUsername')"
