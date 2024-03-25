6c16e74aa4945c83a217b3de66bede02c60d54b3
Kevin DeJong <kddejong@amazon.com>
2024-03-25 12:45:51 -0700

    Update specs and create rules W3689,W3688 (#3118)

---
+++ test/unit/rules/resources/rds/test_db_cluster_sourcedbclusteridentifier.py
@@ -0,0 +1,59 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from collections import deque
+
+import pytest
+
+from cfnlint.jsonschema import CfnTemplateValidator, ValidationError
+from cfnlint.rules.resources.rds.DbClusterSourceDBClusterIdentifier import (
+    DbClusterSourceDBClusterIdentifier,
+)
+
+
+@pytest.fixture(scope="module")
+def rule():
+    rule = DbClusterSourceDBClusterIdentifier()
+    yield rule
+
+
+@pytest.fixture(scope="module")
+def validator():
+    yield CfnTemplateValidator(schema={})
+
+
+@pytest.mark.parametrize(
+    "instance,expected",
+    [
+        (
+            {
+                "SourceDBClusterIdentifier": "foo",
+            },
+            [],
+        ),
+        (
+            {},
+            [],
+        ),
+        (
+            {"SourceDBClusterIdentifier": "foo", "MasterUsername": "foo"},
+            [
+                ValidationError(
+                    (
+                        "Additional properties are ignored ('MasterUsername', "
+                        "'MasterUserPassword', 'StorageEncrypted')"
+                    ),
+                    rule=DbClusterSourceDBClusterIdentifier(),
+                    path=deque(["MasterUsername"]),
+                    validator=None,
+                    schema_path=deque(["then", "properties", "MasterUsername"]),
+                )
+            ],
+        ),
+    ],
+)
+def test_validate(instance, expected, rule, validator):
+    errs = list(rule.validate(validator, "", instance, {}))
+    assert errs == expected, f"Expected {expected} got {errs}"
