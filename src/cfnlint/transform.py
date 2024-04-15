829f3ab21e096e57970973629c123b5aee181f07
Kevin DeJong <kddejong@amazon.com>
2024-04-15 09:54:18 -0700

    More cleanup to change APIs

--- src/cfnlint/transform.py
+++
@@ -1,7 +0,0 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: MIT-0
-"""
-
-# ruff: noqa: F401
-from cfnlint.template.transforms._sam import Transform
