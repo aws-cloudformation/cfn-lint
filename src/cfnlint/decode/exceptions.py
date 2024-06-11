ed09200b170b053f51e124b20f40d3eb8912616b
Kevin DeJong <kddejong@amazon.com>
2024-06-11 13:51:50 -0700

    More cleanup of unused code

--- src/cfnlint/decode/exceptions.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: MIT-0
-"""
-
-
-class TemplateAttributeError(AttributeError):
-    """Custom error to capture Attribute Errors in the Template"""
