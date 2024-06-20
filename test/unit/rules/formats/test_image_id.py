d0aae9bd6e3615e8d1023ca3c5c5aca1992d8a87
Kevin DeJong <kddejong@amazon.com>
2024-06-20 08:06:22 -0700

    Add logic to ImageId format for SSM (#3340)
    * Add logic to ImageId format for SSM
    
    * One more test scenario

--- test/unit/rules/formats/test_image_id.py
+++ test/unit/rules/formats/test_image_id.py
@@ -3,8 +3,11 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
+from collections import deque
+
 import pytest
 
+from cfnlint.context import Path
 from cfnlint.rules.formats.ImageId import ImageId
 
 
@@ -15,26 +18,72 @@ def rule():
 
 
 @pytest.mark.parametrize(
-    "name,instance,expected",
+    "name,instance,path,expected",
     [
         (
             "Valid image id",
             "ami-abcd1234",
+            deque([]),
             True,
         ),
         (
             "Valid image id long",
             "ami-abcdefa1234567890",
+            deque([]),
+            True,
+        ),
+        (
+            "Valid image id using ssm",
+            "resolve:ssm:/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id",
+            deque(
+                [
+                    "Resources",
+                    "AWS::EC2::LaunchTemplate",
+                    "Properties",
+                    "LaunchTemplateData",
+                    "ImageId",
+                ]
+            ),
+            True,
+        ),
+        (
+            "Invalid image id using ssm",
+            "resolve:ssm:/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id",
+            deque([]),
+            False,
+        ),
+        (
+            "Valid image id in exception",
+            "ami-abcd1234",
+            deque(
+                [
+                    "Resources",
+                    "AWS::EC2::LaunchTemplate",
+                    "Properties",
+                    "LaunchTemplateData",
+                    "ImageId",
+                ]
+            ),
             True,
         ),
         (
             "Valid type",
             [],
+            deque([]),
             True,
         ),
-        ("Invalid image ID", "ami-abc", False),
+        ("Invalid image ID", "ami-abc", deque([]), False),
     ],
 )
-def test_validate(name, instance, expected, rule, validator):
+def test_validate(name, instance, path, expected, rule, validator):
+    validator = validator.evolve(
+        context=validator.context.evolve(
+            path=Path(
+                path=deque([]),
+                cfn_path=path,
+            ),
+        )
+    )
+
     result = rule.format(validator, instance)
     assert result == expected, f"Test {name!r} got {result!r}"
