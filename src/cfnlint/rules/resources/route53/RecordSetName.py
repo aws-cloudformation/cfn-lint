b0782f87c49ae280fb63d328c3c4d46b56c96ea4
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-06-22 15:55:15 +0200

    Relax E3041 to support AWS::Route53::RecordSet resources that specify DNS root records (#3376) (#3377)

--- src/cfnlint/rules/resources/route53/RecordSetName.py
+++ src/cfnlint/rules/resources/route53/RecordSetName.py
@@ -16,10 +16,10 @@ class RecordSetName(CfnLintKeyword):
     """Check if a Route53 Resoruce Records Name is valid with a HostedZoneName"""
 
     id = "E3041"
-    shortdesc = "RecordSet HostedZoneName is a superdomain of Name"
+    shortdesc = "RecordSet HostedZoneName is a superdomain of or equal to Name"
     description = (
-        "In a RecordSet, the HostedZoneName must be a superdomain of the Name being"
-        " validated"
+        "In a RecordSet, the HostedZoneName must be a superdomain of or equal to"
+        " the Name being validated"
     )
     source_url = "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-recordset.html#cfn-route53-recordset-name"
     tags = ["resource", "properties", "route53"]
@@ -39,24 +39,22 @@ class RecordSetName(CfnLintKeyword):
             name = props.get("Name", None)
             hz_name = props.get("HostedZoneName", None)
             if isinstance(name, str) and isinstance(hz_name, str):
-                if hz_name[-1] != ".":
+                if not hz_name.endswith("."):
                     yield ValidationError(
                         f"{hz_name!r} must end in a dot",
                         path=deque(["HostedZoneName"]),
                         instance=props.get("HostedZoneName"),
                     )
+                    hz_name = f"{hz_name}."
 
-                if hz_name[-1] == ".":
-                    hz_name = hz_name[:-1]
-                hz_name = f".{hz_name}"
-                if name[-1] == ".":
-                    name = name[:-1]
+                if not name.endswith("."):
+                    name = f"{name}."
 
-                if hz_name not in [name, name[-len(hz_name) :]]:
+                if name != hz_name and not name.endswith(f".{hz_name}"):
                     yield ValidationError(
                         (
                             f"{props.get('Name')!r} must be a subdomain "
-                            f"of {props.get('HostedZoneName')!r}"
+                            f"of or equal to {props.get('HostedZoneName')!r}"
                         ),
                         path=deque(["Name"]),
                         instance=props.get("Name"),
