6956944e4116a3b177f374ee92d5c4a7a01e53c7
Kevin DeJong <kddejong@amazon.com>
2024-07-30 15:45:18 -0700

    Add 3 new rules to validate ECS configs

---
+++ src/cfnlint/rules/resources/ecs/ServiceFargate.py
@@ -0,0 +1,138 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from __future__ import annotations
+
+from collections import deque
+from typing import Any, Iterator
+
+from cfnlint.helpers import ensure_list, is_function
+from cfnlint.jsonschema import ValidationError, ValidationResult
+from cfnlint.jsonschema.protocols import Validator
+from cfnlint.rules.helpers import get_resource_by_name, get_value_from_path
+from cfnlint.rules.jsonschema.CfnLintKeyword import CfnLintKeyword
+
+
+class ServiceFargate(CfnLintKeyword):
+    id = "E3054"
+    shortdesc = (
+        "Validate ECS service using Fargate uses TaskDefinition that allows Fargate"
+    )
+    description = (
+        "When using an ECS service with 'LaunchType' of 'FARGATE' "
+        "the associated task definition must have 'RequiresCompatibilities' "
+        "specified with 'FARGATE' listed"
+    )
+    tags = ["resources", "ecs"]
+
+    def __init__(self) -> None:
+        super().__init__(
+            keywords=["Resources/AWS::ECS::Service/Properties"],
+        )
+
+    def _filter_resource_name(self, instance: Any) -> str | None:
+        fn_k, fn_v = is_function(instance)
+        if fn_k is None:
+            return None
+        if fn_k == "Ref":
+            if isinstance(fn_v, str):
+                return fn_v
+        elif fn_k == "Fn::GetAtt":
+            name = ensure_list(fn_v)[0].split(".")[0]
+            if isinstance(name, str):
+                return name
+        return None
+
+    def _get_service_properties(
+        self, validator: Validator, instance: Any
+    ) -> Iterator[tuple[str, str, Validator]]:
+        for task_definition_id, task_definition_validator in get_value_from_path(
+            validator, instance, deque(["TaskDefinition"])
+        ):
+            task_definition_resource_name = self._filter_resource_name(
+                task_definition_id
+            )
+            if task_definition_resource_name is None:
+                continue
+
+            for (
+                launch_type,
+                launch_type_validator,
+            ) in get_value_from_path(
+                task_definition_validator, instance, deque(["LaunchType"])
+            ):
+                yield (
+                    task_definition_resource_name,
+                    launch_type,
+                    launch_type_validator,
+                )
+
+    def _get_task_definition_properties(
+        self, validator: Validator, resource_name: Any
+    ) -> Iterator[tuple[list[Any] | None, Validator]]:
+        task_definition, task_definition_validator = get_resource_by_name(
+            validator, resource_name, ["AWS::ECS::TaskDefinition"]
+        )
+        if not task_definition:
+            return
+
+        for capabilities, capabilities_validator in get_value_from_path(
+            task_definition_validator,
+            task_definition,
+            path=deque(["Properties", "RequiresCompatibilities"]),
+        ):
+            if capabilities is None:
+                yield capabilities, capabilities_validator
+                continue
+            if not isinstance(capabilities, list):
+                continue
+            for capibility, _ in get_value_from_path(
+                capabilities_validator,
+                capabilities,
+                path=deque(["*"]),
+            ):
+                if isinstance(capibility, dict) or capibility == "FARGATE":
+                    break
+            else:
+                yield capabilities, capabilities_validator
+
+    def validate(
+        self, validator: Validator, _: Any, instance: Any, schema: dict[str, Any]
+    ) -> ValidationResult:
+
+        for (
+            task_definition_resource_name,
+            launch_type,
+            service_validator,
+        ) in self._get_service_properties(
+            validator,
+            instance,
+        ):
+            if launch_type != "FARGATE":
+                continue
+            for (
+                capabilities,
+                capabilities_validator,
+            ) in self._get_task_definition_properties(
+                service_validator,
+                task_definition_resource_name,
+            ):
+                if capabilities is None:
+                    yield ValidationError(
+                        "'RequiresCompatibilities' is a required property",
+                        validator="required",
+                        rule=self,
+                        path_override=deque(
+                            list(capabilities_validator.context.path.path)[:-1]
+                        ),
+                    )
+                    continue
+
+                yield ValidationError(
+                    f"{capabilities!r} does not contain items matching 'FARGATE'",
+                    validator="contains",
+                    rule=self,
+                    path_override=capabilities_validator.context.path.path,
+                )
---
+++ test/unit/rules/resources/ecs/test_service_fargate.py
@@ -0,0 +1,210 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+from __future__ import annotations
+
+from collections import deque
+
+import jsonpatch
+import pytest
+
+from cfnlint.jsonschema import ValidationError
+from cfnlint.rules.helpers import get_value_from_path
+from cfnlint.rules.resources.ecs.ServiceFargate import ServiceFargate
+
+
+@pytest.fixture
+def rule():
+    rule = ServiceFargate()
+    yield rule
+
+
+_task_definition = {
+    "Type": "AWS::ECS::TaskDefinition",
+    "Properties": {
+        "RequiresCompatibilities": ["FARGATE"],
+    },
+}
+
+_service = {
+    "Type": "AWS::ECS::Service",
+    "Properties": {
+        "TaskDefinition": {"Ref": "TaskDefinition"},
+        "LaunchType": "FARGATE",
+    },
+}
+
+
+@pytest.mark.parametrize(
+    "template,start_path,expected",
+    [
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": dict(_task_definition),
+                    "Service": dict(_service),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [],
+        ),
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "remove",
+                                "path": "/Properties/RequiresCompatibilities",
+                            },
+                        ],
+                    ),
+                    "Service": dict(_service),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [
+                ValidationError(
+                    ("'RequiresCompatibilities' is a required property"),
+                    validator="required",
+                    rule=ServiceFargate(),
+                    path_override=deque(["Resources", "TaskDefinition", "Properties"]),
+                )
+            ],
+        ),
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "remove",
+                                "path": "/Properties/RequiresCompatibilities",
+                            },
+                        ],
+                    ),
+                    "Service": jsonpatch.apply_patch(
+                        dict(_service),
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/Properties/LaunchType",
+                                "value": "EC2",
+                            },
+                        ],
+                    ),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [],
+        ),
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/Properties/RequiresCompatibilities",
+                                "value": [
+                                    "EC2",
+                                    "FARGATE",
+                                ],
+                            },
+                        ],
+                    ),
+                    "Service": dict(_service),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [],
+        ),
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/Properties/RequiresCompatibilities",
+                                "value": [
+                                    "EC2",
+                                    "EXTERNAL",
+                                ],
+                            },
+                        ],
+                    ),
+                    "Service": dict(_service),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [
+                ValidationError(
+                    ("['EC2', 'EXTERNAL'] does not contain items matching 'FARGATE'"),
+                    validator="contains",
+                    rule=ServiceFargate(),
+                    path_override=deque(
+                        [
+                            "Resources",
+                            "TaskDefinition",
+                            "Properties",
+                            "RequiresCompatibilities",
+                        ]
+                    ),
+                )
+            ],
+        ),
+        (
+            {
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/Properties/RequiresCompatibilities",
+                                "value": {"Fn::FindInMap": ["A", "B", "C"]},
+                            },
+                        ],
+                    ),
+                    "Service": dict(_service),
+                }
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [],
+        ),
+        (
+            {
+                "Parameters": {"MyLaunchType": {"Type": "String"}},
+                "Resources": {
+                    "TaskDefinition": jsonpatch.apply_patch(
+                        dict(_task_definition),
+                        [
+                            {
+                                "op": "replace",
+                                "path": "/Properties/RequiresCompatibilities",
+                                "value": ["EC2", {"Ref": "MyLaunchType"}],
+                            },
+                        ],
+                    ),
+                    "Service": dict(_service),
+                },
+            },
+            deque(["Resources", "Service", "Properties"]),
+            [],
+        ),
+    ],
+    indirect=["template"],
+)
+def test_validate(template, start_path, expected, rule, validator):
+    for instance, instance_validator in get_value_from_path(
+        validator, template, start_path
+    ):
+        errs = list(rule.validate(instance_validator, "", instance, {}))
+        assert errs == expected, f"Expected {expected} got {errs}"
