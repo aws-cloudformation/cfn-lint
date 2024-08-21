778b4b04ad6d713942fc5b33727be948015b2884
Kevin DeJong <kddejong@amazon.com>
2024-08-21 15:10:30 -0700

    Build a better Changelog

---
+++ scripts/release/changelog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+import argparse
+
+# version = "v1.10.3"
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version")
+args = parser.parse_args()
+
+with open("CHANGELOG.md", "r") as f:
+    text = f.read()
+
+output = []
+
+for line in text.splitlines():
+
+    if line.startswith("### "):
+        if args.version == line[3:].strip():
+            found = True
+        else:
+            break
+    else:
+        if found:
+            output.append(line)
+
+build_dir = Path("build")
+with open(build_dir / "CHANGELOG.md", "w") as f:
+    f.write("\n".join(output))
