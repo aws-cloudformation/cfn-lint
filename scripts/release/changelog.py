fccb4d341c8e3f5f4b0370f3d9ef860f8dd6cd7b
Kevin DeJong <kddejong@amazon.com>
2024-08-22 23:13:42 -0700

    Generate schema artifacts on release (#3611)
    * Script to create release JSON schemas

---
+++ scripts/release/changelog.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--version")
+args = parser.parse_args()
+
+with open("CHANGELOG.md", "r") as f:
+    text = f.read()
+
+output = []
+
+for line in text.splitlines():
+
+    if line.startswith("### "):
+        if args.version == line[3:].strip():
+            found = True
+        elif found:
+            break
+    else:
+        if found:
+            output.append(line)
+
+build_dir = Path("build")
+with open(build_dir / "CHANGELOG.md", "w") as f:
+    f.write("\n".join(output))
