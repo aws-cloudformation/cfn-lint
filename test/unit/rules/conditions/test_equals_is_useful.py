19192ef6a075bab14d6af4a73c295711a7db9b0e
Kevin DeJong <kddejong@amazon.com>
2024-12-05 14:07:10 -0800

    Update equals is useful for always false (#3855)

--- test/unit/rules/conditions/test_equals_is_useful.py
+++ test/unit/rules/conditions/test_equals_is_useful.py
@@ -15,10 +15,12 @@ from cfnlint.rules.conditions.EqualsIsUseful import EqualsIsUseful
         ("Equal string and integer", [1, "1"], 1),
         ("Equal string and boolean", [True, "true"], 1),
         ("Equal string and number", [1.0, "1.0"], 1),
-        ("Not equal string and integer", [1, "1.1"], 0),
-        ("Not equal string and boolean", [True, "True"], 0),
+        ("Not equal string and integer", [1, "1.1"], 1),
+        ("Not equal string and boolean", [True, "True"], 1),
         ("No error on bad type", {"true": True}, 0),
         ("No error on bad length", ["a", "a", "a"], 0),
+        ("No with string and account id", ["A", {"Ref": "AWS::AccountId"}], 0),
+        ("No with string and account id", [{"Ref": "AWS::AccountId"}, "A"], 0),
     ],
 )
 def test_names(name, instance, num_of_errors):
@@ -26,4 +28,4 @@ def test_names(name, instance, num_of_errors):
     validator = CfnTemplateValidator({})
     assert (
         len(list(rule.equals_is_useful(validator, {}, instance, {}))) == num_of_errors
-    ), f"Expected {num_of_errors} errors for {name}"
+    ), f"Expected {num_of_errors} errors for {name} and {instance}"
