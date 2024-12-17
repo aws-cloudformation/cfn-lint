bb4a7cda3899ec305e89e301d1499c4c8e9749e1
Kevin DeJong <kddejong@amazon.com>
2024-12-17 14:41:23 -0800

    Allow for deployment files

---
+++ src/cfnlint/runner/template/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+__all__ = ["run_template_by_file_path", "run_template_by_data"]
+
+from cfnlint.runner.template.runner import (
+    run_template_by_data,
+    run_template_by_file_path,
+)
