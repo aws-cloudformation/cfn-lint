0cc2f7779878a0b5d88c7a0cfcc93759f9860798
Kevin DeJong <kddejong@amazon.com>
2024-12-17 15:24:56 -0800

    Cleanup importing

--- src/cfnlint/runner/__init__.py
+++ src/cfnlint/runner/__init__.py
@@ -3,6 +3,22 @@ Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-__all__ = ["main", "Runner"]
+__all__ = [
+    "main",
+    "Runner",
+    "run_template_by_data",
+    "run_template_by_file_path",
+    "run_deployment_file",
+    "CfnLintExitException",
+    "InvalidRegionException",
+    "UnexpectedRuleException",
+]
 
 from cfnlint.runner.cli import Runner, main
+from cfnlint.runner.deployment_file import run_deployment_file
+from cfnlint.runner.exceptions import (
+    CfnLintExitException,
+    InvalidRegionException,
+    UnexpectedRuleException,
+)
+from cfnlint.runner.template import run_template_by_data, run_template_by_file_path
