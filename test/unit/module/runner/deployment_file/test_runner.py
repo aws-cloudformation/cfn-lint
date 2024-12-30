d355a3145630c94d1cb41715e957be1d2111075b
Kevin DeJong <kddejong@amazon.com>
2024-12-30 14:36:25 -0800

    More additions to testing

--- test/unit/module/runner/deployment_file/test_runner.py
+++ test/unit/module/runner/deployment_file/test_runner.py
@@ -11,49 +11,56 @@ import pytest
 from cfnlint.config import ConfigMixIn
 from cfnlint.rules import Match
 from cfnlint.rules.deployment_files.Configuration import Configuration
-from cfnlint.runner.deployment_file import run_deployment_file
+from cfnlint.runner.deployment_file import run_deployment_files
 
 _filename = "deployment-file.yaml"
 
 
 @pytest.mark.parametrize(
-    "name, decode, validate_template_parameters, validate_template_return, expected",
+    (
+        "name,deployment_files,validate_template_parameters,"
+        "validate_template_return,expected"
+    ),
     [
         (
             "A standard git sync file",
-            (
-                {
-                    "template-file-path": "../a/path",
-                    "parameters": {
-                        "Foo": "Bar",
+            {
+                _filename: (
+                    {
+                        "template-file-path": "../a/path",
+                        "parameters": {
+                            "Foo": "Bar",
+                        },
+                        "tags": {
+                            "Key": "Value",
+                        },
                     },
-                    "tags": {
-                        "Key": "Value",
-                    },
-                },
-                [],
-            ),
+                    [],
+                ),
+            },
             {
                 "filename": Path("../a/path"),
-                "template_parameters": [{"Foo": "Bar"}],
+                "parameters": [{"Foo": "Bar"}],
             },
             [],
             [],
         ),
         (
             "Bad template-file-path type",
-            (
-                {
-                    "template-file-path": ["../a/path"],
-                    "parameters": {
-                        "Foo": "Bar",
+            {
+                _filename: (
+                    {
+                        "template-file-path": ["../a/path"],
+                        "parameters": {
+                            "Foo": "Bar",
+                        },
+                        "tags": {
+                            "Key": "Value",
+                        },
                     },
-                    "tags": {
-                        "Key": "Value",
-                    },
-                },
-                [],
-            ),
+                    [],
+                )
+            },
             {},
             [],
             [
@@ -70,21 +77,23 @@ _filename = "deployment-file.yaml"
         ),
         (
             "Bad template-file-path type",
-            (
-                {
-                    "template-file-path": "../a/path",
-                    "parameters": {
-                        "Foo": "Bar",
+            {
+                _filename: (
+                    {
+                        "template-file-path": "../a/path",
+                        "parameters": {
+                            "Foo": "Bar",
+                        },
+                        "tags": {
+                            "Key": "Value",
+                        },
                     },
-                    "tags": {
-                        "Key": "Value",
-                    },
-                },
-                [],
-            ),
+                    [],
+                ),
+            },
             {
                 "filename": Path("../a/path"),
-                "template_parameters": [{"Foo": "Bar"}],
+                "parameters": [{"Foo": "Bar"}],
             },
             iter(
                 [
@@ -111,30 +120,67 @@ _filename = "deployment-file.yaml"
                 )
             ],
         ),
+        (
+            "Bad decode",
+            {
+                _filename: (
+                    {},
+                    [
+                        Match(
+                            linenumber=1,
+                            columnnumber=1,
+                            linenumberend=1,
+                            columnnumberend=1,
+                            filename=_filename,
+                            message=f"Deployment file {_filename!r} is not supported",
+                            rule=Configuration(),
+                        )
+                    ],
+                ),
+            },
+            {},
+            None,
+            [
+                Match(
+                    linenumber=1,
+                    columnnumber=1,
+                    linenumberend=1,
+                    columnnumberend=1,
+                    filename=_filename,
+                    message=f"Deployment file {_filename!r} is not supported",
+                    rule=Configuration(),
+                )
+            ],
+        ),
     ],
 )
 def test_runner(
-    name, decode, validate_template_parameters, validate_template_return, expected
+    name,
+    deployment_files,
+    validate_template_parameters,
+    validate_template_return,
+    expected,
 ):
 
-    with patch(
-        "cfnlint.runner.deployment_file.runner.decode", return_value=decode
-    ) as mock_decode:
+    decode_results = [v for _, v in deployment_files.items()]
+    deployment_files = {k for k, _ in deployment_files.items()}
+    with patch("cfnlint.runner.deployment_file.runner.decode") as mock_decode:
+        mock_decode.side_effect = decode_results
         with patch(
             "cfnlint.runner.deployment_file.runner.run_template_by_file_path",
             return_value=validate_template_return,
         ) as mock_run_template_by_file_path:
-            deployment = list(run_deployment_file(_filename, ConfigMixIn(), None))
+            config = ConfigMixIn([], deployment_files=deployment_files)
+            deployment = list(run_deployment_files(config, None))
 
-            mock_decode.assert_called_once()
+            for deployment_file in deployment_files:
+                mock_decode.assert_called_with(deployment_file)
             if validate_template_parameters:
                 mock_run_template_by_file_path.assert_called_once()
                 config = mock_run_template_by_file_path.call_args_list
                 assert config[0].kwargs.get(
                     "config"
-                ).template_parameters == validate_template_parameters.get(
-                    "template_parameters"
-                )
+                ).parameters == validate_template_parameters.get("parameters")
                 assert config[0].kwargs.get(
                     "filename"
                 ) == validate_template_parameters.get("filename")
