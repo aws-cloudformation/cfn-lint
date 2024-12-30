48f610d9496e22efd609e7a29464f7203f556270
Kevin DeJong <kddejong@amazon.com>
2024-12-30 14:36:25 -0800

    More additions to testing

--- src/cfnlint/runner/deployment_file/__init__.py
+++ src/cfnlint/runner/deployment_file/__init__.py
@@ -3,6 +3,6 @@ Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
-__all__ = ["run_deployment_file"]
+__all__ = ["run_deployment_files"]
 
-from cfnlint.runner.deployment_file.runner import run_deployment_file
+from cfnlint.runner.deployment_file.runner import run_deployment_files
