75a32ec6374067f043e21117bf05fd2ad398fe2c
Kevin DeJong <kddejong@amazon.com>
2025-04-19 08:10:39 -0700

    Update context when resolving IFs (#4116)

--- src/cfnlint/context/__init__.py
+++ src/cfnlint/context/__init__.py
@@ -1,3 +1,4 @@
 __all__ = ["Context", "create_context_for_template"]
 
+from cfnlint.context.conditions.exceptions import Unsatisfiable
 from cfnlint.context.context import Context, Path, Resource, create_context_for_template
