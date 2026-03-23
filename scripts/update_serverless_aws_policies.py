cb99846e24ded4cf4919f35546f9da1c4f7bbaad
Kevin DeJong <kddejong@amazon.com>
2026-03-23 11:15:41 -0700

    Revert "Fix KeyError when AmazonGuardDutyFullAccess policy no longer exists"
    This reverts commit 41924443d9d29de083ee3864b2179383c1dc4aec.

--- scripts/update_serverless_aws_policies.py
+++ scripts/update_serverless_aws_policies.py
@@ -21,7 +21,7 @@ def main():
     policyLoader.load()
 
     # Exception from PR #4209
-    policyLoader._policy_map.pop("AmazonGuardDutyFullAccess", None)
+    policyLoader._policy_map.pop("AmazonGuardDutyFullAccess")
 
     filename = "src/cfnlint/data/Serverless/ManagedPolicies.json"
     with open(filename, "w+", encoding="utf-8") as f:
